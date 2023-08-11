7bb31096e9e039cd41ce2f38d47fbd424fa51e95
Abhishek Angira <abhishek.angira@celigo.com>
2023-08-11 16:48:25 +0530

    C52048

---
+++ __tests__/em2.0/TC_C52048.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import testData from "@testData/Em2.0/C52048.json";
+
+test.describe("C52048 Verify the Resolved errors tab, when no results are returned for filter selections", () => {
+  test("C52048 Verify the Resolved errors tab, when no results are returned for filter selections", async ({
+    io,
+    page
+  }) => {
+    const id = await io.fillForm(testData, "FLOWS");
+    await io.api.runBatchFlowViaAPI("erronous flow", id);
+    const lastRun = page.getByText("Last run");
+    await lastRun.waitFor({ state: "visible" });
+    await page.getByText("1 error").nth(1).click();
+    await io.flowBuilder.clickByText("Resolved errors");
+    const classification = await page.getByText("Classification").elementHandle();
+    const btn = await classification.$("button");
+    await btn.click();
+    await io.flowBuilder.clickByText("Connection");
+    await io.flowBuilder.clickByText("Apply");
+    await expect(
+      page.getByText(
+        "You don't have any errors that match the filters you applied.."
+      )
+    ).toBeVisible();
+    await expect(
+      page.getByText("Clear all filters to see any errors for this step.")
+    ).toBeVisible();
+  });
+});
