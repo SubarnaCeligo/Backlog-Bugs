96fd2e4c8b360b8afcf48b19dfb495909148d004
Abhishek Angira <abhishek.angira@celigo.com>
2023-08-16 23:40:44 +0530

    C44421

---
+++ __tests__/mapper2.0/TC_C44421.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import testData from "@testData/Mapper2.0/C44421.json";
+
+test.describe(`C44421 verify destination mapping filed dropdown with lengthy field name`, () => {
+  test(`C44421 verify destination mapping filed dropdown with lengthy field name`, async ({
+    io,
+    page
+  }) => {
+    await io.flowBuilder.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.flowBuilder.clickByText("Automation Flows");
+    const testCase = page.getByText("C44421").first();
+    try {
+      await testCase.waitFor({ state: "visible", timeout: 5000 });
+      await testCase.click();
+    } catch {
+      const id = await io.fillForm(testData, "FLOWS");
+      await io.api.runBatchFlowViaAPI("C44421", id);
+    }
+    await page.getByLabel("Define options").nth(1).click();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
+    const destinationField = page.locator(
+      selectors.mappings.Mapper2dot0PO.DESTINATIONFIELDS
+    );
+    await destinationField.waitFor({ state: "visible" });
+    await destinationField.click();
+    const $destinationField = await destinationField.elementHandle();
+    const $textarea = await $destinationField.$("textarea");
+    await $textarea.fill(
+      "C51986 Verify vertical line must be added to source rowC51986 Verify vertical line must be added to source rowC51986 Verify vertical line must be added to source row"
+    );
+    const heightBefore = (await destinationField.boundingBox()).height;
+    await io.flowBuilder.click(selectors.mappings.Mapper2dot0PO.SOURCEFIELDS);
+    const heightAfter = (await destinationField.boundingBox()).height;
+    expect(heightBefore).toBeGreaterThan(heightAfter);
+  });
+});
--- __tests__/mapper2.0/mapper2.0.spec.ts
+++ __tests__/mapper2.0/mapper2.0.spec.ts
@@ -1,1 +1,2 @@
 require("./TC_C51986");
+require("./TC_C44421");
