4b25f9b09ccd1f545ebe874c5b3933cc3bb01472
Harshita <harshita.rai@celigo.com>
2023-08-16 13:58:34 +0530

    latest

--- __tests__/imports/TC_C22818.ts
+++ __tests__/imports/TC_C22818.ts
@@ -10,21 +10,20 @@ test.describe('Verify for the backupBucket for S3 import, only AFE 2.0 supports'
             'FLOWS'
           );
 
-          await io.flowBuilder.clickByText("Transfer");
+        await io.flowBuilder.clickByText("Transfer");
 
 
         await io.flowBuilder.clickByText("Advanced");
 
         const element = await page.waitForSelector(selectors.importPagePO.S3_IMPORT_BACKUP_BUCKET);
+        await page.click(selectors.importPagePO.S3_IMPORT_BACKUP_BUCKET);
 
 
-          const toggleElement1 = await page.locator('[data-toggle-id="afe-1"]');
-    const toggleElement2 = await page.locator('[data-toggle-id="afe-2.09"]');
+        const toggleElement1 = await page.locator('[data-toggle-id="afe-1"]');
+        const toggleElement2 = await page.locator('[data-toggle-id="afe-2.09"]');
 
-    // Use the expect API to assert that the elements are not present
-    await expect(toggleElement1).not.toBeVisible();
-    await expect(toggleElement2).not.toBeVisible();
-
-        
+        // Use the expect API to assert that the elements are not present
+        await expect(toggleElement1).not.toBeVisible();
+        await expect(toggleElement2).not.toBeVisible();
       });       
   })
