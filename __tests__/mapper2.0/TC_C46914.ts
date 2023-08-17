5013b1098ceab2b880e45dc3e137001f527bc50e
Abhishek Angira <abhishek.angira@celigo.com>
2023-08-17 15:08:17 +0530

    C46914

---
+++ __tests__/mapper2.0/TC_C46914.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import testData from "@testData/Mapper2.0/C46914.json";
+
+test.describe(`C46914 Verify When no saved mappings exist, add empty parent row for output to be of 'rows' type`, () => {
+  test(`C46914 Verify When no saved mappings exist, add empty parent row for output to be of 'rows' type`, async ({
+    io,
+    page
+  }) => {
+    await io.flowBuilder.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.flowBuilder.clickByText("Automation Flows");
+    const testCase = page.getByText("C46914").first();
+    try {
+      await testCase.waitFor({ state: "visible", timeout: 5000 });
+      await testCase.click();
+    } catch {
+      const id = await io.fillForm(testData, "FLOWS");
+      await io.api.runBatchFlowViaAPI("C46914", id);
+    }
+    await page.getByLabel("Define options").nth(1).click();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
+    await io.flowBuilder.waitForElementAttached(
+      selectors.mappings.Mapper2dot0PO.CHANGEOUTPUTFORMAT
+    );
+    await io.flowBuilder.click(
+      selectors.mappings.Mapper2dot0PO.CHANGEOUTPUTFORMAT
+    );
+    await io.flowBuilder.clickByText(
+      "Create destination rows [ ] from source rows [ ]"
+    );
+    const destinationFieldsCount = await page
+      .locator(selectors.mappings.Mapper2dot0PO.DESTINATIONFIELDS)
+      .count();
+    const sourceFieldsCount = await page
+      .locator(selectors.mappings.Mapper2dot0PO.SOURCEFIELDS)
+      .count();
+    expect(destinationFieldsCount).toBe(2);
+    expect(sourceFieldsCount).toBe(2);
+  });
+});
--- __tests__/mapper2.0/mapper2.0.spec.ts
+++ __tests__/mapper2.0/mapper2.0.spec.ts
@@ -1,3 +1,5 @@
 require("./TC_C51986");
 require("./TC_C44421");
 require("./TC_C46907");
+// new
+require("./TC_C46914");
