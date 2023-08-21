b0c93abbbe8318312583ebe21e7d546040a18c42
Prajwal Sharma <prajwal.sharma@celigo.com>
2023-08-21 17:33:40 +0530

    Update TC_C51626.ts

--- __tests__/EM2.0/TC_C51626.ts
+++ __tests__/EM2.0/TC_C51626.ts
@@ -9,6 +9,12 @@ test.describe("C51626 Verify the Actions coloumn the New view by navigating from
         const lastRun = page.getByText('Last run')
         await lastRun.waitFor({state: 'visible'});
         await page.getByText("1 error").nth(1).click();
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.EM2dot0PO.TOGGLE_VIEW_DROPDOWN);
+        let options = await page.$$(selectors.flowBuilderPagePO.EM2dot0PO.TOGGLE_VIEW_DROPDOWN_OPTIONS);
+        options[1].click();
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.EM2dot0PO.TOGGLE_VIEW_DROPDOWN);
+        options = await page.$$(selectors.flowBuilderPagePO.EM2dot0PO.TOGGLE_VIEW_DROPDOWN_OPTIONS);
+        options[0].click();
         await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EM2dot0PO.OPEN_ERRORS_TABLE_HEADERS);
         let columnTitles = await page.$$(selectors.flowBuilderPagePO.EM2dot0PO.OPEN_ERRORS_TABLE_HEADERS);
         columnTitles.forEach(async columnTitle => {
