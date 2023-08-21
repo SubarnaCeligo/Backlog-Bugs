d654345f7f1498bc294da9c2c3524f657d45ccb1
Abhishek Angira <abhishek.angira@celigo.com>
2023-08-21 17:22:52 +0530

    C44396

--- __tests__/mapper2.0/TC_C44396.ts
+++ __tests__/mapper2.0/TC_C44396.ts
@@ -7,31 +7,97 @@ test.describe(`C44396 Verify the type of field mapping should be displayed for t
     io,
     page
   }) => {
-    await io.flowBuilder.navigateTo(io.data.links.HOME_PAGE_URL);
-    await io.flowBuilder.clickByText("Automation Flows");
-    const testCase = page.getByText("C44396").first();
     try {
+      await io.homePage.navigateTo(process.env.IO_Integration_URL);
+      const testCase = page.getByText("C44396").first();
       await testCase.waitFor({ state: "visible", timeout: 5000 });
       await testCase.click();
     } catch {
-      const id = await io.fillFormUI(testData, "FLOWS");
-      await io.api.runBatchFlowViaAPI("C44396", id);
+      await io.fillFormUI(testData, "FLOWS");
     }
     await page.getByLabel("Define options").nth(1).click();
     await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
-    const destinationField = page.locator(
-      selectors.mappings.Mapper2dot0PO.DESTINATIONFIELDS
-    );
-    await destinationField.waitFor({ state: "visible" });
-    await destinationField.click();
-    const $destinationField = await destinationField.elementHandle();
-    const $textarea = await $destinationField.$("textarea");
-    await $textarea.fill(
-      "C51986 Verify vertical line must be added to source rowC51986 Verify vertical line must be added to source rowC51986 Verify vertical line must be added to source row"
-    );
-    const heightBefore = (await destinationField.boundingBox()).height;
-    await io.flowBuilder.click(selectors.mappings.Mapper2dot0PO.SOURCEFIELDS);
-    const heightAfter = (await destinationField.boundingBox()).height;
-    expect(heightBefore).toBeGreaterThan(heightAfter);
+    const labels = [
+      "Hard-coded",
+      "Handlebars expression",
+      "Dynamic lookup",
+      "Static lookup"
+    ];
+    for (let i = 0; i < labels.length; i++) {
+      const destinationFields = page.locator(
+        selectors.mappings.Mapper2dot0PO.DESTINATIONFIELDS
+      );
+      const sourceFields = page.locator(
+        selectors.mappings.Mapper2dot0PO.SOURCEFIELDS
+      );
+      const addButtons = page.locator(
+        selectors.mappings.Mapper2dot0PO.ADDBUTTONS
+      );
+      const settingsButtons = page.locator(
+        selectors.mappings.Mapper2dot0PO.SETTINGSBUTTONS
+      );
+      await sourceFields.nth(i).waitFor({ state: "visible" });
+      switch (i) {
+        case 0: {
+          await sourceFields.nth(i).click();
+          let $sourceField = await sourceFields.nth(i).elementHandle();
+          let $sourceTextarea = await $sourceField.$("textarea");
+          await $sourceTextarea.fill(`"hello"`);
+          await addButtons.nth(i).click();
+          break;
+        }
+        case 1:
+          await sourceFields.nth(i).click();
+          let $sourceField = await sourceFields.nth(i).elementHandle();
+          let $sourceTextarea = await $sourceField.$("textarea");
+          await $sourceTextarea.fill("{{hello}}");
+          await addButtons.nth(i).click();
+          break;
+        case 2: {
+          await destinationFields.nth(i).click();
+          let $destinationField = await destinationFields.nth(i).elementHandle();
+          let $destinationTextarea = await $destinationField.$("textarea");
+          await $destinationTextarea.fill("hello");
+          await sourceFields.nth(i).click();
+          await settingsButtons.nth(i).click();
+          await page.getByText("Standard").click();
+          await page.getByText("Lookup", { exact: true }).click();
+          await page.getByText("Dynamic", { exact: true }).click();
+          await page.getByText("Please select", { exact: true }).click();
+          await page.getByText("GET", { exact: true }).click();
+          await page.getByPlaceholder("Resource identifier path").fill("hello");
+          await page
+            .getByPlaceholder("Alphanumeric characters only")
+            .fill("hello");
+          await page.locator(selectors.basePagePO.SAVE_AND_CLOSE).nth(1).click();
+          break;
+        }
+        case 3:
+          await destinationFields.nth(i).click();
+          let $destinationField = await destinationFields.nth(i).elementHandle();
+          let $destinationTextarea = await $destinationField.$("textarea");
+          await $destinationTextarea.fill("hello");
+          await sourceFields.nth(i).click();
+          await settingsButtons.nth(i).click();
+          await page.getByText("Standard").click();
+          await page.getByText("Lookup", { exact: true }).click();
+          await page.getByText("Static", { exact: true }).click();
+          await page
+            .locator(`[id='lookup.mapList'] textarea`)
+            .first()
+            .fill("test");
+          await page.locator(`[id='lookup.mapList'] textarea`).nth(2).fill("123");
+          await page
+            .getByPlaceholder("Alphanumeric characters only")
+            .first()
+            .fill("test");
+          await page.locator(selectors.basePagePO.SAVE_AND_CLOSE).nth(1).click();
+          break;
+      }
+      await expect(page.getByLabel(labels[i], { exact: true })).toBeVisible({
+        timeout: 10000
+      });
+      await addButtons.nth(i).click();
+    }
   });
 });
