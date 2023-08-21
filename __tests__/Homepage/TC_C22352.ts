934aa1085778a6856e3038f20da768dde58022a6
Prajwal Sharma <prajwal.sharma@celigo.com>
2023-08-21 17:28:34 +0530

    Update TC_C22352.ts

--- __tests__/Homepage/TC_C22352.ts
+++ __tests__/Homepage/TC_C22352.ts
@@ -5,8 +5,8 @@ test.describe("C22352 Verify UX for download integration dropdown field", () => 
     test("C22352 Verify UX for download integration dropdown field", async ({io, page}) => {
         await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
         await io.homePage.click(selectors.homePagePO.INTEGRATION_TILES_ACTIONS_MENU);
-        expect( await page.$(selectors.homePagePO.CLONE_INTEGRATION)).not.toBe(null);
-        expect( await page.$(selectors.homePagePO.GENERATE_TEMPLATE_ZIP)).not.toBe(null);
-        expect( await page.$(selectors.homePagePO.DELETE_INTEGRATION)).not.toBe(null);
+        expect( await page.locator(selectors.homePagePO.CLONE_INTEGRATION)).toBeVisible();
+        expect( await page.locator(selectors.homePagePO.GENERATE_TEMPLATE_ZIP)).toBeVisible();
+        expect( await page.locator(selectors.homePagePO.DELETE_INTEGRATION)).toBeVisible();
     });
 });
