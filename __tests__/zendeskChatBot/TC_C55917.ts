9aecf2fe7ff59a153c1260fcbe8384bc87d1e62f
prajwalsharma2000 <prajwal.sharma@celigo.com>
2023-08-21 13:05:27 +0530

    C55917 correction

--- __tests__/zendeskChatBot/TC_C55917.ts
+++ __tests__/zendeskChatBot/TC_C55917.ts
@@ -5,8 +5,10 @@ test.describe("C55917 Verify the Default Structure/options of Pendo-Zendesk chat
     test("C55917 Verify the Default Structure/options of Pendo-Zendesk chat bot which is displayed in the integrator.io", async ({page,io}) => {
         await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
         await io.homePage.waitForElementAttached(selectors.basePagePO.CHAT_BOT);
-        await io.homePage.click(selectors.basePagePO.CHAT_BOT);
-        await io.homePage.click(selectors.basePagePO.CHAT_BOT);
+        console.log(await io.homePage.isVisible(selectors.basePagePO.CHAT_BOT_OPTIONS));
+        if(!await io.homePage.isVisible(selectors.basePagePO.CHAT_BOT_OPTIONS))
+          await io.homePage.click(selectors.basePagePO.CHAT_BOT);
+        await io.homePage.waitForElementAttached(selectors.basePagePO.CHAT_BOT_OPTIONS);
         const helpList = await page.$$(selectors.basePagePO.CHAT_BOT_OPTIONS);
         expect(await helpList[0].innerText()).toContain('Search our docs');
         expect(await helpList[1].innerText()).toContain('Chat with us');
