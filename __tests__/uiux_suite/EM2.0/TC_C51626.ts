c4c844bb21d9133d583dc4eeeef77ad8273b6a3d
ashrithyalala <y.ashrith22@gmail.com>
2023-08-23 15:22:07 +0530

    test folder structure changes

--- __tests__/uiux_suite/EM2.0/TC_C51626.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51626.ts
@@ -1,10 +1,10 @@
 import {expect, test} from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import C51626 from "../../testData/EM2.0/TC_C51626.json"
+import C51626 from "../../../testData/EM2.0/TC_C51626.json"
 
 test.describe("C51626 Verify the Actions coloumn the New view by navigating from the current view", () => {
     test("C51626 Verify the Actions coloumn the New view by navigating from the current view", async ({io, page}) => {
-        const errorFlowId = await io.fillForm(C51626, "FLOWS");
+        const errorFlowId = await io.fillFormUI(C51626, "FLOWS");
         await io.api.runBatchFlowViaAPI('TC_C51626', errorFlowId);
         const lastRun = page.getByText('Last run')
         await lastRun.waitFor({state: 'visible'});
