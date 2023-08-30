1e82bbd3c072aca378a7c94e9044cc985ca3d045
paresh <paresh.vilas.more@celigo.com>
2023-08-30 19:16:54 +0530

    Automation In playwright

---
+++ __tests__/e2e_flows/FTP-SF/TC_C22867_FTP_FTP_SkipAgg.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as FTPtoFTP from "@testData/Flows/create/ftp/TC_C22867_FTP_FTP_SkipAgg.json"
+import allure from "allure-playwright";
+
+test.describe("E2E Flows", () => {
+
+    test.beforeEach(async ({ io }) => {
+        await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+    });
+
+    test("TC_C22867_FTP_FTP_SkipAggs", async ({
+        io
+    }, testInfo) => {
+
+        //Creating PageGenerator 
+        await test.step("*** Creating Page Generator ***", async () => {
+            var exportValidation = await io.pageGenerator(allure, FTPtoFTP);
+        });
+        //Creating PageProcessor
+        await test.step("*** Creating Page Processor ***", async () => {
+            var importValidation = await io.pageProcessor(allure, FTPtoFTP);
+        });
+        //Save, Enable and run the Flow ***
+        await test.step("*** Save, Enable And Run The Flow *** ", async () => {
+            //TODO : Save the flow with test title     
+            await io.flowBuilder.saveFlow(testInfo.title)
+
+            //Enable flow
+            await io.flowBuilder.enableFlow()
+        });
+    });
+});
