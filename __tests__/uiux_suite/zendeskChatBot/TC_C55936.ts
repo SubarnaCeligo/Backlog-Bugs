df779a3fdfa4f65b8cd64256b93ac1a01e8b9768
Abhishek Angira <abhishek.angira@celigo.com>
2023-08-31 11:40:36 +0530

    C55917

---
+++ __tests__/uiux_suite/zendeskChatBot/TC_C55936.ts
@@ -0,0 +1,51 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("C55917 Verify the Default Structure/options of Pendo-Zendesk chat bot which is displayed in the integrator.io", () => {
+  test("C55917 Verify the Default Structure/options of Pendo-Zendesk chat bot which is displayed in the integrator.io", async ({
+    page,
+    io
+  }) => {
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.homePage.waitForElementAttached(selectors.basePagePO.CHAT_BOT);
+    if (!(await io.homePage.isVisible(selectors.basePagePO.CHAT_BOT_OPTIONS)))
+      await io.homePage.click(selectors.basePagePO.CHAT_BOT);
+    await io.homePage.waitForElementAttached(
+      selectors.basePagePO.CHAT_BOT_OPTIONS
+    );
+    const searchOurDocs = (
+      await page.$$(selectors.basePagePO.CHAT_BOT_OPTIONS)
+    )[0];
+    await searchOurDocs.click();
+    const iframe = page.frameLocator("#webWidget");
+    const getInTouchBtn = iframe.locator('[data-testid="pill-button"]');
+    await getInTouchBtn.click();
+    await iframe.getByText("Leave a message").click();
+    await iframe.getByText("Other").click();
+
+    const getInputValue = async (label: string) =>
+      await iframe
+        .getByText(label)
+        .evaluate(e =>
+          e.parentElement.parentElement.nextElementSibling.getAttribute("value")
+        );
+
+    const getFullLabelText = async (label: string) =>
+      await iframe.getByText(label).evaluate(e => e.parentElement.innerText);
+
+    const yourNameInputValue = await getInputValue("Your name");
+    const emailInputValue = await getInputValue("Email address");
+
+    const subjectLabel = await getFullLabelText("Subject");
+    const descriptionLabel = await getFullLabelText("Description");
+    const environmentLabel = await getFullLabelText("Environment");
+
+    expect(yourNameInputValue).not.toBe("");
+    expect(emailInputValue).not.toBe("");
+    expect(subjectLabel).not.toContain("optional");
+    expect(descriptionLabel).not.toContain("optional");
+    expect(environmentLabel).not.toContain("optional");
+
+    await iframe.locator('[data-testid="Icon--dash"]').click();
+  });
+});
--- __tests__/uiux_suite/zendeskChatBot/zendeskChatBot.spec.ts
+++ __tests__/uiux_suite/zendeskChatBot/zendeskChatBot.spec.ts
@@ -1,2 +1,3 @@
-require('./TC_C55915');
-require('./TC_C55917');
+require("./TC_C55915");
+require("./TC_C55917");
+require("./TC_C55936");
