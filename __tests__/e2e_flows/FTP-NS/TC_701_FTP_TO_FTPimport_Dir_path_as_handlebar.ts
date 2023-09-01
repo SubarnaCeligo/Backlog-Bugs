55f73ad7e123a9db8ad567f909e695cc3af49e0d
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2023-09-01 08:37:05 +0530

    Automation of TC_492, 493 ,701, 708 and 710

---
+++ __tests__/e2e_flows/FTP-NS/TC_701_FTP_TO_FTPimport_Dir_path_as_handlebar.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import allure from "allure-playwright";
+import NS from "@testData/Flows/create/ftp/TC_701_FTP_TO_FTPimport_Dir_path_as_handlebar.json"
+
+test.describe("E2E Flows", () => {
+    test("TC_701_FTP_TO_FTPimport_Dir_path_as_handlebar", async ({
+        io
+    }, testInfo) => {
+        //Navigate to Home 
+        await test.step("*** Login and Navigate to Application ***", async () => {
+            await io.homePage.navigateToHome()
+        });
+
+        //Creating PageGenerator 
+        await test.step("*** Creating Page Generator ***", async () => {
+            var exportValidation = await io.pageGenerator(allure, NS);
+        });
+
+        //Creating PageProcessor
+        await test.step("*** Creating Page Processor ***", async () => {
+            var importValidation = await io.pageProcessor(allure, NS);
+        });
+
+        //Save, Enable and run the Flow ***
+        await test.step("*** Save, Enable And Run The Flow *** ", async () => {
+            await io.flowBuilder.saveFlow(testInfo.title)
+            //Enable and run the flow
+            // await io.flowBuilder.runFlow()
+        });
+    });
+});
