a5c178f54c5aed881358f7a3cd772dc3c7353cea
Harshita <harshita.rai@celigo.com>
2023-09-06 15:28:42 +0530

    C93677, C93679,C93680,C93681,C93682

---
+++ __tests__/uiux_suite/scriptDebugger/TC_C93679.ts
@@ -0,0 +1,50 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import C30651 from "@testData/Flows/C26246.json"
+
+
+test.describe('C93679Validate user is getting auto-fill of funtion stub while creating "filter" script through flow builder hook', () => {
+   
+    test('Validate user is getting auto-fill of funtion stub while creating "filter" script through flow builder hook', async({io,page}) => {
+  
+      const id =  await io.fillFormUI(
+        C30651,
+        'FLOWS'
+      );
+  
+      await io.flowBuilder.click(selectors.exportsPagePO.EXPORT_ADDPROCESSOR_BUTTON)
+  
+      await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXPORT_HOOK)
+      await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT_HOOK)
+  
+      await page.getByLabel('Create script').click();
+    
+    
+      await io.flowBuilder.fill(selectors.importPagePO.NAME, "Filter script");
+     
+       
+     
+       // Ensure that the choose function stub field is visible
+       const chooseFunctionStubField = await page.$(selectors.basePagePO.FUNCTION_STUB);
+      await chooseFunctionStubField.click();
+     
+      await io.flowBuilder.selectTextfromDropDown(page,"filter");
+       // Scroll through the list of options to find "Transform" 
+     
+       await io.flowBuilder.clickByText("Save & close");
+     
+       // await page.pause();
+     
+       await page.waitForTimeout(2000)
+  
+  
+  
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.EDIT_ECRIPT_LABEL_SELECTOR)
+    
+    const divSelector = selectors.basePagePO.ACE_CONTENT; // Selector for the <div> element
+   
+  const divTextContent = await page.textContent(divSelector);
+  
+  expect(divTextContent).not.toBe(null);
+    });
+  })
