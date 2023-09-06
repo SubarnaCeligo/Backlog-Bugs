2cf787629e9d354f4df144cc0152943f948cc770
Abhishek Angira <abhishek.angira@celigo.com>
2023-09-06 17:23:00 +0530

    fixed C46914 and added steps

--- __tests__/uiux_suite/mapper2.0/TC_C46914.ts
+++ __tests__/uiux_suite/mapper2.0/TC_C46914.ts
@@ -7,26 +7,35 @@ test.describe(`C46914 Verify When no saved mappings exist, add empty parent row 
     io,
     page
   }) => {
-    await io.flowBuilder.navigateTo(process.env["IO_UI_CONNECTOR_URL"]+"home");
-    await io.flowBuilder.clickByText("Automation Flows");
-    const testCase = page.getByText("C46914").first();
+    await io.homePage.navigateTo(process.env.IO_Integration_URL);
+    await io.homePage.addStep("Navigated to Automation Flows");
     try {
+      const testCase = page.getByText("C46914").first();
       await testCase.waitFor({ state: "visible", timeout: 5000 });
       await testCase.click();
+      await io.homePage.addStep("Clicked on 'C46914'");
     } catch {
       const id = await io.fillFormUI(testData, "FLOWS");
+      await io.homePage.addStep("Created new flow 'C46914'");
       await io.api.runBatchFlowViaAPI("C46914", id);
+      await io.homePage.addStep("Ran flow 'C46914' via API");
     }
     await page.getByLabel("Define options").nth(1).click();
+    await io.flowBuilder.addStep("Clicked on 'Plus button'");
     await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
+    await io.flowBuilder.addStep("Clicked on 'Import Mappings'");
     await io.flowBuilder.waitForElementAttached(
       selectors.mappings.Mapper2dot0PO.CHANGEOUTPUTFORMAT
     );
     await io.flowBuilder.click(
       selectors.mappings.Mapper2dot0PO.CHANGEOUTPUTFORMAT
     );
+    await io.flowBuilder.addStep("Clicked on 'Change Output Format'");
     await io.flowBuilder.clickByText(
-      "Create destination rows [ ] from source rows [ ]"
+      "Create destination rows [ ] from source record { }"
+    );
+    await io.flowBuilder.addStep(
+      "Clicked on 'Create destination rows from source record'"
     );
     const destinationFieldsCount = await page
       .locator(selectors.mappings.Mapper2dot0PO.DESTINATIONFIELDS)
@@ -35,6 +44,8 @@ test.describe(`C46914 Verify When no saved mappings exist, add empty parent row 
       .locator(selectors.mappings.Mapper2dot0PO.SOURCEFIELDS)
       .count();
     expect(destinationFieldsCount).toBe(2);
+    await io.flowBuilder.addStep("Verified 'Destination Fields' count is 2");
     expect(sourceFieldsCount).toBe(2);
+    await io.flowBuilder.addStep("Verified 'Source Fields' count is 2");
   });
 });
