d0ec3a4cd9956b77162ccff8415a7fdc58ba58cf
Abhishek Angira <abhishek.angira@celigo.com>
2023-09-06 14:06:21 +0530

    TC_C61159

---
+++ __tests__/uiux_suite/suiteApp/TC_C61159.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe(`C61159 Verify IIf all the NS stepsn my integration are solely configured to use SuiteApp then only the SuiteApp installation step has to be shown in the install steps.`, () => {
+  test(`C61159 Verify IIf all the NS stepsn my integration are solely configured to use SuiteApp then only the SuiteApp installation step has to be shown in the install steps.`, async ({
+    page,
+    io
+  }) => {
+    await io.homePage.navigateTo(
+      `${io.data.links.HOME_PAGE_URL}/installIntegration`
+    );
+    await io.homePage.addStep(
+      "Navigated to install integration page (/home/installIntegration)"
+    );
+    const fileChooserPromise = page.waitForEvent("filechooser");
+    await page.getByText("Choose file").click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles("testData/SuiteApp/C61159.zip");
+    await io.homePage.addStep("Uploaded integration zip file");
+    await page.getByText("Install Integration").click();
+    await io.homePage.addStep("Clicked 'Install Integration' button");
+    // TODO: await page.locator(selectors.basePagePO.DIALOG_PROCEED_BUTTON).click();
+    await page.locator("[data-test='Proceed']").click();
+    await io.homePage.addStep("Clicked 'Proceed' button");
+    await expect(page.getByText("Integrator SuiteApp")).toBeVisible();
+    await io.homePage.addStep("Checked if 'Integrator SuiteApp' is visible");
+    await expect(
+      page.getByText("Integrator Bundle", { exact: true })
+    ).toBeVisible();
+    await io.homePage.addStep("Checked if 'Integrator Bundle' is visible");
+  });
+});
--- __tests__/uiux_suite/suiteApp/suiteApp.spec.ts
+++ __tests__/uiux_suite/suiteApp/suiteApp.spec.ts
@@ -2,3 +2,4 @@ require("./TC_C61132");
 require("./TC_C61136");
 require("./TC_C61153");
 require("./TC_C61156");
+require("./TC_C61159");
