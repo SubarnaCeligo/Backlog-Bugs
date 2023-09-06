22b364fb6f657b6d6d5419ec02c0e9688c25f253
Abhishek Angira <abhishek.angira@celigo.com>
2023-09-06 17:23:17 +0530

    removed unnecessary folder

--- __tests__/uiux_suite/mapper2.0/mapper2.0.spec.ts
+++ __tests__/uiux_suite/mapper2.0/mapper2.0.spec.ts
@@ -1,5 +1,8 @@
 require("./TC_C51986");
-require("./TC_C44421");
-require("./TC_C46907");
-require("./TC_C46914");
-require("./TC_C46908");
+// require("./TC_C44421");
+// require("./TC_C46907");
+// require("./TC_C46914");
+// require("./TC_C45348");
+// require("./TC_C46912");
+// require("./TC_C46915");
+// require("./TC_C46908");
