868fb2e7c88428d96b34a6ddb723f9628457c1b2
paresh <paresh.vilas.more@celigo.com>
2023-09-06 09:21:05 +0530

    updated spec

--- __tests__/e2e_flows/FTP-SF/FTP-SF_Flows.spec.ts
+++ __tests__/e2e_flows/FTP-SF/FTP-SF_Flows.spec.ts
@@ -1,5 +1,4 @@
 require("./TC_247_FTP_Xml_Export_To_SF_Import")
-require("./TC_327_FTP_SF_Dynamic_Hardcode")
 require("./TC_024_Salesforce_Account_To_FTP_CSV_All")
 require("./TC28689_FTP_Compression_Algorithm")
 require("./TC_C2810_Verify_Ftp_import_CSV_without_headers")
