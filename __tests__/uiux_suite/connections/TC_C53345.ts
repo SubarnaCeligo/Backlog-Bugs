e1f2c58a8e213a87529008204528c5709b29f8c5
ashrithyalala <y.ashrith22@gmail.com>
2023-09-10 22:20:39 +0530

    Test Cases Fix

--- __tests__/uiux_suite/connections/TC_C53345.ts
+++ __tests__/uiux_suite/connections/TC_C53345.ts
@@ -1,17 +1,18 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe("Verify Base URI is not present in UPS Edit Connection page", () => {
+test.describe("C53345 Verify Base URI is not present in UPS Edit Connection page", () => {
 
     test.beforeEach(async ({ io }) => {
-        await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.homePage.navigateTo(io.data.links.CONNECTIONS_PAGE_URL);
       });
 
-      test ("Verify Base URI is not present in UPS Edit Connection page", async ({io,page}) => {
-        await io.homePage.clickByText('Resources');
-        await io.connectionPage.clickByText('Connections')
-        await io.connectionPage.clickByText('Create connection')
-        await io.connectionPage.click(selectors.connectionsPagePO.UPS_CONNECTION)
+      test ("C53345 Verify Base URI is not present in UPS Edit Connection page", async ({io,page}) => {
+        await io.connectionPage.clickByText('Create connection');
+        await page.waitForTimeout(5000);
+        await io.connectionPage.fill(selectors.connectionsPagePO.APP_NAME_INPUT,"UPS");
+        await page.locator(selectors.connectionsPagePO.UPS_CONNECTION).scrollIntoViewIfNeeded();
+        await io.connectionPage.click(selectors.connectionsPagePO.UPS_CONNECTION);
  
         const fieldSelector = selectors.connectionsPagePO.MOCK_BASEURL; // Replace with the actual selector
       
