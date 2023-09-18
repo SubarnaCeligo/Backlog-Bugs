7d6823d589c482e15b65de7e2dba587fa97fd465
Harshita <harshita.rai@celigo.com>
2023-09-18 11:16:53 +0530

    initial commit

---
+++ __tests__/uiux_suite/scriptDebugger/TC_C93819.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import data1 from "@testData/ScriptDebugger/C93819.json"
+
+
+test.describe('C93819 Validate user is able to create "contentBasedFlowRouter" script through flow builder page', () => {
+   
+    test('C93819 Validate user is able to create "contentBasedFlowRouter" script through flow builder page', async({io,page}) => {
+  
+      const id =  await io.fillFormUI(
+         data1,
+        'FLOWS'
+      );
+  
+      await io.flowBuilder.click(selectors.exportsPagePO.EXPORT_ADDPROCESSOR_BUTTON)
+      await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXPORT_HOOK)
+      await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT_HOOK)
+      await io.flowBuilder.click(selectors.basePagePO.CREATE_SCRIPT_ARIA_LABEL);
+      await io.flowBuilder.fill(selectors.importPagePO.NAME, "mockscript");
+      await io.flowBuilder.click(selectors.basePagePO.FUNCTION_STUB);   
+      await io.flowBuilder.selectTextfromDropDown(page, "contentBasedFlowRouter");
+      await io.flowBuilder.clickByText("Save & close");
+  });
+  });
---
+++ __tests__/uiux_suite/scriptDebugger/TC_C93821.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import data1 from "@testData/ScriptDebugger/C93821.json"
+
+test.describe('C93821 Validate user is able to create "handleRequest" script through flow builder page', () => {
+   
+    test('C93821 Validate user is able to create "handleRequest" script through flow builder page', async({io,page}) => {
+  
+      const id =  await io.fillFormUI(
+         data1,
+        'FLOWS'
+      );
+  
+      await io.flowBuilder.click(selectors.exportsPagePO.EXPORT_ADDPROCESSOR_BUTTON)
+      await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXPORT_HOOK)
+      await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT_HOOK)
+      await io.flowBuilder.click(selectors.basePagePO.CREATE_SCRIPT_ARIA_LABEL);
+      await io.flowBuilder.fill(selectors.importPagePO.NAME, "mockscript");
+      await io.flowBuilder.click(selectors.basePagePO.FUNCTION_STUB);   
+      await io.flowBuilder.selectTextfromDropDown(page, "handleRequest");
+      await io.flowBuilder.clickByText("Save & close");
+  });
+  });
