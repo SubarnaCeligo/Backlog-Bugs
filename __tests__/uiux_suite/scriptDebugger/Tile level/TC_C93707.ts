cb51fd71ace69afd33e37ae182bce6425a57ce6c
Harshita <harshita.rai@celigo.com>
2023-09-25 15:11:32 +0530

    initial commit

---
+++ __tests__/uiux_suite/scriptDebugger/Tile level/TC_C93707.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe('C93707 Validate that user is able to see "postResponseMap" function wherever “Insert function stub” field is present.(Tile level access)', () => {
+   
+    test.beforeEach(async ({ io }) => {
+      await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    });
+    test('C93707 Validate that user is able to see "postResponseMap" function wherever “Insert function stub” field is present.(Tile level access)', async ({
+      io,
+      page
+    }) => {
+     const isResourceVisible = await io.homePage.isVisible(selectors.basePagePO.RESOURCES);
+        if(isResourceVisible){
+            await page.hover(selectors.basePagePO.RESOURCES);
+            const isScriptsVisible = await io.homePage.isVisible('[data-test="Scripts"]');
+            await io.assert.expectToBeValue('false', isScriptsVisible.toString(), 'Scripts page is visible');
+        }else{
+            await io.assert.expectToBeValue('false', isResourceVisible.toString(), 'Resource page is visible');
+        }
+    });
+  });
+  
