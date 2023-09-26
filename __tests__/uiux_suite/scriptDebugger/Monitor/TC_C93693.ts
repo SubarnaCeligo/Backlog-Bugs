5fcf30912dc5cec598cb383351824458041347e7
prajwalsharma2000 <prajwal.sharma@celigo.com>
2023-09-26 16:20:38 +0530

    fix

--- __tests__/uiux_suite/scriptDebugger/Monitor/TC_C93693.ts
+++ __tests__/uiux_suite/scriptDebugger/Monitor/TC_C93693.ts
@@ -8,7 +8,7 @@ test.describe('C93693 Validate that user is not able to see "formInit" function 
     const isResourceVisible = await io.homePage.isVisible(selectors.basePagePO.RESOURCES);
     if(isResourceVisible){
         await page.hover(selectors.basePagePO.RESOURCES);
-        const isScriptsVisible = await io.homePage.isVisible('[data-test="Scripts"]');
+        const isScriptsVisible = await io.homePage.isVisible(selectors.basePagePO.SCRIPTS);
         await io.assert.expectToBeValue('false', isScriptsVisible.toString(), 'Scripts page is visible');
     }else{
         await io.assert.expectToBeValue('false', isResourceVisible.toString(), 'Resource page is visible');
