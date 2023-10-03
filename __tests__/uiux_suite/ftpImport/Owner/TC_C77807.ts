c9b173a419ee639756f1e11a173fb3ab838367e2
Harshita <harshita.rai@celigo.com>
2023-10-03 12:39:17 +0530

    test cases added

---
+++ __tests__/uiux_suite/ftpImport/Owner/TC_C77807.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import data1 from "@testData/Flows/C77807.json"
+
+test.describe(`C77807 Validate User is not seeing the vertical line when download retry data option is not available (Owner account)`, () => {
+    test(`C77807 Validate User is not seeing the vertical line when download retry data option is not available (Owner account)`, async ({
+      io,
+      page
+    }) => {
+        const id =  await io.fillFormUI(
+            data1,
+           'FLOWS'
+         );
+         await io.api.runBatchFlowViaAPI('C77807', id);
+         const lastRun = page.getByText('Last run')
+         await lastRun.waitFor({state: 'visible', timeout: 180000});
+         
+         await io.flowBuilder.clickByTextByIndex("1 error", 1);
+         const element = await io.flowBuilder.isVisible(selectors.basePagePO.MENU_ITEM)
+        await io.assert.expectToBeValue(element.toString(), "false", "Element is not present")
+          
+    });
+  });
---
+++ __tests__/uiux_suite/ftpImport/Tile/TC_C77839.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import data from "@testData/FtpImport/C77839.json"
+
+
+test.describe(`C77831 Validate User is not seeing the vertical line when download retry data option is not available (Tile level access account)`, () => {
+    test(`C77831 Validate User is not seeing the vertical line when download retry data option is not available (Tile level access account)`, async ({
+      io,
+      page
+    }) => {
+        const id =  await io.fillFormUI(
+            data,
+           'FLOWS'
+         );
+         await io.api.runBatchFlowViaAPI('C77839', id);
+         const lastRun = page.getByText('Last run')
+         await lastRun.waitFor({state: 'visible', timeout: 180000});
+         
+         await io.flowBuilder.clickByTextByIndex("1 error", 1);
+        const element = await io.flowBuilder.isVisible(selectors.basePagePO.MENU_ITEM)
+        await io.assert.expectToBeValue(element.toString(), "false", "Element is not present")
+          
+    });
+  });
+
