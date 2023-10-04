1fd7f169f33afd279fa17942dff41e713dedf06f
ashrithyalala <y.ashrith22@gmail.com>
2023-10-04 11:35:17 +0530

    Folder Update 1

--- __tests__/uiux_suite/EM2.0/TC_C51653.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51653.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import testData from "@testData/EM2.0/C51653.json";
+
 
 test.describe(`C51653 Verify the height and the veritical scroll bar of the message coloumn in the "Error rows" panel`, () => {
   test(`C51653 Verify the height and the veritical scroll bar of the message coloumn in the "Error rows" panel`, async ({
@@ -10,16 +10,16 @@ test.describe(`C51653 Verify the height and the veritical scroll bar of the mess
     // await io.flowBuilder.navigateTo(process.env["IO_UI_CONNECTOR_URL"]+"home");
     // await io.flowBuilder.clickByText("Automation Flows");
     // await io.flowBuilder.clickByText("mytestflow");
-    const id = await io.fillFormUI(testData.flow, "FLOWS");
+    const id = await io.fillFormUI("testData.flow", "FLOWS");
     await io.api.runBatchFlowViaAPI("C51653", id);
     const lastRun = page.getByText("Last run");
     await lastRun.waitFor({ state: "visible" });
     await page.getByText("1 error").nth(1).click();
-    const errorMessageDiv = page.getByText(testData.errorText);
+    const errorMessageDiv = page.getByText("testData.errorText");
     const heightBefore = (await errorMessageDiv.boundingBox()).height;
     await errorMessageDiv.evaluate((e, newText) => {
       e.textContent = newText;
-    }, testData.newText);
+    }, "testData.newText");
     const heightAfter = (await errorMessageDiv.boundingBox()).height;
     expect(heightAfter).toBeGreaterThan(heightBefore);
   });
