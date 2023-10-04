775d5afe6dd0cef77fd57fca2297ecef6de23dcf
ashrithyalala <y.ashrith22@gmail.com>
2023-10-04 11:16:40 +0530

    File Update-1

--- __tests__/uiux_suite/EM2.0/TC_C51630.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51630.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import flowJSON from "@testData/EM2.0/C51630.json";
+
 
 test.describe(`C51630 Verify the "Previous" & "Next" options in the "Error details" drawer`, () => {
   test(`C51630 Verify the "Previous" & "Next" options in the "Error details" drawer`, async ({
@@ -10,7 +10,7 @@ test.describe(`C51630 Verify the "Previous" & "Next" options in the "Error detai
     // await io.flowBuilder.navigateTo(process.env["IO_UI_CONNECTOR_URL"]+"home");
     // await io.flowBuilder.clickByText("Automation Flows");
     // await io.flowBuilder.clickByText("mytestflow");
-    const id = await io.fillFormUI(flowJSON, "FLOWS");
+    const id = await io.fillFormUI("flowJSON", "FLOWS");
     await io.api.runBatchFlowViaAPI("C51630", id);
     const lastRun = page.getByText("Last run");
     await lastRun.waitFor({ state: "visible" });
