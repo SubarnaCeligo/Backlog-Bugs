c92a2a0f92a004dc5498ee84ea82c212ad616ded
ashrithyalala <y.ashrith22@gmail.com>
2023-10-04 11:46:23 +0530

    Folder Update-5

--- __tests__/uiux_suite/EM2.0/TC_C51669.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51669.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import flowJSON from "@testData/EM2.0/C51669.json";
+
 
 test.describe(`C51669 Verify by closing the "Error details" drawer without saving the changes made on "Edit Retry data"tab`, () => {
   test(`C51669 Verify by closing the "Error details" drawer without saving the changes made on "Edit Retry data"tab`, async ({
@@ -10,7 +10,7 @@ test.describe(`C51669 Verify by closing the "Error details" drawer without savin
     // await io.flowBuilder.navigateTo(process.env["IO_UI_CONNECTOR_URL"]+"home");
     // await io.flowBuilder.clickByText("Automation Flows");
     // await io.flowBuilder.clickByText("C51669");
-    const id = await io.fillFormUI(flowJSON, "FLOWS");
+    const id = await io.fillFormUI("flowJSON", "FLOWS");
     await io.api.runBatchFlowViaAPI("C51669", id);
     const lastRun = page.getByText("Last run");
     await lastRun.waitFor({ state: "visible" });
