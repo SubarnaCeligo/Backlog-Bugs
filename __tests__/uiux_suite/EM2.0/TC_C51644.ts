559b58379dfa915aed3242d15ee581a663f9ae62
ashrithyalala <y.ashrith22@gmail.com>
2023-10-04 11:32:16 +0530

    Folder Update

--- __tests__/uiux_suite/EM2.0/TC_C51644.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51644.ts
@@ -1,6 +1,5 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import flowJSON from "@testData/EM2.0/C51644.json";
 
 test.describe(`C51644 Verify the error rows table header fields displayed in the New View`, () => {
   test(`C51644 Verify the error rows table header fields displayed in the New View`, async ({
@@ -10,7 +9,7 @@ test.describe(`C51644 Verify the error rows table header fields displayed in the
     // await io.flowBuilder.navigateTo(process.env["IO_UI_CONNECTOR_URL"]+"home");
     // await io.flowBuilder.clickByText("Automation Flows");
     // await io.flowBuilder.clickByText("mytestflow");
-    const id = await io.fillFormUI(flowJSON, "FLOWS");
+    const id = await io.fillFormUI("flowJSON", "FLOWS");
     await io.api.runBatchFlowViaAPI("C51644", id);
     const lastRun = page.getByText("Last run");
     await lastRun.waitFor({ state: "visible" });
