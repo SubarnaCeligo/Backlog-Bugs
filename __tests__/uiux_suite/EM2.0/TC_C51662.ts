c1e93874c6f0527b160c9689750f4d4dc1b2bd29
ashrithyalala <y.ashrith22@gmail.com>
2023-10-04 11:45:39 +0530

    Folder Update-4

--- __tests__/uiux_suite/EM2.0/TC_C51662.ts
+++ __tests__/uiux_suite/EM2.0/TC_C51662.ts
@@ -1,12 +1,13 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
+import testData from "@testData/EM2.0/C51662.json";
 
 test.describe(`C51662 Verify the default view of the Error details drawer`, () => {
   test(`C51662 Verify the default view of the Error details drawer`, async ({
     io,
     page
   }) => {
-    const id = await io.fillFormUI("testData", "FLOWS");
+    const id = await io.fillFormUI(testData, "FLOWS");
     await io.api.runBatchFlowViaAPI("C51662", id);
     const lastRun = page.getByText("Last run");
     await lastRun.waitFor({ state: "visible" });
