c20b9debd1a54cf5a0b8abe3af0b3b2f1285d6c6
ShivaPotlapelli <spshiva71@gmail.com>
2023-10-11 18:01:16 +0530

    both versions

--- __tests__/e2e_flows/FTP-NS/FTP-NS_Flows.spec.ts
+++ __tests__/e2e_flows/FTP-NS/FTP-NS_Flows.spec.ts
@@ -1,39 +1,20 @@
-import { API } from "@celigo/ui-core-automation";
-
-async function fetchFlakyCases() {
-  const PI = new API();
-  return PI.getFlakyTestcases("/platform_suites/widget/25");
-}
-const testCases = [
-  "TC_311_Multifilter_AND_OR_Operator_equals_notequals_greaterthan",
-  "TC_313_Multifilter_GROUP_NOT_Operator_equals_notequals",
-  "TC_314_Filter_Rows_Data",
-  "TC_316_FTPExport_GroupedData_InputFilter",
-  "TC_317_InputFilter_With_Rows_Data_With_NS_Export",
-  "TC_318_InputFilter_With_Record_Data_With_NS_Export",
-  "TC_487_FTP_SortByFields_Descending_To_FTP_Import",
-  "TC_488_FTP_SortByFields_Ascending_To_FTP_Import",
-  "TC_489_FTP_Without_SortAndGroup_To_FTP_Import",
-  "TC_490_FTP_GroupByFields_To_FTP_Import",
-  "TC_491_FTP_SortAndGroup_To_FTP_Import",
-  "TC_492_FTP_SortAndGroup_NestedJson_To_FTP_Import",
-  "TC_493_FTP_To_FTP_Import_With_Skip_Aggregation_True",
-  "TC_701_FTP_TO_FTPimport_Dir_path_as_handlebar",
-  "TC_708_Skip_Row_Validation",
-  "TC_710_FTP_Export_Encoding_UTF8",
-  "TC_711_FTP_Export_Encoding_WIN1252",
-  "TC_C20794_FTP_Huge_Header_Export_And_Import",
-  "TC_C37478_FTP_InvalidHandlebarErrorValidation",
-  "TC_043_Create_81_Sftp_XlSX_To_Netsuite_Customer_Using_FileType_XLSX"
-];
-
-async function runTestCases() {
-  const flakycases = await fetchFlakyCases();
-  for (const testCase of testCases) {
-    if (!flakycases.includes(testCase)) {
-      require(`./${testCase}`);
-    }
-  }
-}
-
-runTestCases();
+require("./TC_311_Multifilter_AND_OR_Operator_equals_notequals_greaterthan")
+require("./TC_313_Multifilter_GROUP_NOT_Operator_equals_notequals")
+require("./TC_314_Filter_Rows_Data")
+require("./TC_316_FTPExport_GroupedData_InputFilter")
+require("./TC_317_InputFilter_With_Rows_Data_With_NS_Export")
+require("./TC_318_InputFilter_With_Record_Data_With_NS_Export")
+require("./TC_487_FTP_SortByFields_Descending_To_FTP_Import")
+require("./TC_488_FTP_SortByFields_Ascending_To_FTP_Import")
+require("./TC_489_FTP_Without_SortAndGroup_To_FTP_Import")
+require("./TC_490_FTP_GroupByFields_To_FTP_Import")
+require("./TC_491_FTP_SortAndGroup_To_FTP_Import")
+require("./TC_492_FTP_SortAndGroup_NestedJson_To_FTP_Import")
+require("./TC_493_FTP_To_FTP_Import_With_Skip_Aggregation_True")
+require("./TC_701_FTP_TO_FTPimport_Dir_path_as_handlebar")
+require("./TC_708_Skip_Row_Validation")
+require("./TC_710_FTP_Export_Encoding_UTF8")
+require("./TC_711_FTP_Export_Encoding_WIN1252")
+require("./TC_C20794_FTP_Huge_Header_Export_And_Import")
+require("./TC_C37478_FTP_InvalidHandlebarErrorValidation")
+require("./TC_043_Create_81_Sftp_XlSX_To_Netsuite_Customer_Using_FileType_XLSX")
