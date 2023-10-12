1d3f53ae06944c71e71b9dc01ef229211916bdd5
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2023-10-12 15:37:54 +0530

    Removed duplicate spec file

--- __tests__/connections/Create/create_connections.spec.ts
+++
@@ -1,4 +0,0 @@
-require("./TC_011_Create_Connection_REST_Token")
-require("./TC_017_Create_Connection_HTTP_Basic_JSON_Zendesk")
-require("./TC_029_Create_Connection_Slack")
-require("./TC_040_Create_Connection_AS2")
