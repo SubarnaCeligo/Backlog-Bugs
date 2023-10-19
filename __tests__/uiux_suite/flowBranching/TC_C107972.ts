d6f47450c17ddd3bf628fdf86c24a68316cd3587
BHAVANI VUPPALA <bhavani.vuppala@celigo.com>
2023-10-19 16:29:48 +0530

    [IO-45242] edit flow branching

--- __tests__/uiux_suite/flowBranching/TC_C107972.ts
+++ __tests__/uiux_suite/flowBranching/TC_C107972.ts
@@ -12,10 +12,10 @@ test.describe("C107972 Showing the 'Save the changes' pop up when we are seeing 
       await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
       await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EDIT_BRANCHING);
       await io.flowBuilder.click(selectors.flowBuilderPagePO.EDIT_BRANCHING);
+      await page.locator(selectors.flowBuilderPagePO.BRANCH_TOGGLE).first().click();
       await io.assert.verifyElementAttributeContainsText(selectors.basePagePO.SAVE, 'class', 'Mui-disabled');
       await io.assert.verifyElementIsDisplayed(selectors.basePagePO.CLOSE, 'Close button not displayed');
       const isSaveAndCloseVisible = await io.flowBuilder.isVisible(selectors.basePagePO.SAVE_AND_CLOSE);
       await io.assert.expectToBeValue("false", isSaveAndCloseVisible.toString(), 'Save and Close is displayed');
-      
     });
   });
