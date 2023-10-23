5a0dc752fbf416b90fc726c845f12027fce834a1
ladimanish <ladi.manish@celigo.com>
2023-10-23 12:45:22 +0530

    test cases

--- __tests__/uiux_suite/flowDebugger/flowDebugger.spec.ts
+++ __tests__/uiux_suite/flowDebugger/flowDebugger.spec.ts
@@ -3,3 +3,4 @@ require('./TC_C55445');
 require('./TC_C55447');
 require('./TC_C55446');
 require('./TC_C96303');
+require('./IO-41074');
---
+++ __tests__/uiux_suite/general_page/IO-46625.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("IO-46625", () => {
+  test("IO-46625", async ({ io, page }) => {
+    // C107958 C107959 C107960	C107961	C107962
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.TOOLS);
+    await io.homePage.goToMenu("Tools", "Playground");
+    await io.flowBuilder.waitForElementAttached(
+      selectors.playgroundPO.HANDLEBARS_EDITOR
+    );
+    await io.flowBuilder.click(selectors.playgroundPO.HANDLEBARS_EDITOR);
+    await io.flowBuilder.click(selectors.playgroundPO.NESTED_JSON);
+    await io.flowBuilder.click(selectors.playgroundPO.LAYOUT_TOGGLE);
+    await io.assert.verifyElementIsDisplayed(
+      selectors.playgroundPO.SELECTED_COLUMN_VIEW,
+      "Default layout is not column view"
+    );
+
+    await io.flowBuilder.click(selectors.playgroundPO.SELECTED_COLUMN_VIEW);
+    await io.flowBuilder.click(selectors.playgroundPO.SQL_QUERY_BUILDER);
+    await io.flowBuilder.click(selectors.playgroundPO.SIMPLE_SQL_QUERY);
+    await io.flowBuilder.click(selectors.playgroundPO.LAYOUT_TOGGLE);
+    await io.assert.verifyElementIsDisplayed(
+      selectors.playgroundPO.SELECTED_COMPACT_VIEW,
+      "Default layout is not column view"
+    );
+  });
+});
