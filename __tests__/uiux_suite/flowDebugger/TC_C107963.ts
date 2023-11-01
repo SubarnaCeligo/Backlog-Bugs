7ebb6db30481ef00aa9de49495787047efffb50e
ShritiSCeligoIO <shriti.b.s@celigo.com>
2023-11-01 19:26:19 +0530

    Updated the test cases to use "hover" method

--- __tests__/uiux_suite/flowDebugger/TC_C107963.ts
+++ __tests__/uiux_suite/flowDebugger/TC_C107963.ts
@@ -12,36 +12,19 @@ test.describe("Verify the hover text for disabled source in dropdown.", () => {
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.FLOW_TOGGLE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.FLOW_TOGGLE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.FLOW_DISABLE);
-
+    
     //Click on the Run Test chevron icon
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUN_SPECIFIC_SOURCE_DROPDOWN);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.RUN_SPECIFIC_SOURCE_DROPDOWN);
 
-    // Extract the X and Y coordinates from the handle
-    const runTestDropdownButton = await page.locator('ul> div> li> div > span > button[data-test="runTest"]').first();
-    const handle = await runTestDropdownButton.evaluateHandle(element => {
-      const rect = element.getBoundingClientRect();
-      return {
-        x: rect.x,
-        y: rect.y
-      };
-    });
-
-    const coordinates = await handle.jsonValue();
-    console.log('X coordinate:', coordinates.x);
-    console.log('Y coordinate:', coordinates.y);
-    await handle.dispose();
-
-    //Move the mouse on disabled button
-    await page.mouse.move(coordinates.x, coordinates.y);
-    await page.waitForTimeout(2000);
+    //Hover on the disabled button on Source Selection dropdown
+    await io.flowBuilder.hover(selectors.flowBuilderPagePO.RUNTEST_BUTTON, 2, true);
 
     //Get the hover text
-    const hoverText = await page.locator('[role="tooltip"] div div').allTextContents();
-
-    //Validate the hover text
-    const isHoverTextVisible = hoverText.includes('You can\'t select this source for a test run unless your source is either online or has mock output data. Any lookup steps present in the flow must also either be online or have mock output data.');
-    await io.assert.expectToBeTrue(isHoverTextVisible, "Hover Text did not appear on moving the mouse");
+    const hoverText = (await io.flowBuilder.getText(selectors.flowBuilderPagePO.RUNTEST_TOOLTIP)).toString();
     
+    //Validate the hover text
+    await io.assert.expectToContainValue('You can\'t select this source for a test run unless your source is either online or has mock output data. Any lookup steps present in the flow must also either be online or have mock output data.', hoverText, 'Hover Text did not appear' );
+  
   });
 });
--- __tests__/uiux_suite/flowDebugger/TC_C107965.ts
+++ __tests__/uiux_suite/flowDebugger/TC_C107965.ts
@@ -7,67 +7,34 @@ test.describe("Validate the hover text for disabled Test Run button (On top and 
   test("Validate the hover text for disabled Test Run button (On top and in the run console)", async ({ io, page }) => {
     //Create a flow with offline connections
     await io.createResourceFromAPI(C107965, "FLOWS");
-
+   
     //Disable the flow
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.FLOW_TOGGLE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.FLOW_TOGGLE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.FLOW_DISABLE);
-    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUNTEST_BUTTON_TOP_XPATH);
+    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUNTEST_BUTTON);
 
-    //Run button on top
-    // Extract the X and Y coordinates from the handle
-    const runTrunTestButtonOnTop = page.locator(selectors.flowBuilderPagePO.RUNTEST_BUTTON_TOP_XPATH);
-    const handle = await runTrunTestButtonOnTop.evaluateHandle(element => {
-      const rect = element.getBoundingClientRect();
-      return {
-        x: rect.x,
-        y: rect.y
-      };
-    });
-
-    const coordinates_top = await handle.jsonValue();
-    console.log('X coordinate:', coordinates_top.x);
-    console.log('Y coordinate:', coordinates_top.y);
-    await handle.dispose();
-
-    //Move the mouse on the coordinates
-    await page.mouse.move(coordinates_top.x, coordinates_top.y);
-
+    //Hover on the disabled Run Test button on top
+    await io.flowBuilder.hover(selectors.flowBuilderPagePO.RUNTEST_BUTTON, 0, true);
+   
     //Get the hover text
-    const hoverText = await page.locator('[role="tooltip"] div div').first().innerText();
+    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUNTEST_TOOLTIP);
+    const hoverText = (await io.flowBuilder.getText(selectors.flowBuilderPagePO.RUNTEST_TOOLTIP)).toString();
     console.log(hoverText);
 
     //Validate the hover text
-    const isHoverTextVisible = (hoverText.includes('You can\'t use the currently selected source for a test run unless you either bring it online or enter mock output data.') && hoverText.includes('Any lookup steps present in the flow must also either be online or have mock output data.'));
-    await io.assert.expectToBeTrue(isHoverTextVisible, "Hover Text did not appear on moving the mouse");
-   
-    //Run Test button on run console
-    const runTrunTestButtonRunConole = page.locator(selectors.flowBuilderPagePO.RUNTEST_BUTTON_RUN_CONSOLE_XPATH);
-    const handleConsole = await runTrunTestButtonRunConole.evaluateHandle(element => {
-      const rect = element.getBoundingClientRect();
-      return {
-        x: rect.x,
-        y: rect.y
-      };
-    });
-
-    // Extract the X and Y coordinates from the handle
-    const coordinates_console = await handleConsole.jsonValue();
-    console.log('X coordinate:', coordinates_console.x);
-    console.log('Y coordinate:', coordinates_console.y);
-    await handleConsole.dispose();
-
-
-    //Move the mouse on the coordinates
-    await page.mouse.move(coordinates_console.x, coordinates_console.y);
+    await io.assert.expectToContainValue('You can\'t use the currently selected source for a test run unless you either bring it online or enter mock output data. Any lookup steps present in the flow must also either be online or have mock output data.', hoverText, 'Hover text did not appear');
+    
+    //Hover on the disabled Run Test button on run console
+    await io.flowBuilder.hover(selectors.flowBuilderPagePO.RUNTEST_BUTTON, 1, true);
 
     //Get the hover text
-    const hoverTextConsole = await page.locator('[role="tooltip"] div div').first().innerText();
-    console.log(hoverTextConsole);
-
+    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUNTEST_TOOLTIP);
+    const hoverTextConsole = (await io.flowBuilder.getText(selectors.flowBuilderPagePO.RUNTEST_TOOLTIP)).toString();
+    
     //Validate the hover text
-    const isHoverTextVisibleOnConsole = (hoverTextConsole.includes('You can\'t use the currently selected source for a test run unless you either bring it online or enter mock output data.') && hoverTextConsole.includes('Any lookup steps present in the flow must also either be online or have mock output data.'));
-    await io.assert.expectToBeTrue(isHoverTextVisibleOnConsole, "Hover Text did not appear on moving the mouse");
+    await io.assert.expectToContainValue('You can\'t use the currently selected source for a test run unless you either bring it online or enter mock output data. Any lookup steps present in the flow must also either be online or have mock output data.', hoverTextConsole, 'Hover text did not appear');
+    
 
   });
 
