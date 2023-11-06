a363ea8c75dd81fd852245bfe148534ffad5d049
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2023-11-06 18:18:27 +0530

    Fixes

--- __tests__/uiux_suite/flowBuilder/TC_C106452.ts
+++ __tests__/uiux_suite/flowBuilder/TC_C106452.ts
@@ -4,34 +4,28 @@ import C106452 from '../../../testData/inputData/Flows/C106452.json';
 
 test.describe("C106452_C106453 Verified new icon(Bubble close icon)", () => {
     test("C106452_C106453 Verified new icon(Bubble close icon)", async ({ io, page }) => {
-        await io.fillFormUI(C106452, 'FLOWS');
+        const id = await io.createResourceFromAPI(C106452, "FLOWS");
         await io.homePage.addStep('Hovering over "X" icon in header');
-        await page
-            .locator(selectors.flowBuilderPagePO.CLOSE_FLOW_BUILDER).hover();
-        await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.EM2dot0PO.COMPLETED_ROWS_HOVER);
+        await io.homePage.hover(selectors.flowBuilderPagePO.CLOSE_FLOW_BUILDER);
+        await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.EM2DOT0PO.COMPLETED_ROWS_HOVER);
         var hoverText = await io.connectionPage.getText(
-            selectors.flowBuilderPagePO.EM2dot0PO.COMPLETED_ROWS_HOVER
+            selectors.flowBuilderPagePO.EM2DOT0PO.COMPLETED_ROWS_HOVER
         );
-        await expect(hoverText).toBe(
-            "Exit flow & return to all integration flows"
-        );
+        await io.assert.expectToBeValue("Exit flow & return to all integration flows", hoverText.toString(), "Element not found");
         await io.homePage.addStep('Verified hover text for top "x" icon');
 
         await io.homePage.addStep('Hovering over "X" icon on bubble');
-        await page
-        .locator(selectors.flowBuilderPagePO.REMOVE_PAGE_PROCESSOR).hover();
-        await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.EM2dot0PO.COMPLETED_ROWS_HOVER);
-        await io.flowBuilder.delay(1000);
+        await io.homePage.hover(selectors.flowBuilderPagePO.REMOVE_PAGE_PROCESSOR);
+        await io.flowBuilder.delay(500);
+        await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.EM2DOT0PO.COMPLETED_ROWS_HOVER);
         var hoverText1 = await io.connectionPage.getText(
-            selectors.flowBuilderPagePO.EM2dot0PO.COMPLETED_ROWS_HOVER
+            selectors.flowBuilderPagePO.EM2DOT0PO.COMPLETED_ROWS_HOVER
         );
-        await expect(hoverText1).toBe(
-            "Remove resource from flow"
-        );
+        await io.assert.expectToBeValue("Remove resource from flow", hoverText1.toString(), "Element not found");
         await io.homePage.click(selectors.flowBuilderPagePO.REMOVE_PAGE_GENERATOR);
         await io.homePage.addStep('Verifying pop up to appear');
         await io.assert.verifyElementIsDisplayed(selectors.myAccountPagePO.DIALOG_BOX, 'Pop up did not appear');
-        await io.homePage.click(selectors.mappings.Mapper2dot0PO.CLOSEBUTTON);
+        await io.homePage.click(selectors.mappings.MAPPER2DOT0PO.CLOSEBUTTON);
         await io.homePage.addStep('Clicking on cross icon to remove import');
         await io.homePage.click(selectors.flowBuilderPagePO.REMOVE_PAGE_PROCESSOR);
         await io.homePage.addStep('Verifying pop up to appear');
--- __tests__/uiux_suite/flows/TC_C106448.ts
+++ __tests__/uiux_suite/flows/TC_C106448.ts
@@ -4,7 +4,7 @@ import testData from "@testData/Flows/C106448.json";
 
 test.describe(`C106448  Verify Parsed output label`, () => {
   test(`C106448 Verify Parsed output label`, async ({ io, page }) => {
-    await io.fillFormUI(testData, "FLOWS");
+    const id = await io.createResourceFromAPI(testData, "FLOWS");
     //Salesforce
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXPORT);
     await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.EXPORT, 0);
@@ -12,7 +12,7 @@ test.describe(`C106448  Verify Parsed output label`, () => {
     await expect(await page.locator(selectors.importPagePO.PARSED_OUTPUT)).not.toHaveCSS("cursor", "pointer");
     await io.assert.verifyElementAttributeContainsText(selectors.importPagePO.PARSED_OUTPUT, 'class', 'Mui-disabled');
     await io.flowBuilder.click(selectors.flowBuilderPagePO.CLOSE);
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.CANVAS_FIT_TO_SCREEN);
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.FITVIEW);
     await io.flowBuilder.addStep("Verified Parsed output label for Salesforce adaptor");
 
     //MongoDB
