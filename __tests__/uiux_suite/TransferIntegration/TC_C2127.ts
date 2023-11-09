150e3805b94b291b05451572649292616ad411c8
Harshita Rai <harshita.rai@celigo.com>
2023-11-09 13:59:36 +0530

    test cases added

---
+++ __tests__/uiux_suite/TransferIntegration/TC_C2127.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import testData from "./transfer.json";
+
+ 
+
+test.describe(`C2127 Verify Transfer list must be updated with lastmodified at top for done,dismiss and cancel actions`, () => {
+  test(`C2127 Verify Transfer list must be updated with lastmodified at top for done,dismiss and cancel actions.`, async ({
+    page,
+    io
+  }) => {
+     
+    const res = await io.api.postCall(
+      `v1/transfers/invite`,
+      testData
+    );
+    console.log(res)
+
+    await io.api.putCall(
+        `v1/transfers/${res._id}/cancel`,
+       res._id
+     );
+    await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
+    await io.myAccountPage.click(selectors.homePagePO.TRANSFER)
+    await page.waitForSelector('table');
+    const statusText = await page.$eval('table tr:nth-child(2) td:nth-child(4)', (cell) => cell.textContent);
+    await io.assert.expectToBeValue(statusText,"Canceled", "transfer is not dismissed")
+     
+  });
+});
