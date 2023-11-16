e86859363ba1896041e7eca1185b858937826513
Abhishek Angira <abhishek.angira@celigo.com>
2023-11-16 14:12:18 +0530

    Created flow

--- __tests__/uiux_suite/imports/TC_C30504.ts
+++ __tests__/uiux_suite/imports/TC_C30504.ts
@@ -1,20 +1,17 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
+import testData from "@testData/Connections/FTP_To_NS.json";
 
 test.describe(`C30504 Verify export fields are populated for NS subrecord import mappings`, () => {
   test(`C30504 Verify export fields are populated for NS subrecord import mappings`, async ({
     io,
     page
   }) => {
-    await io.homePage.navigateTo(process.env.IO_Integration_URL);
-    const testCase = page.getByText("FTP_To_NS_DND").first();
-    await testCase.waitFor({ state: "visible", timeout: 18000 });
-    await testCase.click();
+    await io.createResourceFromAPI(testData, "FLOWS");
     await page.getByLabel("Define options").nth(1).click();
     await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
-    await io.flowBuilder.clickByText(
-      "NS premium import - Items : Inventory Details (Subrecord)"
-    );
+    await page.getByText("(Subrecord)").click();
+    await io.flowBuilder.addStep("Clicked on subrecord");
     await io.flowBuilder.click(
       selectors.mappings.MAPPER1DOT0PO.DESTINATION_INPUT
     );
