b9101edfe9a49dfd0b3a7ef44d5ba206bc63d958
ShritiSCeligoIO <shriti.b.s@celigo.com>
2023-11-27 11:26:27 +0530

    formatting

--- __tests__/uiux_suite/flowDebugger/TC_C107968.ts
+++ __tests__/uiux_suite/flowDebugger/TC_C107968.ts
@@ -1,10 +1,10 @@
-import {expect, test} from "@celigo/ui-core-automation";
+import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import C107968 from '../../../testData/inputData/FlowDebugger/C107968.json';
 import C107968_MultipleSource from '../../../testData/inputData/FlowDebugger/C107968_MultipleSource.json';
 
 test.describe("Validate the Run confirmation dialog title when a flow step has offline connections", () => {
-    test("Validate the Run confirmation dialog title when a flow steps has offline connections", async ({io, page}) => {
+    test("Validate the Run confirmation dialog title when a flow steps has offline connections", async ({ io, page }) => {
         //Create a flow with offiline connection and wait for the integration to load
         await io.createResourceFromAPI(C107968, "FLOWS");
 
@@ -19,13 +19,13 @@ test.describe("Validate the Run confirmation dialog title when a flow step has o
         //Validate the dialog title
         const dialogTitle = (await io.flowBuilder.getText("h2")).toString();
         await io.assert.expectToContainValue('Confirm run with offline connections', dialogTitle, 'Dialog title is incorrect')
-    
+
         //Validate the dialog contents.
         const dialogContents = (await io.flowBuilder.getText("p")).toString();
         await io.assert.expectToContainValue('Are you sure you want to run this flow?,One or more steps have offline connections and will not be completed.,Bring your connections online to ensure completion of all steps.', dialogContents, 'Dialog contents are incorrect')
-    
+
     });
-    test("Bug#IO-48090-Validate the Run confirmation dialog from Run Source dropdown when a flow steps has offline connections", async ({io, page}) => {
+    test("Bug#IO-48090-Validate the Run confirmation dialog from Run Source dropdown when a flow steps has offline connections", async ({ io, page }) => {
         //Create a flow with offiline connection and wait for the integration to load
         await io.createResourceFromAPI(C107968_MultipleSource, "FLOWS");
 
@@ -44,10 +44,10 @@ test.describe("Validate the Run confirmation dialog title when a flow step has o
         //Validate the dialog title
         const dialogTitle = (await io.flowBuilder.getText("h2")).toString();
         await io.assert.expectToContainValue('Confirm run with offline connections', dialogTitle, 'Dialog title is incorrect')
-    
+
         //Validate the dialog contents.
         const dialogContents = (await io.flowBuilder.getText("p")).toString();
         await io.assert.expectToContainValue('Are you sure you want to run this flow?,One or more steps have offline connections and will not be completed.,Bring your connections online to ensure completion of all steps.', dialogContents, 'Dialog contents are incorrect')
-    
+
     });
 });
