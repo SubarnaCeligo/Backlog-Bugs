4e01eddd4b3ccea791b13ef747dffea841e270dc
ShritiSCeligoIO <shriti.b.s@celigo.com>
2023-12-21 16:16:27 +0530

    Added beforeEach

--- __tests__/uiux_suite/connections/TC_C63009.ts
+++ __tests__/uiux_suite/connections/TC_C63009.ts
@@ -1,8 +1,11 @@
-import {expect, test} from "@celigo/ui-core-automation";
+import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C63009 - Verify if API type and API version are displayed for the connection", () => {
-  test("C63009 - Verify if API type and API version are displayed for the connection", async ({io, page}) => {
+  test.beforeEach(async ({ io }) => {
+    await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+  });
+  test("C63009 - Verify if API type and API version are displayed for the connection", async ({ io, page }) => {
     //Navigate to Exports page and click on 'Create export'
     await io.homePage.navigateTo(io.data.links.EXPORTS_PAGE_URL);
     await io.exportsPage.click(selectors.basePagePO.ADD_NEW_RESOURCE);
@@ -21,8 +24,8 @@ test.describe("C63009 - Verify if API type and API version are displayed for the
 
     // Verify if API type and API version are displayed for the connection
     const connectionDetails = (await io.exportsPage.getText(selectors.exportsPagePO.CONNECTION_DROPDOWN_OPTIONS)).toString();
-    await io.assert.expectToContainValue('API type:', connectionDetails, 'API type is not displayed' );
-    await io.assert.expectToContainValue('API version:', connectionDetails, 'API version is not displayed' );
+    await io.assert.expectToContainValue('API type:', connectionDetails, 'API type is not displayed');
+    await io.assert.expectToContainValue('API version:', connectionDetails, 'API version is not displayed');
 
   });
 });
