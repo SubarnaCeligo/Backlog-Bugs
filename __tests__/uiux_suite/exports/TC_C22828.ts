7245edc032f8cf39b4c465728727f574f4df8d94
Sudhanshukumar28 <sudhanshu.kumar@celigo.com>
2023-12-27 20:13:24 +0530

    PR COMMENT

--- __tests__/uiux_suite/exports/TC_C22828.ts
+++ __tests__/uiux_suite/exports/TC_C22828.ts
@@ -10,20 +10,16 @@ test.describe("C22828 Verify Online or offline connection - to always show, and 
     //Flow Description on Integration page C115168 C115169
     await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.OPENAI.FLOW_DESCRIPTION_BUTTON, "Flow description is not displayed");
     await io.exportsPage.loadingTime();
-    let descButton = await page.locator(selectors.flowBuilderPagePO.CONNECTION_TABLE).first();
-    descButton.click();
+    // let descButton = await page.locator(selectors.flowBuilderPagePO.CONNECTION_TABLE).first();
+    // descButton.click();
+    await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.CONNECTION_TABLE, 0);
     await io.exportsPage.loadingTime();
     // Define selectors for both online and offline elements
     await io.exportsPage.waitForElementAttached(selectors.flowBuilderPagePO.CONNECTION_STATUS);
     // Get the value of the aria-label attribute
     const connectionStatus = await page.$eval(selectors.flowBuilderPagePO.CONNECTION_STATUS, (span) => span.getAttribute('aria-label'));  
+    let result = connectionStatus === 'error' || connectionStatus === 'success';
     // Determine the connection status based on the aria-label value
-    if (connectionStatus === 'error') {
-      await io.exportsPage.addStep("Connection is offline.");
-    } else if (connectionStatus === 'success') {
-      await io.exportsPage.addStep("Connection is online.");    } 
-      else {
-        await io.exportsPage.addStep("Status elements not found. Unable to determine online/offline status.");
-    }
+    await io.assert.expectToBeTrue(result , "Connection is not Online and Offline")
    })
 });       
