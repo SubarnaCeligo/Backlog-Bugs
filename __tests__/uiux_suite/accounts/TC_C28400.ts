051b5233a3822896c4dbac087bd4daa6ae27c2a6
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-01-02 14:02:29 +0530

    Commiting TC_C28400

---
+++ __tests__/uiux_suite/accounts/TC_C28400.ts
@@ -0,0 +1,37 @@
+import { test, expect, links } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe('C28400 Running flows tab -Verify the pagination is shown correctly - 50 rows in 1 page', () => {
+  test.beforeEach(async ({ io }) => {
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+  
+  
+      
+  });
+     
+    test('C25638 Running flows tab -Verify the pagination is shown correctly - 50 rows in 1 page', async ({
+      io,
+      page
+    }) => {
+      await io.homePage.click(selectors.homePagePO.SEARCH_INTEGRATION);
+      await io.homePage.fill(selectors.homePagePO.SEARCH_INTEGRATION,"50");
+      await io.homePage.clickByText("50Flows_TC_28400"); 
+      await io.homePage.loadingTime();
+      await page.pause();
+      let i =1;
+      while(i<51)
+      {
+        await io.homePage.clickButtonByIndex(selectors.flowBuilderPagePO.RUN_FLOW,i);
+        i++;
+        
+      }
+      await io.homePage.click(selectors.flowBuilderPagePO.DASHBOARD);
+      await io.assert.verifyElementDisplayedByText("1 - 50 of 50","Count is not expected");
+      
+      
+     
+       
+     
+      
+    });
+});
