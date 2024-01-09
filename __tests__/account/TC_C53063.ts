4ffe4aca0d8a85a2c16c17bcc2be1ff094d4fe81
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-01-09 11:56:08 +0530

    Added Json body in seperate file

--- __tests__/account/TC_C53063.ts
+++ __tests__/account/TC_C53063.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
+import ProfessionalLicense from '@testData/Flows/ProfessionalLicense.json';
 import { MyAccountPage } from "@celigo/ui-core-automation/dist/src/pageFactory/pages/MyAccountPage";
 
 test.describe('C53063 Verify that the 30 and 60 days option is enable for license types Professional tire', () => {
@@ -15,34 +16,7 @@ test.describe('C53063 Verify that the 30 and 60 days option is enable for licens
      
         const licenses = await io.api.getCall("v1/licenses");
         let  LID= "6595487f92df6f0af1f73f89"
-        let putcall =await io.api.putCall(`v1/test/licenses/${LID}`,{
-          "_id": LID,
-          "expires": "2021-01-01T00:00:00.000Z",
-          "type": "endpoint",
-          "tier": "professional",
-          "supportTier": "preferred",
-          "sandbox": true,
-          "numEndpoints": 100,
-          "numAddOnEndpoints": 0,
-          "numFlows": 10,
-          "numAddOnFlows": 0,
-          "numTradingPartners": 9,
-          "numAddOnTradingPartners": 0,
-          "numAgents": 0,
-          "numAddOnAgents": 0,
-          "numSandboxEndpoints": 100,
-          "numSandboxAddOnEndpoints": 10,
-          "numSandboxFlows": 0,
-          "numSandboxAddOnFlows": 0,
-          "numSandboxTradingPartners": 0,
-          "numSandboxAddOnTradingPartners": 0,
-          "numSandboxAgents": 0,
-          "numSandboxAddOnAgents": 0,
-          "apiManagement": true,
-          "resumable": false,
-          "sso": false,
-          "maxAllowedDataRetention": 60
-        });
+        let putcall =await io.api.putCall(`v1/test/licenses/${LID}`, ProfessionalLicense);
       
 
           await io.myAccountPage.click(selectors.myAccountPagePO.DATA_RETENTION);
