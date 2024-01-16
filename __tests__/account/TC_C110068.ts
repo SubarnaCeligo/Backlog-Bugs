ff7d25549c53e7159c86b6d5631627a7df12f712
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-01-16 09:56:14 +0530

    add fix

--- __tests__/account/TC_C110068.ts
+++ __tests__/account/TC_C110068.ts
@@ -26,17 +26,26 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     // sign out of IO and navigate to login page
     await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
     const isNotLoggedIn = await io.loginPage.checkLoginState();
-    if (!isNotLoggedIn) {
-      await io.homePage.waitForElementAttached(selectors.basePagePO.ACCOUNT);
-      await io.homePage.hover(selectors.basePagePO.ACCOUNT);
-      await io.homePage.click(selectors.basePagePO.SIGN_OUT);
-    }
-    // check for link in email and navigate to that link
     await page.waitForTimeout(5000);
     const link = await io.emailVal.getLinkFromEmail(
       "[qa.staging.integrator.io] Request to change your email", false, "pwqa1"
     );
     console.log("link: " + link.toString())
+    let AccountTab=".MuiToolbar-root .MuiSvgIcon-root"
+    let AccountTab1=await page.$$(".MuiToolbar-root .MuiSvgIcon-root")
+    await page.waitForTimeout(20000);
+    console.log("Account tab : " + AccountTab)
+    console.log("Account tab len : " + AccountTab.length)
+    let  last = await AccountTab1.length - 1;
+    console.log("last: " + last)
+    let x=AccountTab
+    if (!isNotLoggedIn) {
+      // await io.homePage.waitForElementAttached(AccountTab[last]);
+      await io.myAccountPage.clickByIndex(AccountTab, last);
+      await io.homePage.click(selectors.basePagePO.SIGN_OUT);
+    }
+    // check for link in email and navigate to that link
+                           
     await io.homePage.navigateTo(link.toString());
     await io.loginPage.fill(selectors.loginPagePO.EMAIL, "qaautomation1+emailcheck1@celigo.com");
     await io.loginPage.fill(selectors.loginPagePO.PASSWORD, decrypt(process.env["IO_Password"]));
@@ -44,14 +53,18 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
 
     //click on agent token and validate
     await io.homePage.goToMenu("Resources","Agents");
+    await io.homePage.loadingTime()
+    await io.homePage.loadingTime()
+    await io.homePage.loadingTime()
    let agentselect= '[data-test="displayAgentToken"]'
     await io.flowBuilder.click(agentselect);
-    await page.waitForTimeout(5000);
-    
-    const agentlink =await io.emailVal.getLinkFromEmail(
-      "ALERT: API token was displayed in clear text", false, "pwqa1"
+    await page.waitForTimeout(10000);
+        const agentlink = await io.emailVal.getLinkFromEmail(
+      "ALERT: Agent access token was displayed in clear text",true, "pwqa1"
     );
+console.log("agentlink: " + agentlink)
     let result=agentlink.toString();
+    console.log("result: " + result)
     let validate=false;
       if(result.includes('agent'))
       {
@@ -59,29 +72,50 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       }
       expect(validate).toBeTruthy();
 
-    //revert email to orginal
+    // //revert email to orginal
     await io.homePage.loadingTime()
+    // navigate to my account page
     await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
     await io.myAccountPage.click(selectors.basePagePO.EDIT_EMAIL_BUTTON)
+    // Click on edit email button
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_EMAIL)
     console.log("user name: " + process.env.IO_UserName)
-    await io.myAccountPage.fill(selectors.basePagePO.NEW_EMAIL, process.env["IO_Password"])
+    await io.myAccountPage.fill(selectors.basePagePO.NEW_EMAIL, "qaautomation1+emailcheck@celigo.com")
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_PASSWORD)
     await io.myAccountPage.fill(selectors.basePagePO.NEW_PASSWORD, decrypt(process.env["IO_Password"]));
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.CHANGE_EMAIL)
     await io.myAccountPage.click(selectors.basePagePO.CHANGE_EMAIL);
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.CLOSE_BUTTON)
     await io.myAccountPage.click(selectors.basePagePO.CLOSE_BUTTON);
-    // signin of IO and navigate to login page
 
-    
+
+    // // signin of IO and navigate to login page
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    const isNotLoggedIn1 = await io.loginPage.checkLoginState();
     await page.waitForTimeout(5000);
-    const link2 = await io.emailVal.getLinkFromEmail(
+    const link1 = await io.emailVal.getLinkFromEmail(
       "[qa.staging.integrator.io] Request to change your email", false, "pwqa1"
     );
-    console.log("link: " + link2.toString())
-    await io.homePage.navigateTo(link2.toString());
-    await io.loginPage.fill(selectors.loginPagePO.EMAIL, process.env.IO_UserName);
+    console.log("link: " + link1.toString())
+    let Account1Tab=".MuiToolbar-root .MuiSvgIcon-root"
+    let Account1Tab1=await page.$$(".MuiToolbar-root .MuiSvgIcon-root")
+    await page.waitForTimeout(20000);
+    console.log("Account tab : " + Account1Tab)
+    console.log("Account tab len : " + Account1Tab.length)
+    let  last1 = await Account1Tab1.length - 1;
+    console.log("last: " + last1)
+
+    if (!isNotLoggedIn1) {
+      // await io.homePage.waitForElementAttached(AccountTab[last]);
+      await io.myAccountPage.clickByIndex(Account1Tab, last1);
+      await io.homePage.click(selectors.basePagePO.SIGN_OUT);
+    }
+    // check for link in email and navigate to that link
+             
+    await io.homePage.navigateTo(link1.toString());
+    await page.waitForTimeout(10000);
+    await io.homePage.navigateTo(link1.toString());
+    await io.loginPage.fill(selectors.loginPagePO.EMAIL, "qaautomation1+emailcheck@celigo.com");
     await io.loginPage.fill(selectors.loginPagePO.PASSWORD, decrypt(process.env["IO_Password"]));
     await io.loginPage.click(selectors.basePagePO.SUBMIT);
 
