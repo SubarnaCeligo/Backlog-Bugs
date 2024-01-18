1b9c8d75d6dda8253da5a47b1f7f3c0aaa5314e4
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-01-18 13:43:56 +0530

    Backlog automation

--- __tests__/account/TC_C110068.ts
+++ __tests__/account/TC_C110068.ts
@@ -15,7 +15,6 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     await io.myAccountPage.click(selectors.basePagePO.EDIT_EMAIL_BUTTON)
     // Click on edit email button
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_EMAIL)
-    // console.log("user name: " + process.env.IO_UserName)
     await io.myAccountPage.fill(selectors.basePagePO.NEW_EMAIL, "qaautomation1+emailcheck1@celigo.com")
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_PASSWORD)
     await io.myAccountPage.fill(selectors.basePagePO.NEW_PASSWORD, decrypt(process.env["IO_Password"]));
@@ -30,14 +29,10 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     const link = await io.emailVal.getLinkFromEmail(
       "[qa.staging.integrator.io] Request to change your email", false, "pwqa1"
     );
-    // console.log("link: " + link.toString())
     let AccountTab=".MuiToolbar-root .MuiSvgIcon-root"
     let AccountTab1=await page.$$(".MuiToolbar-root .MuiSvgIcon-root")
     await page.waitForTimeout(20000);
-    // console.log("Account tab : " + AccountTab)
-    // console.log("Account tab len : " + AccountTab.length)
     let  last = await AccountTab1.length - 1;
-    // console.log("last: " + last)
     let x=AccountTab
     if (!isNotLoggedIn) {
       // await io.homePage.waitForElementAttached(AccountTab[last]);
@@ -64,9 +59,7 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
         const agentlink = await io.emailVal.getLinkFromEmail(
       "ALERT: Agent access token was displayed in clear text",true, "pwqa1"
     );
-// console.log("agentlink: " + agentlink)
     let result=agentlink.toString();
-    // console.log("result: " + result)
     let validate=false;
       if(result.includes('agent'))
       {
@@ -81,7 +74,6 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     await io.myAccountPage.click(selectors.basePagePO.EDIT_EMAIL_BUTTON)
     // Click on edit email button
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_EMAIL)
-    // console.log("user name: " + process.env.IO_UserName)
     await io.myAccountPage.fill(selectors.basePagePO.NEW_EMAIL, "qaautomation1+emailcheck@celigo.com")
     await io.myAccountPage.waitForElementAttached(selectors.basePagePO.NEW_PASSWORD)
     await io.myAccountPage.fill(selectors.basePagePO.NEW_PASSWORD, decrypt(process.env["IO_Password"]));
@@ -98,14 +90,10 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     const link1 = await io.emailVal.getLinkFromEmail(
       "[qa.staging.integrator.io] Request to change your email", false, "pwqa1"
     );
-    // console.log("link: " + link1.toString())
     let Account1Tab=".MuiToolbar-root .MuiSvgIcon-root"
     let Account1Tab1=await page.$$(".MuiToolbar-root .MuiSvgIcon-root")
     await page.waitForTimeout(20000);
-    // console.log("Account tab : " + Account1Tab)
-    // console.log("Account tab len : " + Account1Tab.length)
     let  last1 = await Account1Tab1.length - 1;
-    // console.log("last: " + last1)
 
     if (!isNotLoggedIn1) {
       // await io.homePage.waitForElementAttached(AccountTab[last]);
@@ -120,8 +108,7 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
     //End TC_C1655
 
     // check for link in email and navigate to that link
-             
-    // await io.homePage.navigateTo(link1.toString());
+   
     await page.waitForTimeout(10000);
     await io.homePage.navigateTo(link1.toString());
     await io.loginPage.fill(selectors.loginPagePO.EMAIL, "qaautomation1+emailcheck@celigo.com");
