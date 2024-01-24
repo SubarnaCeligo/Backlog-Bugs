d016ed9fa1b16b9a88246ff36b095463d910ddc5
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-01-24 13:54:29 +0530

    C111920

---
+++ __tests__/uiux_suite/general_page/TC_C111920.ts
@@ -0,0 +1,53 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import C111920 from '../../../testData/inputData/transformation/C111920.json';
+
+
+test.describe("C111920", () => {
+    test("C111920", async ({ io, page }) => {
+
+
+        //Create a flow with a lookup that has an offline connection
+        await io.createResourceFromAPI(C111920, "FLOWS");
+        await io.homePage.loadingTime()
+        //Wait for flow toggle button to be visible
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.FLOW_TOGGLE);
+        await io.homePage.loadingTime()
+        await io.flowBuilder.clickByIndex(selectors.basePagePO.ADD_DATA_PROCESSOR, 1);
+
+        await io.homePage.click(selectors.mappings.DEFAULT_MAPPING_TYPE.RESPONSE_MAPPING)
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_GENE+'0"] div')
+        await io.loginPage.fill(selectors.transformationPO.REACT_SELECT+'2-input', 'new');
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_EXTRACT+'0"] div')
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_EXTRACT+'0"] div')
+        await io.loginPage.fill(selectors.transformationPO.REACT_SELECT+'3-input', 'ignored');
+        
+
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_GENE+'1"] div')
+        await io.loginPage.fill(selectors.transformationPO.REACT_SELECT+'4-input','data');
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_EXTRACT+'1"] div')
+        await io.myAccountPage.click(selectors.transformationPO.TEXT_EXTRACT+'1"] div')
+        await io.loginPage.fill(selectors.transformationPO.REACT_SELECT+'5-input','data');
+
+        await page.locator(selectors.basePagePO.SAVE_AND_CLOSE).click();
+        await io.homePage.loadingTime()
+        await io.flowBuilder.clickByIndex(selectors.basePagePO.ADD_DATA_PROCESSOR, 2);
+
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
+
+
+        let divTextContent = await io.flowBuilder.getText(selectors.basePagePO.ACE_CONTENT);
+
+        let isValid = false
+        if (divTextContent.includes("data") && divTextContent.includes("new")) {
+            isValid = true
+        }
+
+        await expect(isValid).toBeTruthy();
+
+
+
+    });
+});
+
+
