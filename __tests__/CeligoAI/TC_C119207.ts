b422fa1f9c38a2b90ca64ac597f48ddf278f8e1a
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-02-27 14:14:18 +0530

    added test cases for celigo ai bar enhancements

--- __tests__/CeligoAI/TC_C119207.ts
+++ __tests__/CeligoAI/TC_C119207.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe("C117448 Verify Filter is having Celigo AI", () => {
+test.describe("C117448 Verify Celigo AI panel enhancements", () => {
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
@@ -9,30 +9,31 @@ test.describe("C117448 Verify Filter is having Celigo AI", () => {
     await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
     await io.flowBuilder.clickByText('Filter_DND');
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR);
-    //EXPORT_FILTER 
+
     await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR, 0);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT_FILTER);
-    //Verify Celigo AI are in collapsed state and disabled.
-    // await io.assert.verifyElementAttribute(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_BAR,"aria-expanded","false");
-    // const celigoAiDragbar = await page.locator('[style^="grid-area: dragBar"][style$="aiChatbot"]');
-    const celigoAiDragbar = await page.locator('[style*="grid-area: dragBar_"][style*="_aiChatbot;"]');
+
+    const celigoAiDragbar = await page.locator(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_DRAG_BAR);
     await expect(celigoAiDragbar).not.toBeVisible();
     await io.flowBuilder.clickByText('Celigo AI');
     const celigoAiWindow = await page.locator('[style="grid-area: chat;"]');
     const clientHeight = await celigoAiWindow.evaluate(node => node.clientHeight);
     expect(clientHeight).toBeLessThan(200);
-    await io.assert.verifyElementIsDisplayed('[style*="aiChatbot"]', "Celigo AI dragbar is not displayed")
+    await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.OPENAI.AICHATBOT_PANEL, "Celigo AI dragbar is not displayed")
 
-    // const h = await page.locator('[style*="grid-area: dragBar_v_0;"]');
+    // drag the bar
     await celigoAiDragbar.hover();
     await page.mouse.down();
-    await page.mouse.move(0, 10, { steps: 10 }); // Move mouse 100 pixels up
+    await page.mouse.move(0, 10, { steps: 10 });
     await page.mouse.up();
 
     await celigoAiDragbar.hover();
     await page.mouse.down();
-    await page.mouse.move(0, 10, { steps: 10 }); // Move mouse 100 pixels up
+    await page.mouse.move(0, 10, { steps: 10 });
     await page.mouse.up();
+
+    const updatedClientHeight = await celigoAiWindow.evaluate(node => node.clientHeight);
+    expect(updatedClientHeight).toBeGreaterThan(200);
 
     await io.flowBuilder.clickByText("JavaScript");
     
@@ -52,6 +53,5 @@ test.describe("C117448 Verify Filter is having Celigo AI", () => {
 
     const resetButton = await page.$(selectors.flowBuilderPagePO.OPENAI.RESET_CONVERSATION);
     expect(await resetButton.getAttribute('class')).toContain('Mui-disabled');
-    // test
   });
 });
