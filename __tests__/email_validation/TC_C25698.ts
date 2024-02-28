0c8586acfd2d2f0e6f4bd36e0a670d6b770d0900
Swaraj Cheguri <swaraj.cheguri@celigo.com>
2024-02-28 12:36:19 +0530

    updated selectors

--- __tests__/email_validation/TC_C25698.ts
+++ __tests__/email_validation/TC_C25698.ts
@@ -13,19 +13,18 @@ test.describe("C25698 Verify the link on the email on resolved errors", () => {
     await io.flowBuilder.click(`${selectors.flowBuilderPagePO.NOTIFY_ME_ON_FLOW_ERROR} ${selectors.basePagePO.VALUE_TRUE}`);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.RUN_FLOW);
-    await io.homePage.waitForElementAttached('[data-test="account-dashboard-open-errors"]');
-    await io.flowBuilder.click('[data-test="account-dashboard-open-errors"]');
+    await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.ACCOUNT_DASHBOARD_OPEN_ERRORS);
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.ACCOUNT_DASHBOARD_OPEN_ERRORS);
     await page.getByRole('row', { name: 'Message Code Source' }).getByRole('checkbox').check();
     await io.homePage.clickByText('Resolve & next');
     await io.flowBuilder.delay(1000 * 60 * 15);
     const res = await io.emailVal.getLinkFromEmail("1 resolved error: TC_C25698", true, 'pwqa1');
-    console.log('resoonse from mail', res);
     // testing the flow link
     await io.homePage.navigateTo(res[0].split(`\\`)[0]);
     await io.homePage.waitForElementAttached("text='TC_C25698'");
     await io.assert.verifyElementIsDisplayed("text='TC_C25698'", 'Flow Link not loading properly');
     // testing the Unsubscribe link
-    await io.homePage.navigateTo(res[5].split(`\\`)[0]);
+    await io.homePage.navigateTo(res[7].split(`\\`)[0]);
     await io.homePage.waitForElementAttached("text='Notify me of flow error'");
     await io.assert.verifyElementIsDisplayed("text='TC_C25698'", 'Unsubscribe link not loading properly');
     // testing export name 
@@ -33,6 +32,13 @@ test.describe("C25698 Verify the link on the email on resolved errors", () => {
     await io.homePage.navigateTo(res[2].split('>')[0]);
     await io.homePage.waitForElementAttached("text='TC_C25698'");
     await io.assert.verifyElementIsDisplayed("text='TC_C25698'", 'export name link not  loading properly');
-
+    // testing the View import record link
+    await io.homePage.navigateTo(res[5].split(`\\`)[0]);
+    await io.homePage.waitForElementAttached("text='Sign in to Celigo'");
+    await io.assert.verifyElementIsDisplayed("text='Sign in to Celigo'", 'View import record link not loading properly');
+        // testing the View export record link
+    await io.homePage.navigateTo(res[4].split(`\\`)[0]);
+    await io.homePage.waitForElementAttached("text='Sign in to Celigo'");
+    await io.assert.verifyElementIsDisplayed("text='Sign in to Celigo'", 'View export record link not loading properly');
   });
 });
