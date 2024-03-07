a05d7d8d76f5f4b82073809c61f05b0294221669
Parth Ashvinbhai <parth.patel@celigo.com>
2024-03-07 22:13:40 +0530

    review comments resolved

--- __tests__/connections/Create/TC_IO-T4387.ts
+++ __tests__/connections/Create/TC_IO-T4387.ts
@@ -1,11 +1,12 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import body from "@testData/Connections/IO_T4387.json"
+import bodyOffline from "@testData/Connections/IO_T4387_offline.json"
+import bodyOnline from "@testData/Connections/IO_T4387_online.json"
 
 test.describe("TC_IO-T4387", () => {
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
-    await io.connections.deleteConnection(body.name)
+    await io.connections.deleteConnection(bodyOffline.name)
   });
   test("Verify when the offline connection is made online, success colour is changed to green.", async ({
     io,
@@ -13,41 +14,29 @@ test.describe("TC_IO-T4387", () => {
   }) => {
     let actualJson;
     await test.step("*** Creating Offline Connection ***", async () => {
-      actualJson = await io.connections.createConnectionViaAPI(body);
+      actualJson = await io.connections.createConnectionViaAPI(bodyOffline);
       await io.connectionPage.navigateTo(io.data.links.CONNECTIONS_PAGE_URL);
     });
 
     await test.step("*** Verifying the created connection and making it offline***", async () => {
-      await io.connectionPage.fill(selectors.connectionsPagePO.CONNECTION_PAGE_SEARCH_BAR, body.name);
-      await io.assert.expectToBeTrue(await io.homePage.isVisible(`text=${body.name}`), "Connection is not created");
+      await io.connectionPage.fill(selectors.connectionsPagePO.CONNECTION_PAGE_SEARCH_BAR, bodyOffline.name);
+      await io.assert.expectToBeTrue(await io.homePage.isVisible(`text=${bodyOffline.name}`), "Connection is not created");
       await io.connectionPage.click(selectors.integrationPagePO.OPENACTIONSMENU);
       await io.connectionPage.clickByText("Edit connection");
       await io.connectionPage.loadingTime();
       await io.connectionPage.click(selectors.flowBuilderPagePO.CLOSE);
 
       await page.getByText('Offline', { exact: true }).waitFor({ state: 'visible', timeout: 60000 });
-      const connectionStatus = await page.$eval(selectors.flowBuilderPagePO.CONNECTION_STATUS, (span) => span.getAttribute('aria-label'));
-      let result = connectionStatus === 'error';
-      // Determine the connection status based on the aria-label value
-      await io.assert.expectToBeTrue(result, "Connection is not Offline");
+      await io.assert.verifyElementAttribute(selectors.flowBuilderPagePO.CONNECTION_STATUS, 'aria-label', 'error');
 
     });
 
     await test.step("*** Making the connection online and verify if the success color is green ***", async () => {
-      const connectionData = {
-        importJSON: {
-          ...body, ftp: { ...body.ftp, username: process.env["FTP_IPASS2_USERNAME"], password: "Test@123456", qa__password: process.env["FTP_IPASS2_PASSWORD"] },
-        },
-        apiJSON: true,
-      }
-
-      await io.connections.createOrEditConnection(connectionData);
-      await io.connectionPage.fill(selectors.connectionsPagePO.CONNECTION_PAGE_SEARCH_BAR, body.name);
-      await io.assert.expectToBeTrue(await io.homePage.isVisible(`text=${body.name}`), "Connection is not created");
+      await io.connections.createOrEditConnection(bodyOnline);
+      await io.connectionPage.fill(selectors.connectionsPagePO.CONNECTION_PAGE_SEARCH_BAR, bodyOffline.name);
+      await io.assert.expectToBeTrue(await io.homePage.isVisible(`text=${bodyOffline.name}`), "Connection is not created");
       await io.connectionPage.loadingTime();
-      const connectionStatus = await page.$eval(selectors.flowBuilderPagePO.CONNECTION_STATUS, (span) => span.getAttribute('aria-label'));
-      let result = connectionStatus === 'success';
-      await io.assert.expectToBeTrue(result, "Connection is Offline");
+      await io.assert.verifyElementAttribute(selectors.flowBuilderPagePO.CONNECTION_STATUS, 'aria-label', 'success');
       await expect(page.getByRole("status")).toHaveCSS("background-color", "rgb(76, 187, 2)");
     });
 
