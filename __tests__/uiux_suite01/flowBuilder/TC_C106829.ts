0237f05e41c8b6a5bae1818c78ee5cb1830bffbf
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-03-11 09:40:08 +0530

    fix C106829

--- __tests__/uiux_suite01/flowBuilder/TC_C106829.ts
+++ __tests__/uiux_suite01/flowBuilder/TC_C106829.ts
@@ -3,8 +3,10 @@ import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C106829 Test to validate that user is able to see helper syntax for nested expressions only when the cursor is pointing nested expression", () => {
   test("C106829 Test to validate that user is able to see helper syntax for nested expressions only when the cursor is pointing nested expression", async ({io, page}) => {
-    await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
-    await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.flowBuilder.click(selectors.basePagePO.TOOLS);
+    await io.homePage.loadingTime();
+    await io.flowBuilder.click(selectors.basePagePO.FLOW_BUILDER);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.ADD_SOURCE);
     await io.flowBuilder.fill(selectors.settingsPagePO.APP_NAME_INPUT, 'http');
     await io.flowBuilder.click(selectors.flowBuilderPagePO.HTTP_FORM_SWITCH);
