549ba9aada8a7c3d02cb6e9ea25e54ba9c585c32
Vikram Thakre <vikramgithub93@gmail.com>
2024-03-13 22:49:51 +0530

    Specfile

--- __tests__/agent_db_suite/agent_db_suite.spec.ts
+++ __tests__/agent_db_suite/agent_db_suite.spec.ts
@@ -1,12 +1,12 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
 
-  //  "C120199",
-  //  "C120205",
-  //   "C120208",
-    "C120212",
-  //  "C120217",
-  // "C120218",
+   "C120199",
+   "C120205",
+   "C120208",
+   "C120212",
+   "C120217",
+   "C120218",
  
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
