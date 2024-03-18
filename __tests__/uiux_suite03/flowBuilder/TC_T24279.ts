afd93d257ee5684114ea64f81a9a1b93b91e90e3
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-03-18 14:53:19 +0530

    IO-54539

---
+++ __tests__/uiux_suite03/flowBuilder/TC_T24279.ts
@@ -0,0 +1,58 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("T24279 - Verify user is able to create export/lookup/import with 'Clone flow step' radio button", () => {
+    test.beforeEach(async ({ io }) => {
+        await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+        //make sure the account has at least one shopify export
+        // await io.createResourceFromAPI(exportJson, 'EXPORT');
+    });
+    test("T24279 - Verify user is able to create export/lookup/import with 'Clone flow step' radio button", async ({ io, page }) => {
+        //Navigate to flow builder page
+        await io.homePage.goToMenu("Tools", "Flow builder");
+
+        //Add Source
+        await io.flowBuilder.click(selectors.basePagePO.ADD_SOURCE_BUTTON);
+
+        //Search and select an application
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.APP_NAME_INPUT);
+        await io.flowBuilder.fill(selectors.flowBuilderPagePO.APP_NAME_INPUT, "HTTP");
+        await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.HTTP_CNNECTOR);
+        await io.flowBuilder.click(selectors.connectionsPagePO.HTTP_CNNECTOR);
+
+        //Wait for existing resources to load
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXISTING_ACCOUNT_RESOURCE);
+        
+        //Click on any existing resource
+        await io.flowBuilder.clickButtonByIndex(selectors.flowBuilderPagePO.EXISTING_ACCOUNT_RESOURCE, 0);
+
+        //Verify if Next button is clickable
+        await io.flowBuilder.click(selectors.basePagePO.SAVE);
+
+        //Wait for the page to load
+        await io.flowBuilder.waitForElementAttached(selectors.basePagePO.ADD_NAME);
+
+        //select connection
+        await io.flowBuilder.click(selectors.basePagePO.CONNECTION);
+        await io.flowBuilder.fill(selectors.basePagePO.CONNECTION_DROPDOWN,'HTTP ZENDESK CONNECTION');
+        await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST);
+        await io.flowBuilder.clickByTextByIndex('HTTP ZENDESK CONNECTION', 0);
+
+         //Make sure Clone flow step is selected
+         await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.CLONE_FLOW_STEP_RADIO);
+         await io.assert.verifyElementAttributeContainsText(selectors.flowBuilderPagePO.CLONE_FLOW_STEP_RADIO, "class", "Mui-checked" );
+
+        //save
+        await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+        await io.flowBuilder.loadingTime();
+
+        //Verify that export is added
+        let isExportAdded = await io.flowBuilder.isVisible(selectors.flowBuilderPagePO.EMPTY_PAGE_GENERATOR);
+        await io.assert.expectToBeFalse(isExportAdded, 'Export is not saved');
+
+
+
+      
+        
+    });
+});
