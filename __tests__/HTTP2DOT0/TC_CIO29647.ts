f9e5aa1c9c5dc08a6c4ac9a580040ce7f01e2772
Revathi-D <revathi.dasari@celigo.com>
2024-03-18 13:32:44 +0530

    template connection changes

---
+++ __tests__/HTTP2DOT0/TC_CIO29647.ts
@@ -0,0 +1,54 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("TC_CIO29647", () => {
+test("IO29647", async ({ io, page }) => {
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.homePage.waitForElementAttached(selectors.basePagePO.MARKETPLACE);
+    await io.flowBuilder.click(selectors.basePagePO.MARKETPLACE);
+    await io.flowBuilder.fill(selectors.templatePagePO.MARKETPLACE,'HTTP2.0');
+    await io.flowBuilder.click(selectors.flowGroupingPagePO.INSTALLTEMPLATE);
+    await io.flowBuilder.clickByText("Install now");
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.waitForElementAttached('[data-test="Configure"]');
+    await io.flowBuilder.click('[data-test="Configure"]');
+
+    await io.assert.verifyElementIsDisplayed(selectors.templatePagePO.FTPGUIDE,'ftp KB guide displayed');
+    await io.flowBuilder.click("[data-test='existing']");
+    await io.flowBuilder.click(selectors.basePagePO.CONNECTION);
+    await io.flowBuilder.clickByText("FTP");
+    await io.flowBuilder.click(selectors.basePagePO.SAVE);
+
+
+    await io.flowBuilder.click(selectors.integrationPagePO.SETUP_INTEGRATION_CONFIGURE_BUTTON);
+    await io.flowBuilder.loadingTime();
+    await io.assert.verifyElementIsDisplayed(selectors.templatePagePO.ORDERFULGUIDE,'orderful KB article guide displayed');
+   await io.assert.verifyElementIsDisplayed(selectors.basePagePO.HTTP_2DOT0,'HTTP toggle not displayed');
+   await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.SIMPLE_FORM_SWITCH,'Simple toggle not displayed');
+   await io.flowBuilder.click(selectors.basePagePO.HTTP_2DOT0);
+   await io.flowBuilder.loadingTime();
+   const element1 = await page.locator(selectors.templatePagePO.ORDERFULTOKEN);
+      await element1.scrollIntoViewIfNeeded();
+
+   await io.flowBuilder.fill(selectors.templatePagePO.ORDERFULTOKEN,'d5a50368c52d4db284e0e4ec8c2ca5ab');
+   await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+   await io.flowBuilder.click(selectors.integrationPagePO.SETUP_INTEGRATION_INSTALL_BUTTON);
+   await io.flowBuilder.click(selectors.basePagePO.CONNECTIONS);
+   await io.flowBuilder.clickByText('toggle(orderful)');
+   await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.MEDIA_TYPE,'Mediatype displayed in http view');
+   await io.flowBuilder.click(selectors.basePagePO.CLOSE);
+   await io.flowBuilder.click(selectors.flowGroupingPagePO.FLOWS);
+   await io.flowBuilder.loadingTime();
+   await io.flowBuilder.click(selectors.flowBuilderPagePO.ACTIONSMENUINFLOW);
+   await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.DELETE_CONNECTION);
+   await io.flowBuilder.click(selectors.connectionsPagePO.DELETE_CONNECTION);
+   await io.flowBuilder.loadingTime();
+   await io.flowBuilder.click(selectors.basePagePO.DELETE);
+   await io.flowBuilder.waitForElementAttached(selectors.homePagePO.DELETE_INTEGRATION);
+   await io.flowBuilder.click(selectors.homePagePO.DELETE_INTEGRATION);
+   await io.flowBuilder.click(selectors.basePagePO.DELETE);
+
+
+
+  });
+});
--- __tests__/HTTP2DOT0/http2dot0.spec.ts
+++ __tests__/HTTP2DOT0/http2dot0.spec.ts
@@ -1,11 +1,12 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  //"C58006",
+  "C58006",
   "C119138",
   "CIO58085",
   "CIO58085two",
   "C58006",
-  "C66039"
+  "C66039",
+  "CIO29647"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 
