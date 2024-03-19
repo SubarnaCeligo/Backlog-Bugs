575b1d116b8f56f821392948a3a8ca9959a16d58
Lalit Agrawal <lalit.agrawal@celigo.com>
2024-03-19 18:00:22 +0530

    NSAW tests part3

---
+++ __tests__/connections/Edit/TC_T27708_T27709.ts
@@ -0,0 +1,61 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import { Flow, Oracle_Connection, NSAW_Connection } from "@testData/Connections/T27708_T27709.json";
+
+test.describe('TC_T27708_T27709', () => {
+    let flowId;
+    let conn1;
+    let conn2;
+    test.afterEach(async ({ io }) => {
+        await io.api.deleteFlowViaAPI(flowId);
+        await io.connections.deleteConnection('Oracle_Connection', conn1);
+        await io.connections.deleteConnection('NSAW_Connection', conn2);
+    });
+
+    test('TC_T27708_T27709', async({ io, page }) => {
+        flowId = await io.createResourceFromAPI(Flow, "FLOWS");
+
+        await test.step("*** Creating Connections ***", async () => {
+            conn1 = await io.connections.createConnectionViaAPI(Oracle_Connection);
+            conn2 = await io.connections.createConnectionViaAPI(NSAW_Connection);
+        });
+
+        await test.step('IO-T27708 Verify Replace connection from exports & imports form', async() => {
+            await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT);
+            await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
+            let connections = await page
+            .locator(`${selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST} li`).filter({ hasText: Oracle_Connection.name }).count();
+            expect(connections).toBe(0);
+            await io.flowBuilder.clickByText(NSAW_Connection.name);
+            await io.flowBuilder.click(selectors.basePagePO.CLOSE);
+            await io.flowBuilder.click(selectors.basePagePO.DISCARD_CHANGES);
+
+            await io.flowBuilder.click(selectors.flowBuilderPagePO.LOOKUP);
+            await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
+            connections = await page
+            .locator(`${selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST} li`).filter({ hasText: Oracle_Connection.name }).count();
+            expect(connections).toBe(1);
+            await io.flowBuilder.click(selectors.basePagePO.CLOSE);
+
+            await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT);
+            await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
+            connections = await page
+            .locator(`${selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST} li`).filter({ hasText: NSAW_Connection.name }).count();
+            expect(connections).toBe(1);
+            await io.flowBuilder.click(selectors.basePagePO.CLOSE);
+        });
+
+
+       await test.step('IO-T27709 Verify Replace connection from flowbuilder replace connection form', async() => {
+            await io.homePage.click(selectors.flowBuilderPagePO.NOTIFICATION_CONNECTIONS);
+            await io.homePage.clickButtonAtTopOfArray(selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU);
+            // const replaceConnection = selectors.flowBuilderPagePO.REPLACE_CONNECTION;
+            await io.homePage.click('[data-test="replaceConnection"]');
+            await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
+            const connections = await page
+            .locator(`${selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST} li`).count();
+            expect(connections).toBe(1);
+            await io.flowBuilder.click(selectors.basePagePO.CLOSE_RIGHT_DRAWER);
+        });
+    });
+});
--- __tests__/connections/Edit/edit_connection.spec.ts
+++ __tests__/connections/Edit/edit_connection.spec.ts
@@ -2,6 +2,7 @@ import { filterTestCases } from "@celigo/aut-utilities"
 var testCases = [
  "T27692_T27693_T27694",
  "T27705_T27706",
+ "T27708_T27709",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 
