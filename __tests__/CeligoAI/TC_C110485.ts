bfb492f8e81d05de8e14d89916499736317cba2f
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-03-27 11:57:12 +0530

    tagged env all

--- __tests__/CeligoAI/TC_C110485.ts
+++ __tests__/CeligoAI/TC_C110485.ts
@@ -1,27 +1,41 @@
-import {expect, test} from "@celigo/ui-core-automation";
+import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C110485 Verify export of MS SQL is having Celigo AI", () => {
-  test("C110485 Verify export of MS SQL is having Celigo AI.", async ({io, page}) => {
+  test("@Env-All C110485 Verify export of MS SQL is having Celigo AI.", async ({
+    io,
+    page
+  }) => {
     await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
     await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.ADD_SOURCE);
-    await io.flowBuilder.fill(selectors.settingsPagePO.APP_NAME_INPUT, 'Microsoft SQL');
+    await io.flowBuilder.fill(
+      selectors.settingsPagePO.APP_NAME_INPUT,
+      "Microsoft SQL"
+    );
     await io.flowBuilder.click(selectors.flowBuilderPagePO.MICROSOFT_SQL);
     await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN);
-    await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN,'MSSQL CONNECTION');
-    await io.flowBuilder.clickByText('MSSQL CONNECTION');
+    await io.flowBuilder.fill(
+      selectors.connectionsPagePO.CONNECTIONS_DROPDOWN,
+      "MSSQL CONNECTION"
+    );
+    await io.flowBuilder.clickByText("MSSQL CONNECTION");
     await io.flowBuilder.click(selectors.basePagePO.SAVE);
-    await io.flowBuilder.fill(selectors.basePagePO.INPUT_NAME_SELECTOR, 'MSSQL AI');
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.OPENAI.OPEN_HANDLEBARS_EDITOR);
+    await io.flowBuilder.fill(
+      selectors.basePagePO.INPUT_NAME_SELECTOR,
+      "MSSQL AI"
+    );
+    await io.flowBuilder.click(
+      selectors.flowBuilderPagePO.OPENAI.OPEN_HANDLEBARS_EDITOR
+    );
     await io.assert.verifyElementIsDisplayed(
       selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_HELPTEXT,
       "Celigo AI is not displayed"
-    )
-    await io.flowBuilder.clickByText('Celigo AI');
+    );
+    await io.flowBuilder.clickByText("Celigo AI");
     await io.assert.verifyElementIsDisplayed(
       selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_FIELD,
       "Celigo AI Placeholder is not displayed"
-    )
+    );
   });
 });
--- __tests__/CeligoAI/TC_C119207.ts
+++ __tests__/CeligoAI/TC_C119207.ts
@@ -5,21 +5,34 @@ test.describe("C119207 Verify Celigo AI panel enhancements", () => {
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test("C119207 Verify Celigo AI panel enhancements", async ({ io, page }) => {
+  test("@Env-All C119207 Verify Celigo AI panel enhancements", async ({
+    io,
+    page
+  }) => {
     await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
-    await io.flowBuilder.clickByText('Filter_DND');
-    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR);
+    await io.flowBuilder.clickByText("Filter_DND");
+    await io.flowBuilder.waitForElementAttached(
+      selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR
+    );
 
-    await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR, 0);
+    await io.flowBuilder.clickByIndex(
+      selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR,
+      0
+    );
     await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT_FILTER);
 
-    const celigoAiDragbar = await page.locator(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_DRAG_BAR);
+    const celigoAiDragbar = await page.locator(
+      selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_DRAG_BAR
+    );
     await expect(celigoAiDragbar).not.toBeVisible();
-    await io.flowBuilder.clickByText('Celigo AI');
+    await io.flowBuilder.clickByText("Celigo AI");
     const celigoAiWindow = await page.locator('[style="grid-area: chat;"]');
     const clientHeight = await celigoAiWindow.evaluate(node => node.clientHeight);
     expect(clientHeight).toBeLessThan(200);
-    await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.OPENAI.AICHATBOT_PANEL, "Celigo AI dragbar is not displayed")
+    await io.assert.verifyElementIsDisplayed(
+      selectors.flowBuilderPagePO.OPENAI.AICHATBOT_PANEL,
+      "Celigo AI dragbar is not displayed"
+    );
 
     // drag the bar
     await celigoAiDragbar.hover();
@@ -32,26 +45,41 @@ test.describe("C119207 Verify Celigo AI panel enhancements", () => {
     await page.mouse.move(0, 10, { steps: 10 });
     await page.mouse.up();
 
-    const updatedClientHeight = await celigoAiWindow.evaluate(node => node.clientHeight);
+    const updatedClientHeight = await celigoAiWindow.evaluate(
+      node => node.clientHeight
+    );
     expect(updatedClientHeight).toBeGreaterThan(200);
 
     await io.flowBuilder.clickByText("JavaScript");
-    
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.SCRIPT_LIST_DROPDOWN_ID);
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('Enter');
-    await page.keyboard.press('Tab');
-    await page.keyboard.type('test');
 
-    await (page.locator(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_BAR).nth(-1)).click();
-    await io.assert.verifyElementAttribute(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_FIELD,"placeholder","Tell me about your function here... I will apply your request to the existing function unless you tell me to replace it");
+    await io.flowBuilder.click(
+      selectors.flowBuilderPagePO.SCRIPT_LIST_DROPDOWN_ID
+    );
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Tab");
+    await page.keyboard.type("test");
 
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.SCRIPT_LIST_DROPDOWN_ID);
-    await page.keyboard.press('ArrowUp');
-    await page.keyboard.press('Enter');
-    await page.keyboard.press('Tab');
+    await page
+      .locator(selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_BAR)
+      .nth(-1)
+      .click();
+    await io.assert.verifyElementAttribute(
+      selectors.flowBuilderPagePO.OPENAI.CELIGO_AI_FIELD,
+      "placeholder",
+      "Tell me about your function here... I will apply your request to the existing function unless you tell me to replace it"
+    );
 
-    const resetButton = await page.$(selectors.flowBuilderPagePO.OPENAI.RESET_CONVERSATION);
-    expect(await resetButton.getAttribute('class')).toContain('Mui-disabled');
+    await io.flowBuilder.click(
+      selectors.flowBuilderPagePO.SCRIPT_LIST_DROPDOWN_ID
+    );
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Tab");
+
+    const resetButton = await page.$(
+      selectors.flowBuilderPagePO.OPENAI.RESET_CONVERSATION
+    );
+    expect(await resetButton.getAttribute("class")).toContain("Mui-disabled");
   });
 });
