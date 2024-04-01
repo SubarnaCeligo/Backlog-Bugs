9273f6b5a21675fd620269ee7f6151a05cbed2a2
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-04-01 11:36:59 +0530

    fix

--- __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
+++ __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
@@ -179,13 +179,6 @@ var testCases = [
   "T28451",
   "T28606",
   "T24261",
-  "C24268IO_C24269",
-  "C24270",
-  "C24271",
-  "C24272",
-  "C24273_C24274",
-  "C24282",
-   "C24297",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
