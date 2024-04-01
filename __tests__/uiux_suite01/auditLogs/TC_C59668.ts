0bac91dabd4c6e8ddc83c10baebf1c986005037c
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2024-04-01 21:19:19 +0530

    Update tags for auditLogs suite

--- __tests__/uiux_suite01/auditLogs/TC_C59668.ts
+++ __tests__/uiux_suite01/auditLogs/TC_C59668.ts
@@ -3,7 +3,7 @@ import * as selectors from "@celigo/aut-selectors";
 import testData from "@testData/Connections/Narvar.json";
 
 test.describe(`C59668 Verify Click to view label should be named to View in Audit log`, () => {
-  test(`C59668 Verify Click to view label should be named to View in Audit log`, async ({
+  test(`@Env-All C59668 Verify Click to view label should be named to View in Audit log`, async ({
     io,
     page
   }) => {
