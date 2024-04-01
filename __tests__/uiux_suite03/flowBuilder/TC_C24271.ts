571c99bcb85d8a83f17f7b1a81c079dbf4ff56c7
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-04-01 10:45:18 +0530

    FIX

--- __tests__/uiux_suite03/flowBuilder/TC_C24271.ts
+++ __tests__/uiux_suite03/flowBuilder/TC_C24271.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("TC_C24271", () => {
-  test("@Epic-IO-54539 @Priority-P2 @Zephyr-T24271 @Env-STAGING C24271", async ({ io, page }) => {
+  test("@Epic-IO-54539 @Priority-P2 @Zephyr-IO-T24271 @Env-STAGING C24271", async ({ io, page }) => {
         await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
         await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
 
--- __tests__/uiux_suite03/flowBuilder/TC_C24297.ts
+++ __tests__/uiux_suite03/flowBuilder/TC_C24297.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("TC_C24297", () => {
-  test("@Epic-IO-54539 @Priority-P2 @Zephyr-T24297 @Env-STAGING C24297", async ({ io, page }) => {
+  test("@Epic-IO-54539 @Priority-P2 @Zephyr-IO-T24297 @Env-STAGING C24297", async ({ io, page }) => {
     await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
     await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
 
