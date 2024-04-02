5ef1770ad5218f16d4b8d003ce98adac87ffda83
sushantceligo <sushant.bhosle@celigo.com>
2024-04-02 14:44:12 +0530

    added changes

--- __tests__/connections/Create/TC_C12034.ts
+++ __tests__/connections/Create/TC_C12034.ts
@@ -23,7 +23,7 @@ test.describe(`C12034_Verify Import and load the export which has 4000+ audit re
     await io.flowBuilder.clickByText("Please select");
     await io.flowBuilder.clickByTextByIndex("TC_C12034_Connection_DND", 1);
     await io.flowBuilder.clickByText("Done");
-    // await io.flowBuilder.clickByText("Unassigned");
+    await io.flowBuilder.loadingTime()
     await io.flowBuilder.clickByText("Last updated");
     await io.flowBuilder.clickByTextByIndex("Clone - TC_C12034_Flow_DND", 0);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.FLOW_TOGGLE);
