19440c9da48a370627d5f818e819fee1b94d5a18
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-04-02 19:56:04 +0530

    changed 58006

--- __tests__/HTTP2DOT0/TC_C58006.ts
+++ __tests__/HTTP2DOT0/TC_C58006.ts
@@ -15,41 +15,40 @@ test.describe("IO-58006 scenarios", () => {
     await io.homePage.loadingTime();
     await io.homePage.waitForElementAttached(selectors.basePagePO.TOOLS);
     await io.homePage.goToMenu("Resources", "Connections");
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.ADD_NEW_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.ADD_NEW_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.THREEPL_CONNECTION);
-    await io.flowBuilder.waitForElementAttached(selectors.importPagePO.NAME);
     await io.flowBuilder.fill(selectors.importPagePO.NAME, randomString);
     const edit = await page.$$(selectors.basePagePO.ADD_NEW_RESOURCE);
     await edit[1].click();
-    await io.flowBuilder.loadingTime();
     const name = await page.$$(selectors.importPagePO.NAME);
     await name[1].fill(randomString);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTS_ID);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTS_ID,
       decrypt("MjExOWY2MWItMzgyNS00NTM0LTliMmUtOWYxZTBhNjNjYWFj")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
+    await io.flowBuilder.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
+    await io.flowBuilder.loadingTime();
     const save = await page.$$(selectors.basePagePO.SAVE);
     await save[1].click();
+    await io.flowBuilder.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.EDIT_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.EDIT_RESOURCE);
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
@@ -58,77 +57,72 @@ test.describe("IO-58006 scenarios", () => {
       selectors.connectionsPagePO.HTTP_CNNECTOR,
       1
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
     const save1 = await page.$$(selectors.basePagePO.SAVE);
     await save1[1].click();
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.EDIT_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.EDIT_RESOURCE);
+    await io.homePage.loadingTime();
     const cancel = await page.$$(selectors.basePagePO.CLOSE);
     await cancel[1].click();
-    await io.flowBuilder.loadingTime();
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.USERNAM3PL,
       "b779b82f-f5e5-4d59-a2c9-ea2c5eb8eec3"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
     await io.flowBuilder.click(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENV3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.ENV3PL);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE);
+    await io.homePage.loadingTime();
     // created connection validation in import
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.RESOURCES);
     await io.flowBuilder.click(selectors.basePagePO.RESOURCES);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.IMPORTS);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.IMPORTS);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.ADD_NEW_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.ADD_NEW_RESOURCE);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.THREEPL_CONNECTION);
     await io.flowBuilder.click(selectors.connectionsPagePO.THREEPL_CONNECTION);
-    await io.flowBuilder.waitForElementAttached(selectors.exportsPagePO.CREATE_SELECT_CONNECTION);
+    await io.flowBuilder.loadingTime();
     await io.flowBuilder.click(selectors.exportsPagePO.CREATE_SELECT_CONNECTION);
-    await io.flowBuilder.waitForElementAttached(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
     await io.flowBuilder.fill(
       selectors.exportsPagePO.CONNECTIONS_DROPDOWN,
       randomString
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CONNECTIONDROPDOWNMENU);
     await io.flowBuilder.click(
       selectors.connectionsPagePO.CONNECTIONDROPDOWNMENU
     );
+    await io.flowBuilder.loadingTime();
     const edit6 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edit6[0].click();
     const edits7 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edits7[1].click();
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
     const save6 = await page.$$(selectors.basePagePO.SAVE);
     await save6[2].click();
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.EDIT_RESOURCE);
+    await io.homePage.loadingTime();
     const edit7 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edit7[1].click();
     await io.homePage.loadingTime();
@@ -156,42 +150,44 @@ test.describe("IO-58006 scenarios", () => {
     await edit9[1].click();
     const close1 = await page.$$(selectors.basePagePO.CLOSE);
     await close1[2].click();
+    await io.homePage.loadingTime();
     const close2 = await page.$$(selectors.basePagePO.CLOSE);
     await close2[1].click();
+    await io.homePage.loadingTime();
     const newconnection1 = await page.$$(selectors.basePagePO.ADD_NEW_RESOURCE);
     await newconnection1[1].click();
     // new connection in export
+    await io.homePage.loadingTime();
     const expconn = await page.$$(selectors.importPagePO.NAME);
     await expconn[1].fill(randomString);
     const edits = await page.$$(selectors.basePagePO.ADD_NEW_RESOURCE);
     await edits[2].click();
     const name6 = await page.$$(selectors.importPagePO.NAME);
     await name6[2].fill(randomString);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTS_ID);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTS_ID,
       decrypt("MjExOWY2MWItMzgyNS00NTM0LTliMmUtOWYxZTBhNjNjYWFj")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
     const save7 = await page.$$(selectors.basePagePO.SAVE);
     await save7[2].click();
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.homePage.loadingTime();
     const editimport2 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await editimport2[1].click();
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
@@ -209,15 +205,16 @@ test.describe("IO-58006 scenarios", () => {
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
+    await io.flowBuilder.loadingTime();
     const saves = await page.$$(selectors.basePagePO.SAVE);
     await saves[2].click();
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.homePage.loadingTime();
     const editexport = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await editexport[1].click();
     await io.homePage.loadingTime();
@@ -228,39 +225,35 @@ test.describe("IO-58006 scenarios", () => {
     const cancels = await page.$$(selectors.basePagePO.CLOSE);
     await cancels[2].click();
     await io.homePage.loadingTime();
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.USERNAM3PL);
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.USERNAM3PL,
       "b779b82f-f5e5-4d59-a2c9-ea2c5eb8eec3"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
     await io.flowBuilder.click(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENV3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.ENV3PL);
     const savencontinues = await page.$$(selectors.basePagePO.SAVE);
     await savencontinues[1].click();
     await io.homePage.loadingTime();
     // validation in Export side
     await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.RESOURCES);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.RESOURCES);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.EXPORTS);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.EXPORTS);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.ADD_NEW_RESOURCE);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.ADD_NEW_RESOURCE);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.THREEPL_CONNECTION);
     await io.flowBuilder.click(selectors.connectionsPagePO.THREEPL_CONNECTION);
-    await io.flowBuilder.waitForElementAttached(selectors.exportsPagePO.CREATE_SELECT_CONNECTION);
+    await io.flowBuilder.loadingTime();
     await io.flowBuilder.click(selectors.exportsPagePO.CREATE_SELECT_CONNECTION);
-    await io.flowBuilder.waitForElementAttached(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
     await io.flowBuilder.fill(
       selectors.exportsPagePO.CONNECTIONS_DROPDOWN,
       randomString
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CONNECTIONDROPDOWNMENU);
     await io.flowBuilder.click(
       selectors.connectionsPagePO.CONNECTIONDROPDOWNMENU
     );
+    await io.flowBuilder.loadingTime();
     const edit1 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edit1[0].click();
     const edit4 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
@@ -280,10 +273,11 @@ test.describe("IO-58006 scenarios", () => {
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.homePage.loadingTime();
     const edit2 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edit2[1].click();
+    await io.homePage.loadingTime();
     await io.flowBuilder.clickByIndex(
       selectors.flowBuilderPagePO.SIMPLE_FORM_SWITCH,
       1
@@ -298,21 +292,24 @@ test.describe("IO-58006 scenarios", () => {
     );
     const save3 = await page.$$(selectors.basePagePO.SAVE);
     await save3[2].click();
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
-    );  
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
+    );
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
     const edit3 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await edit3[1].click();
     const close = await page.$$(selectors.basePagePO.CLOSE);
     await close[2].click();
+    await io.homePage.loadingTime();
     const closes1 = await page.$$(selectors.basePagePO.CLOSE);
     await closes1[1].click();
+    await io.homePage.loadingTime();
     const newconnection = await page.$$(selectors.basePagePO.ADD_NEW_RESOURCE);
     await newconnection[1].click();
     // new connection in import
+    await io.homePage.loadingTime();
     const impconn = await page.$$(selectors.importPagePO.NAME);
     await impconn[1].fill(randomString);
     const edit5 = await page.$$(selectors.basePagePO.ADD_NEW_RESOURCE);
@@ -338,19 +335,20 @@ test.describe("IO-58006 scenarios", () => {
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.homePage.loadingTime();
     const editimport = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await editimport[1].click();
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.CLIENTS_ID)).isVisible(),
       "Saved in Simple View Mode"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.HTTP_CNNECTOR);
     await io.flowBuilder.clickByIndex(
       selectors.connectionsPagePO.HTTP_CNNECTOR,
       1
     );
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.ENABLEJWTHTTP)).isVisible(),
       "HTTP View is not displayed"
@@ -361,15 +359,16 @@ test.describe("IO-58006 scenarios", () => {
     );
     const saves6 = await page.$$(selectors.basePagePO.SAVE);
     await saves6[2].click();
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CLIENTSECRET3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.fill(
       selectors.connectionsPagePO.CLIENTSECRET3PL,
       decrypt("TG9xazJFbVovMno5L2xGNk9COFB3aUh2dzdGbW1IaHA=")
     );
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.homePage.loadingTime();
     const editimport1 = await page.$$(selectors.connectionsPagePO.EDIT_RESOURCE);
     await editimport1[1].click();
+    await io.homePage.loadingTime();
     await io.assert.expectToBeTrue(
       await (await page.$(selectors.connectionsPagePO.ENABLEJWTHTTP)).isVisible(),
       "HTTP View is not displayed"
@@ -381,11 +380,11 @@ test.describe("IO-58006 scenarios", () => {
       selectors.connectionsPagePO.USERNAM3PL,
       "b779b82f-f5e5-4d59-a2c9-ea2c5eb8eec3"
     );
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
     await io.flowBuilder.click(selectors.connectionsPagePO.ENVIRONMENT_WDIO);
-    await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.ENV3PL);
+    await io.homePage.loadingTime();
     await io.flowBuilder.click(selectors.connectionsPagePO.ENV3PL);
     const savencontinue = await page.$$(selectors.basePagePO.SAVE);
     await savencontinue[1].click();
+    await io.homePage.loadingTime();
   });
 });
