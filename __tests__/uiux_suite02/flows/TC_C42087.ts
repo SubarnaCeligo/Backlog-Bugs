0f9bb5253ecef23b727b309c8d7e857ecc2381ce
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-04-04 12:18:04 +0530

    updated assertion

--- __tests__/uiux_suite02/flows/TC_C42087.ts
+++ __tests__/uiux_suite02/flows/TC_C42087.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C42087 @Epic-IO-19524 @Env-All @Zephyr/IO-T5642 Test to verify that user is able to select a snapshot which is available in 2nd or 3rd page under revisions tab and initiate a revert on it", () => {
-  test("C118293 Test to verify that user is able to select a snapshot which is available in 2nd or 3rd page under revisions tab and initiate a revert on it", async ({ io, page }) => {
+  test("C42087 Test to verify that user is able to select a snapshot which is available in 2nd or 3rd page under revisions tab and initiate a revert on it", async ({ io, page }) => {
     await io.flowBuilder.navigateTo(io.data.links.HOME_PAGE_URL);
     await io.flowBuilder.waitForElementAttached(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR);
     await io.flowBuilder.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, 'Clone - C42087-rename_DND');
@@ -12,6 +12,6 @@ test.describe("C42087 @Epic-IO-19524 @Env-All @Zephyr/IO-T5642 Test to verify th
     await io.flowBuilder.clickByText("10");
     await io.flowBuilder.click(selectors.basePagePO.IDNEXTPAGE);
     await io.flowBuilder.clickByIndex(selectors.integrationPagePO.OPENACTIONSMENU, 2);
-    await io.flowBuilder.clickByText("Revert to this revision");
+    await io.assert.verifyElementContainsText(selectors.integrationPagePO.REVERTREV, 'Revert to this revision')
   });
 });
