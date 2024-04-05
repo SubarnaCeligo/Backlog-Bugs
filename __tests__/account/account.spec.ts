9cf03c63b3842c9012c0bbab3af71c8f5ef6bc73
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-04-05 11:59:42 +0530

    removed comma

--- __tests__/account/account.spec.ts
+++ __tests__/account/account.spec.ts
@@ -1,7 +1,7 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
   "C53063",
-    "C110068", // C110068 should be at the end.,
+    "C110068" // C110068 should be at the end.,
    
   ];
     var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
