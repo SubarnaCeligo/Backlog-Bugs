56f8a42dacd93d562d5909c5187862f4b188bae4
sagna123 <sagnasagnaperala@gamil.com>
2024-04-08 11:24:03 +0530

    added ENV tag

--- __tests__/uiux_suite03/agents/TC_C104745.ts
+++ __tests__/uiux_suite03/agents/TC_C104745.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C104745 Verify for Agent dropdown", () => {
-  test("C104745 Verify for Agent dropdown", async ({
+  test("C104745 Verify for Agent dropdown  @ENV-All" , async ({
     io,
     page
   }) => {
