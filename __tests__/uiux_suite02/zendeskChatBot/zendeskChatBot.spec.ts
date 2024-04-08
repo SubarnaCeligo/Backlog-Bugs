6362228e9a71d6c5a1bfc0b889f1844180247383
sagna123 <sagnasagnaperala@gamil.com>
2024-04-08 12:23:35 +0530

    upadting zendeskchatBot suite sepc file

--- __tests__/uiux_suite02/zendeskChatBot/zendeskChatBot.spec.ts
+++ __tests__/uiux_suite02/zendeskChatBot/zendeskChatBot.spec.ts
@@ -1,14 +1,15 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  "C55915",
-  "C55917",
-  "C66905",
-  "C66906",
-  "C55918",
-  "C55928",
-  "C55930",
-  "C55934",
-  "C55936"
+  // "C55915",
+  // "C55917",
+  // "C66905",
+  // "C66906",
+  // "C55918",
+  // "C55928",
+  // "C55930",
+  // "C55934",
+  // "C55936"
+  // These test cases are obselete, will delete these files once they are removed from zephyr.
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
