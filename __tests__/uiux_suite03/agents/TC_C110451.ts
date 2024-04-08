659073565dbe947a7c6761664063ad23b53f40d3
sagna123 <sagnasagnaperala@gamil.com>
2024-04-08 12:41:26 +0530

    added tags

--- __tests__/uiux_suite03/agents/TC_C110451.ts
+++ __tests__/uiux_suite03/agents/TC_C110451.ts
@@ -2,7 +2,7 @@ import {expect, test} from "@celigo/ui-core-automation";
 const fs = require('fs');
 import * as selectors from "@celigo/aut-selectors";
 import { HomePage } from "@celigo/ui-core-automation/dist/src/pageFactory/pages/HomePage";
-test.describe("C110451 Verify user is able to download the agent on Linux machine. @ENV-All", () => {
+test.describe("C110451 Verify user is able to download the agent on Linux machine. @Priority-P2 @Zephyr-IO-T18511 @ENV-All", () => {
     test.beforeEach(async ({ io }) => {
          
         await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
