cf5516dd6f77e1f55fa4504c5b4108854c2702fa
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-04-10 13:52:34 +0530

    Adding TC

--- __tests__/uiux_suite01/exports/Exports.spec.ts
+++ __tests__/uiux_suite01/exports/Exports.spec.ts
@@ -64,7 +64,8 @@ var testCases = [
   "T28417_T28454_T28455_T28456",
   "T284932",
   "T731",
-  "T833"
+  "T833",
+  "T26351"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
