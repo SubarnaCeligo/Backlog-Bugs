e450746729107a82d57c4cc46b807ebf5acb3279
Revathi-D <revathi.dasari@celigo.com>
2024-04-15 14:11:23 +0530

    http2.0cases

--- __tests__/HTTP2DOT0/http2dot0.spec.ts
+++ __tests__/HTTP2DOT0/http2dot0.spec.ts
@@ -5,7 +5,8 @@ var testCases = [
   "CIO58085two",
   "C58006",
   "C66039",
-  "CIO29647"
+  "CIO29647",
+  "CT26354T26351T28950T19565"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 
