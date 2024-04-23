0233b3dcc6024d8f43f01ff36c317c29d3f4af3d
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-04-23 11:05:21 +0530

    made changes after reverted

--- __tests__/email_suite/TC_C55953.ts
+++ __tests__/email_suite/TC_C55953.ts
@@ -13,7 +13,7 @@ test.describe.skip(
         await io.homePage.click(selectors.basePagePO.SIGN_OUT);
       }
     });
-    test("@Env-All C55953 Verify the user is able to save the newly set password successfully via email reset password link", async ({
+    test("C55953 Verify the user is able to save the newly set password successfully via email reset password link", async ({
       io,
       page
     }) => {
