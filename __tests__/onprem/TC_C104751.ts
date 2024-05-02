096543730a858bc33b06637cc6062cf9947801ca
prathamsingh <prathamsingh96@gmail.com>
2024-05-02 12:58:55 +0530

    minor change

--- __tests__/onprem/TC_C104751.ts
+++ __tests__/onprem/TC_C104751.ts
@@ -8,7 +8,7 @@ test.describe("TC_C104751 Verify test connection for JDBC On-Prem cnnector", () 
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test("TC_C104751 Verify test connection for JDBC On-Prem cnnector", async ({ io, page }) => {
+  test("@Zephyr-IO-24824 @Env-Staging @Priority-P2 TC_C104751 Verify test connection for JDBC On-Prem cnnector", async ({ io, page }, testInfo) => {
     await io.homePage.loadingTime()
     // navigate to my connection page
     await io.connectionPage.navigateTo(io.data.links.CONNECTIONS_PAGE_URL);
@@ -30,7 +30,7 @@ test.describe("TC_C104751 Verify test connection for JDBC On-Prem cnnector", () 
     await io.flowBuilder.fill(selectors.connectionsPagePO.JDBC_USER_INPUT, "admin");
     await io.flowBuilder.fill(selectors.connectionsPagePO.JDBC_PASSWORD_INPUT, decrypt("bkxLNEJnR0RXWXdM"));
     await io.connectionPage.click(selectors.basePagePO.TEST_CONNECTION);
-    await io.flowBuilder.click('.MuiDialogActions-root .MuiButtonBase-root');
-    await io.assert.verifyElementContainsText(selectors.basePagePO.NOTIFICTION_BAR, 'This connection is currently verify test connection for JDBC On-Prem cnnector.');
+
+    await io.assert.verifyElementContainsText(selectors.basePagePO.NOTIFICTION_BAR, 'Your connection is working great! Nice Job!');
   });
 });
