cda51d29e5ec30dbd96a184b9d53a7e2ffb2d637
Kiran Chandrasekharan <kiran.chandrasekharan@celigo.com>
2024-05-06 09:58:51 +0530

    add test case

--- __tests__/uiux_suite02/flows/Flows.spec.ts
+++ __tests__/uiux_suite02/flows/Flows.spec.ts
@@ -26,6 +26,7 @@ var testCases = [
   'C60440',
   "C20808",
   "C24620",
+  "C107735"
   // "C115891" Duplicate test case
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
---
+++ __tests__/uiux_suite02/flows/TC_C107735.ts
@@ -0,0 +1,38 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import C107735 from '../../../testData/inputData/FlowBuilder/C107735.json';
+
+test.describe(`C107735`, () => {
+  test.beforeEach(async ({ io }) => {
+    await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+});
+  test(`@Env-All @Priority-P2 C107735 check for Error tag can be deleted`, async ({
+    io,
+    page
+  }) => {
+    await io.createResourceFromAPI(C107735, "FLOWS");
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.click(selectors.basePagePO.RUNFLOW);
+    await io.homePage.addStep("*** Running the flow ***");
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.RUNFLOW);
+    await io.homePage.addStep("*** Flow ran succesfully ***");
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT);
+    await io.flowBuilder.click(selectors.importPagePO.FETCH_PREVIEW);
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.CLOSE);
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.reloadPage();
+    await io.homePage.click(selectors.flowBuilderPagePO.ERROR_BUBBLE);
+    await io.homePage.addStep("*** Opened Error page ***");
+    await page.locator(selectors.flowBuilderPagePO.ERROR_TAG).nth(2).click();
+    await io.flowBuilder.fill('[placeholder="Type a new tag for your account"]', "107735C");
+    await io.flowBuilder.click('[data-test="createTag"]');
+    await page.locator('[data-test="deleteTag"]').first().click();
+    await io.flowBuilder.click(selectors.basePagePO.DELETE);
+    await page.locator(selectors.flowBuilderPagePO.ERROR_TAG).nth(2).click();
+    await expect(page.getByText("107735C")).not.toBeVisible();
+  });
+});
