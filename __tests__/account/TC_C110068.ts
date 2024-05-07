cdeb469924d97ae2cdff68b767649f363637a714
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-05-07 12:41:23 +0530

    account updated

--- __tests__/account/TC_C110068.ts
+++ __tests__/account/TC_C110068.ts
@@ -24,8 +24,16 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       );
       await io.flowBuilder.delay(1000 * 60 * 2);
       await io.loginPage.click(selectors.basePagePO.SUBMIT);
-      if (testInfo.status == "failed"){
-        testInfo.retry;
+      let rateLimitErrorsVisibility = await page
+        .locator('[id="error-icon"] ~ div')
+        .isVisible();
+      if (rateLimitErrorsVisibility) {
+        let rateLimitErrors = await page
+          .locator("[id='error-icon'] ~ div")
+          .textContent();
+        let rateLimitErrorTime = await String(rateLimitErrors).search(/[0-9]/);
+        await io.flowBuilder.delay(1000 * Number(rateLimitErrorTime));
+        await io.loginPage.click(selectors.basePagePO.SUBMIT);
       }
       await io.homePage.loadingTime();
       await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
@@ -56,15 +64,21 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       await io.myAccountPage.click(selectors.basePagePO.CLOSE_BUTTON);
       let iourl = new URL(process.env["IO_UI_CONNECTOR_URL"]);
       await page.waitForTimeout(5000);
-      const webHostLink = await io.emailVal.getLinkFromEmail("[" + iourl.host +"] Request to change your email", false, "pwqa1");
+      const webHostLink = await io.emailVal.getLinkFromEmail(
+        "[" + iourl.host + "] Request to change your email",
+        false,
+        "pwqa1"
+      );
       await io.flowBuilder.delay(1000 * 60 * 2);
-        // sign out of IO and navigate to login page
+      // sign out of IO and navigate to login page
       await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
       await io.homePage.loadingTime();
       await io.flowBuilder.click(selectors.basePagePO.ACCOUNT_BUTTON);
       await io.homePage.click(selectors.basePagePO.SIGN_OUT);
       await io.flowBuilder.loadingTime();
       await io.flowBuilder.navigateTo(webHostLink.toString());
+      await io.flowBuilder.loadingTime();
+      await io.flowBuilder.navigateTo(process.env.IOURL);
       await io.flowBuilder.loadingTime();
       await io.loginPage.fill(
         selectors.loginPagePO.EMAIL,
@@ -75,7 +89,14 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
         decrypt(process.env["IO_Password"])
       );
       await io.loginPage.click(selectors.basePagePO.SUBMIT);
-      testInfo.retry;
+      if (rateLimitErrorsVisibility) {
+        let rateLimitErrors = await page
+          .locator("[id='error-icon'] ~ div")
+          .textContent();
+        let rateLimitErrorTime = await String(rateLimitErrors).search(/[0-9]/);
+        await io.flowBuilder.delay(1000 * Number(rateLimitErrorTime));
+        await io.loginPage.click(selectors.basePagePO.SUBMIT);
+      }
     }
     await io.homePage.loadingTime();
     // navigate to my account page
@@ -135,6 +156,17 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       decrypt(process.env["IO_Password"])
     );
     await io.loginPage.click(selectors.basePagePO.SUBMIT);
+    let rateLimitErrorsVisibility = await page
+      .locator('[id="error-icon"] ~ div')
+      .isVisible();
+    if (rateLimitErrorsVisibility) {
+      let rateLimitErrors = await page
+        .locator("[id='error-icon'] ~ div")
+        .textContent();
+      let rateLimitErrorTime = await String(rateLimitErrors).search(/[0-9]/);
+      await io.flowBuilder.delay(1000 * Number(rateLimitErrorTime));
+      await io.loginPage.click(selectors.basePagePO.SUBMIT);
+    }
     await io.homePage.loadingTime();
 
     //click on agent token and validate
@@ -197,12 +229,11 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       false,
       "pwqa1"
     );
-    await io.flowBuilder.delay(1000 * 60 * 2);
     if (!isNotLoggedIn) {
       await io.flowBuilder.click(selectors.basePagePO.ACCOUNT_BUTTON);
       await io.homePage.click(selectors.basePagePO.SIGN_OUT);
     }
-
+    await io.homePage.delay(1000 * 100 * 2);
     //Start TC_C1655 - Email Change Link continues to work even after changing the password
     await io.homePage.navigateTo(link.toString());
     const errorMessage = (
@@ -229,5 +260,16 @@ test.describe("TC_C110068 when user email is changed from profile page, verify E
       decrypt(process.env["IO_Password"])
     );
     await io.loginPage.click(selectors.basePagePO.SUBMIT);
+    let rateLimitErrorsVisibility1 = await page
+      .locator('[id="error-icon"] ~ div')
+      .isVisible();
+    if (rateLimitErrorsVisibility1) {
+      let rateLimitErrors = await page
+        .locator("[id='error-icon'] ~ div")
+        .textContent();
+      let rateLimitErrorTime = await String(rateLimitErrors).search(/[0-9]/);
+      await io.flowBuilder.delay(1000 * Number(rateLimitErrorTime));
+      await io.loginPage.click(selectors.basePagePO.SUBMIT);
+    }
   });
 });
--- __tests__/account/TC_C53063.ts
+++ __tests__/account/TC_C53063.ts
@@ -2,16 +2,46 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import ProfessionalLicense from "@testData/Flows/ProfessionalLicense.json";
 import { MyAccountPage } from "@celigo/ui-core-automation/dist/src/pageFactory/pages/MyAccountPage";
+import { decrypt } from "@celigo/aut-utilities";
 
 test.describe("C53063 Verify that the 30 and 60 days option is enable for license types Professional tire", () => {
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
+    await io.homePage.loadingTime();
   });
 
   test("@Env-All @Zephyr-IO-T14552 C53063 Verify that the 30 and 60 days option is enable for license types Professional tire", async ({
     io,
     page
   }) => {
+    let signInLink = await page.url();
+    if (signInLink.endsWith("signin")) {
+      await io.loginPage.fill(
+        selectors.loginPagePO.EMAIL,
+        "qaautomation1+emailcheck5@celigo.com"
+      );
+      await io.loginPage.fill(
+        selectors.loginPagePO.PASSWORD,
+        decrypt(process.env["IO_Password"])
+      );
+      await io.flowBuilder.delay(1000 * 60 * 2);
+      await io.loginPage.click(selectors.basePagePO.SUBMIT);
+      await page.waitForTimeout(10000);
+      let rateLimitErrorsVisibility = await page
+        .locator('[id="error-icon"]~div')
+        .isVisible();
+      if (rateLimitErrorsVisibility) {
+        let rateLimitErrors = await io.flowBuilder.getTextFromElement(
+          '[id="error-icon"]~div'
+        );
+        let rateLimitErrorTime = await String(rateLimitErrors).search(/[0-9]/);
+        await io.flowBuilder.delay(1000 * Number(rateLimitErrorTime));
+        await io.loginPage.click(selectors.basePagePO.SUBMIT);
+        await io.flowBuilder.loadingTime();
+      }
+    }
+    await io.homePage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
+    await io.homePage.loadingTime();
     const licenses = await io.api.getCall("v1/licenses");
     let putcall = await io.api.putCall(
       `v1/test/licenses/${process.env["IO_Integration_ID"]}`,
