2bc08fdfdf6f194d71391ee051d40a1912f8f341
Kiran Chandrasekharan <kiran.chandrasekharan@celigo.com>
2024-05-07 10:11:22 +0530

    remove the test JSON

--- __tests__/uiux_suite02/flows/TC_C107735.ts
+++ __tests__/uiux_suite02/flows/TC_C107735.ts
@@ -1,6 +1,5 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import C107735 from '../../../testData/inputData/FlowBuilder/C107735.json';
 
 test.describe(`C107735`, () => {
   test.beforeEach(async ({ io }) => {
@@ -10,29 +9,19 @@ test.describe(`C107735`, () => {
     io,
     page
   }) => {
-    await io.createResourceFromAPI(C107735, "FLOWS");
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.click(selectors.basePagePO.RUNFLOW);
-    await io.homePage.addStep("*** Running the flow ***");
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.RUNFLOW);
-    await io.homePage.addStep("*** Flow ran succesfully ***");
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT);
-    await io.flowBuilder.click(selectors.importPagePO.FETCH_PREVIEW);
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.CLOSE);
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.reloadPage();
-    await io.homePage.click(selectors.flowBuilderPagePO.ERROR_BUBBLE);
-    await io.homePage.addStep("*** Opened Error page ***");
+    await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+    await io.integrationPage.waitForElementAttached(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR);
+    await io.integrationPage.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, 'Flow_With_Errors_DND');
+    await io.integrationPage.delay(2000); // wait for the search to complete
+    //Open the flow
+    await io.flowBuilder.clickByText('Flow_With_Errors_DND');
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.ACCOUNT_DASHBOARD_OPEN_ERRORS);
+    await io.flowBuilder.hover(selectors.flowBuilderPagePO.ERROR_TAG, 2, true);
     await page.locator(selectors.flowBuilderPagePO.ERROR_TAG).nth(2).click();
     await io.flowBuilder.fill(selectors.flowBuilderPagePO.ERROR_TAG_PLACEHOLDER, "107735C");
     await io.flowBuilder.click(selectors.flowBuilderPagePO.CREATE_ERROR_TAG);
     await page.locator(selectors.flowBuilderPagePO.DELETE_ERROR_TAG).first().click();
     await io.flowBuilder.click(selectors.basePagePO.DELETE);
-    await page.locator(selectors.flowBuilderPagePO.ERROR_TAG).nth(2).click();
     await expect(page.getByText("107735C")).not.toBeVisible();
   });
 });
