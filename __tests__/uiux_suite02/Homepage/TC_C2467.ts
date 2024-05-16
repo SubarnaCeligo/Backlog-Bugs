76ad961f34828401f7f203f0d8e75a09a496d1d8
paresh <paresh.vilas.more@celigo.com>
2024-05-16 11:59:12 +0530

    fix

--- __tests__/uiux_suite02/Homepage/TC_C2467.ts
+++ __tests__/uiux_suite02/Homepage/TC_C2467.ts
@@ -5,9 +5,12 @@ import C2467 from '@testData/Flows/C2467.json';
 test.describe("C2467 Verified all the possible pop-ups and they are closed on click.", () => {
     test("@Env-All C2467 Verified the delete integration pop up", async ({io, page}) => {
         await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.homePage.loadingTime()
         await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU);
         await io.homePage.fill(selectors.homePagePO.SEARCH_INTEGRATION, "Automation Flows")
-        await io.homePage.clickByIndex(`:has-text("Automation Flows") ${selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU}`, 0);
+        await io.homePage.loadingTime()
+        await io.homePage.delay(1000)
+        await io.homePage.clickByIndex(selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU, 0);
         await io.homePage.click(selectors.homePagePO.DELETE_INTEGRATION);
         await io.homePage.addStep('Verifying pop up to appear');
         await io.assert.verifyElementIsDisplayed(selectors.myAccountPagePO.DIALOG_BOX, 'Pop up did not appear');
