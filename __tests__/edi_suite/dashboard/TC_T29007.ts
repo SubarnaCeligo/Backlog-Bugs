5583ed5dc275ebd8441d0003ed3a2ee422754d1c
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-05-20 11:22:17 +0530

    Updating tag

--- __tests__/edi_suite/dashboard/TC_T29007.ts
+++ __tests__/edi_suite/dashboard/TC_T29007.ts
@@ -5,7 +5,7 @@ test.describe("@Author-Shriti S Verify that EDI documents can be filtered using 
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test("@Env-QA @Epic-IO-31713 @Priority-P2 @Zephyr-IO-T29007 Verify that EDI documents can be filtered using date range filter.", async ({ io, page }) => {
+  test("@Env-All @Epic-IO-31713 @Priority-P2 @Zephyr-IO-T29007 Verify that EDI documents can be filtered using date range filter.", async ({ io, page }) => {
 
     //Go to Dashboard
     await io.myAccountPage.navigateTo(process.env["IO_UI_CONNECTOR_URL"] + "dashboard");
--- __tests__/edi_suite/dashboard/TC_T29009.ts
+++ __tests__/edi_suite/dashboard/TC_T29009.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe("@Env-QA @Author-Shriti S Verify that EDI activity can be filtered using multiple FA status.", () => {
+test.describe("@Author-Shriti S Verify that EDI activity can be filtered using multiple FA status.", () => {
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test("@Epic-IO-31713 @Priority-P2 @Zephyr-IO-T29009 Verify that EDI activity can be filtered using multiple FA status.", async ({ io, page }) => {
+  test("@Env-All @Epic-IO-31713 @Priority-P2 @Zephyr-IO-T29009 Verify that EDI activity can be filtered using multiple FA status.", async ({ io, page }) => {
 
     //Go to Dashboard
     await io.myAccountPage.navigateTo(process.env["IO_UI_CONNECTOR_URL"] + "dashboard");
