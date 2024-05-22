0aa702ca4fb1ace99f4109c8389cad1442f1031e
sagna123 <sagnasagnaperala@gamil.com>
2024-05-22 16:12:24 +0530

    minor change

--- __tests__/uiux_suite01/exports/TC_T29677.ts
+++ __tests__/uiux_suite01/exports/TC_T29677.ts
@@ -25,8 +25,14 @@ test.describe("@Author_sagna123 TC_T29677_Test to validate user is able to 'Name
         await io.homePage.addStep("*** Opened the export ***");
         await io.flowBuilder.click(selectors.exportsPagePO.NAME_HELP_TEXT);
         await io.homePage.addStep("*** Checked name field help text  ***");
+        await io.flowBuilder.click(selectors.exportsPagePO.NAME_HELP_TEXT);
+        await io.homePage.addStep("*** Checked name field help text  ***");
         await io.flowBuilder.click(selectors.exportsPagePO.DESCRIPTION_HELP_TEXT);
         await io.homePage.addStep("*** Checked description field help text ***");
+        await io.flowBuilder.click(selectors.exportsPagePO.DESCRIPTION_HELP_TEXT);
+        await io.homePage.addStep("*** Checked description field help text ***");
+        await io.flowBuilder.click(selectors.exportsPagePO.CONNECTION_HELP_TEXT);
+        await io.homePage.addStep("*** Checked connection field help text ***");
         await io.flowBuilder.click(selectors.exportsPagePO.CONNECTION_HELP_TEXT);
         await io.homePage.addStep("*** Checked connection field help text ***");
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
