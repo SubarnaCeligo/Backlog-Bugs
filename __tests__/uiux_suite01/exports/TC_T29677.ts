1c216a11907ea685a7c9108668b5c2f98be131db
sagna123 <sagnasagnaperala@gamil.com>
2024-05-22 14:07:35 +0530

    updating tags for may release test cases

--- __tests__/uiux_suite01/exports/TC_T29677.ts
+++ __tests__/uiux_suite01/exports/TC_T29677.ts
@@ -5,7 +5,7 @@ test.describe("@Author_sagna123 TC_T29677_Test to validate user is able to 'Name
     test.beforeEach(async ({ io }) => {
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
     });
-    test("@Epic-IO-65860 @Priority-P2 @Zephyr-IO-T29677 @Env-QA", async ({ io, page }) => {
+    test("@Epic-IO-65860 @Priority-P2 @Zephyr-IO-T29677 @Env-All", async ({ io, page }) => {
         await io.homePage.addStep("*** Navigated to home page ***");
         await io.homePage.waitForElementAttached(selectors.basePagePO.RESOURCES);
         await io.homePage.goToMenu("Resources","Exports");
