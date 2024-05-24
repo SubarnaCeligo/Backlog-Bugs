a8d9b37c51f557d48701e3c73f92db53f6991a48
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-05-24 14:43:46 +0530

    market place 01

--- __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
+++ __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
@@ -1,10 +1,10 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-// "C22282",
-// "C2397",
-// "C77923",
-// "C22227",
-// "T28487",
+"C22282",
+"C2397",
+"C77923",
+"C22227",
+"T28487",
 "C102890"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
