167a120ab598aac3f437b3a340032b6a3b1270aa
sushantceligo <sushant.bhosle@celigo.com>
2024-05-30 23:16:50 +0530

    Added changes

--- __tests__/transferSBflow/transfer.spec.ts
+++ __tests__/transferSBflow/transfer.spec.ts
@@ -1,7 +1,7 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-//  "C2758",
-//  "C36721",
+ "C2758",
+ "C36721",
  "C1582"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
