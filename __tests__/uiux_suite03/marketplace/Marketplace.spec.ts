43db84aecefde1c651019ea0ad8db99e04dbfa26
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-05-31 16:38:23 +0530

    adding commented cases back

--- __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
+++ __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
@@ -1,11 +1,11 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-// "C22282",
-// "C2397",
-// "C77923",
-// "C22227",
-// "T28487",
-// "C102890",
+"C22282",
+"C2397",
+"C77923",
+"C22227",
+"T28487",
+"C102890",
 "IOT31811_T31824_T31809_T31812"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
