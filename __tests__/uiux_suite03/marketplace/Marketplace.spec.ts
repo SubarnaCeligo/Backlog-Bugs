3fe6c3548a41ce90a339bf7750918f2c371889f5
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-05-31 15:23:24 +0530

    IO-60595 - Marketplace text changes

--- __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
+++ __tests__/uiux_suite03/marketplace/Marketplace.spec.ts
@@ -1,11 +1,17 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-"C22282",
-"C2397",
-"C77923",
-"C22227",
-"T28487",
-"C102890"
+// "C22282",
+// "C2397",
+// "C77923",
+// "C22227",
+// "T28487",
+// "C102890",
+"T31825",
+"T31826",
+"T31827",
+"T31828_T31829_T31831",
+"T31830",
+"T31832"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
     (async () => {
