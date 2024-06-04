f7acaa49544bde8371c60421879b1be13bc41f13
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-06-04 17:06:54 +0530

    BUG AUTOMATION

---
+++ __tests__/flowbuilder02/TC_IO-T31974.ts
@@ -0,0 +1,74 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import TC from '../../testData/inputData/FlowBuilder/T31974.json';
+
+test.describe("@Author_MaheshNivruttiSutar Verify section should be hidden if we set 'useAsPrimaryInterface' is Set to True in Script", () => {
+    let id; let formInit;
+    test.describe.configure({ retries: 1 })
+    test.beforeEach(async ({ io, page  }) => {
+        formInit = await io.api.createScriptViaAPI(TC.script1);
+    });
+    test.afterEach(async ({ io, page }) => {
+        await io.api.deleteFlowViaAPI(id);
+        await io.api.deleteScriptViaAPI(formInit);
+    });
+    test("@Bug-IO-80699 @Env-QA @Priority-P2 @Zephyr-IO-T31974", async ({ io, page }) => {
+        TC.pageGenerators[0].qa__export.settingsForm.init._scriptId = formInit;
+        id = await io.createResourceFromAPI(TC, "FLOWS");
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUN_FLOW);
+        await io.flowBuilder.reloadPage();
+        await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+        await io.integrationPage.loadingTime();
+        await io.flowBuilder.click(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR);
+        await io.integrationPage.fill(
+            selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR,
+            "IO-T31974"
+        );
+        //Open the flow
+        await io.flowBuilder.clickByTextByIndex("IO-T31974",0);
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.RUN_FLOW);
+
+        //Export
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.EXPORT);
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.DATATEST);
+        //clicking on launch from builder
+        await io.flowBuilder.click(selectors.basePagePO.LAUNCH_EDITOR);
+        await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.SCRIPT_DATA_CONTENT);
+        await io.flowBuilder.click('[id="code"] .ace_content');
+        await io.flowBuilder.clearTextValue('[id="code"] .ace_content');
+        await io.flowBuilder.enterHugeData('[id="code"] .ace_content', (TC.script.content));
+        await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+        await io.homePage.loadingTime();
+        // -'Simple/HTTP' toggle at the very top of the bubble drawers (i.e. for HTTP based connectors) should be hidden
+        await io.flowBuilder.waitForElementAttached(selectors.importPagePO.ADVANCED);
+        const toggleSimple = await io.flowBuilder.isVisible(selectors.flowBuilderPagePO.SIMPLE_FORM_SWITCH);
+        await io.assert.expectToBeFalse(toggleSimple, "Simple toggle is not visible");
+        const toggleHttp = await io.flowBuilder.isVisible(selectors.flowBuilderPagePO.SIMPLE_FORM_SWITCH);
+        await io.assert.expectToBeFalse(toggleHttp, "Http toggle is not hidden");
+        // -Other sections 'Configure export type' or 'What would you like to export' or any other should be hidden
+        const exportType = await io.flowBuilder.isVisible(selectors.exportsPagePO.CONFIGURE_EXPORT_TYPE);
+        await io.assert.expectToBeFalse(exportType, "Configure export type is not hidden");
+        const wouldLike = await io.flowBuilder.isVisible(selectors.exportsPagePO.WHAT_WOULD_YOU_LIKE_TO_EXPORT_TAB);
+        await io.assert.expectToBeFalse(wouldLike, "What would you like to export? is not hidden");
+        const apiPage = await io.flowBuilder.isVisible(selectors.flowBuilderPagePO.PAGE);
+        await io.assert.expectToBeFalse(apiPage, "Does this API use paging? is not hidden");
+        const apiResp = await io.flowBuilder.isVisible(selectors.exportsPagePO.NON_STANDARD_API_TAB);
+        await io.assert.expectToBeFalse(apiResp, "Non-standard API response patterns is not hidden");
+        //-'Custom settings' should be renamed to 'Settings'.
+        const Text = await io.homePage.isVisible("text='Settings'");
+        await io.assert.expectToBeTrue(Text, "Settings name not shown");
+
+        // -'Mock output' and 'Advanced' sections should be displayed below 'Settings'
+        // -'Custom settings' section should be displayed below 'General' section.
+        await io.flowBuilder.click(selectors.basePagePO.CUSTOM_SETTING);
+        await io.flowBuilder.click(selectors.connectionsPagePO.GENERAL);
+        await io.homePage.loadingTime();
+        expect(await page.screenshot()).toMatchSnapshot("IO-T31974.png");
+        await io.flowBuilder.click(selectors.basePagePO.CUSTOM_SETTING);
+
+        //-'Launch form builder' button should be displayed in 'Settings' section
+        const buttonDis = await page.$(selectors.flowBuilderPagePO.SETTING);
+        expect(await buttonDis.screenshot()).toMatchSnapshot("IO-T31974_1.png");
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.CLOSE);
+    });
+});
---
+++ __tests__/flowbuilder02/TC_IO-T32338.ts
@@ -0,0 +1,29 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import TC from '../../testData/inputData/FlowBuilder/T32338.json';
+
+test.describe("@Author_MaheshNivruttiSutar Verify is able to create graphql blob import thorough create from scratch", () => {
+    test("@Bug-IO-80221 @Env-QA @Priority-P2 @Zephyr-IO-T32021", async ({ io, page }) => {
+        await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+        await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
+        await io.flowBuilder.click(selectors.flowBuilderPagePO.ADD_DESTINATION_OR_LOOKUP);
+        await io.flowBuilder.fill(selectors.flowBuilderPagePO.APP_NAME_INPUT, "GRAPHQL");
+        await io.flowBuilder.clickByText('GraphQL');
+        await io.flowBuilder.clickByText("Look up additional files (per record)");
+        await io.flowBuilder.clickByText('Create flow step');
+        await io.flowBuilder.loadingTime();
+        await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
+        await io.flowBuilder.clickByText("GRAPHQL CONNECTION");
+        await io.flowBuilder.click(selectors.exportsPagePO.HTTP_METHOD);
+        await io.flowBuilder.click(selectors.importPagePO.HTTPPOSTMETHOD);
+        await io.connectionPage.addStep("HTTP method as POST");
+        await io.flowBuilder.fill(selectors.exportsPagePO.NAME, "GRAPHQL BLOB IMPORT");
+        await io.flowBuilder.fill(selectors.flowBuilderPagePO.GRAPHQLQUERY1, TC.query);
+        await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.SAVEANDCLOSE);
+        await io.flowBuilder.loadingTime();
+        const flow = await io.flowBuilder.isVisible("text='DESTINATIONS & LOOKUPS'");
+        await io.assert.expectToBeTrue(flow, "Import is not created");
+
+    });
+}
+);
