8e2ecd389bf3810f1e4933998de7c12835961899
Kaushik Nandan Uyyala <kaushiknandanuyyala@Kaushiks-MacBook-Pro-2.local>
2024-06-05 11:48:40 +0530

    fix

--- __tests__/uiux_suite03/marketplace/TC_T23721.ts
+++ __tests__/uiux_suite03/marketplace/TC_T23721.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("IO-T23721 Verify IA/Template logo is showing properly in placeholder.", () => {
+  test.describe.configure({ retries: 2 })
   test("@Priority-P3 @Zephyr-IO-T23721 @Env-All Verify IA/Template logo is showing properly in placeholder.", async ({ io, page }) => {
     await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
     await io.homePage.waitForElementAttached(selectors.basePagePO.MARKETPLACE)
@@ -12,10 +13,10 @@ test.describe("IO-T23721 Verify IA/Template logo is showing properly in placehol
     await io.homePage.click(selectors.marketplacePagePO.INTEGRATION_APP_TYPE);
     await io.homePage.loadingTime();
     const Symbol1 = await page.$(selectors.marketplacePagePO.MAGENTO_APP_IMAGE);
-    expect(await Symbol1.screenshot()).toMatchSnapshot("TC_T23721_Connector.png");
+    expect(await Symbol1.screenshot()).toMatchSnapshot("TC_T23721.png");
     await io.homePage.click(selectors.marketplacePagePO.TEMPLATE_TYPE);
     await io.homePage.loadingTime();
     const Symbol2 = await page.$(selectors.marketplacePagePO.MAGENTO_APP_IMAGE);
-    expect(await Symbol2.screenshot()).toMatchSnapshot("TC_T23721_Template.png");
+    expect(await Symbol2.screenshot()).toMatchSnapshot("TC_T23721.png");
   });
 });
