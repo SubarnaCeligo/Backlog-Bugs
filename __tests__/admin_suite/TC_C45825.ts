7ff890003f3b53671823e52917c38e7377394735
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-06-10 21:34:44 +0530

    admin spec updated

---
+++ __tests__/admin_suite/TC_C45825.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import { randomNumber, randomString } from "@celigo/aut-utilities";
+
+test.describe("TC_C45825 Verify if the message is shown correcly on the screen when MFA is disabled for a user.", () => {
+  test("@Zephyr-IO-T17233 @Env-All C45825 Verify if the message is shown correcly on the screen when MFA is disabled for a user.", async ({
+    io,
+    page
+  }) => {
+    await io.flowBuilder.navigateTo(
+      process.env["IO_UI_CONNECTOR_URL"] + "myAccount/users"
+    );
+    await io.flowBuilder.loadingTime();
+    //Enable
+    await io.flowBuilder.clickByIndex(selectors.basePagePO.ENABLESSO, 3);
+    await io.flowBuilder.delay(1000 * 10 * 1);
+
+    //Disable
+    await io.flowBuilder.clickByIndex(selectors.basePagePO.ENABLESSO, 3);
+    await io.assert.verifyElementIsDisplayed(
+      selectors.flowGroupingPagePO.ALERT_MESSAGE,
+      "Disabled error message is not displayed"
+    );
+    await io.flowBuilder.delay(1000 * 10 * 1);
+    await io.flowBuilder.clickByIndex(selectors.basePagePO.ENABLESSO, 3);
+  });
+});
