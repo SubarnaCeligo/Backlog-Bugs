00c7a84687a128f81be6df575a58d7ac68dff4ed
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-06-12 12:02:32 +0530

    ftp spec 01

--- __tests__/uiux_suite03/FTP/FTP.spec.ts
+++ __tests__/uiux_suite03/FTP/FTP.spec.ts
@@ -1,16 +1,16 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  // "C68814",
-  // "C68815",
-  // "C68816",
-  // "C68820",
-  // "C68821",
-  // "C68822",
-  // "C68824",
-  // "C68826",
-  // "C68830",
-  // "C48555",
-  // "C29580",
+  "C68814",
+  "C68815",
+  "C68816",
+  "C68820",
+  "C68821",
+  "C68822",
+  "C68824",
+  "C68826",
+  "C68830",
+  "C48555",
+  "C29580",
   "C23859"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
