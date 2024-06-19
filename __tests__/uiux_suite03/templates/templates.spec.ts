544efb61b520bec47815f76af5eeaeec036bde44
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-06-19 12:12:55 +0530

    resolved conflict

--- __tests__/uiux_suite03/templates/templates.spec.ts
+++ __tests__/uiux_suite03/templates/templates.spec.ts
@@ -1,9 +1,9 @@
 import { filterTestCases } from "@celigo/aut-utilities"
 var testCases = [
-  "T1686",
-  "T26356",
-  "IO-T31836",
-  "IO-T32339",
+  // "T1686",
+  // "T26356",
+  // "IO-T31836",
+  // "IO-T32339",
   "C108853",
   "C19911",
 ];
