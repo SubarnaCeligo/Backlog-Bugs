c70f816951d046cf7cb61783f0a9c494605dff95
snigdharani.panigrahy712@gmail.com <snigdharani.panigrahy@celigo.com>
2024-06-20 19:48:51 +0530

    update

--- __tests__/admin_suite/TC_T451.ts
+++ __tests__/admin_suite/TC_T451.ts
@@ -4,7 +4,7 @@ test.describe(`IO_T451 To verify Pull from Integration dropdown field in both pr
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test(`@Env-All @Zephyr-IO_T451 C41570 To verify Pull from Integration dropdown field in both production and sandbox account(Admin access)`, async ({
+  test(`@Env-All @Zephyr-IO_T451 C41570 To verify Pull from Integration dropdown field in the production account(Admin access)`, async ({
     page,
     io
   }) => {
@@ -12,7 +12,7 @@ test.describe(`IO_T451 To verify Pull from Integration dropdown field in both pr
     await io.flowBuilder.click(selectors.homePagePO.CREATE_NEW_INTEGRATION);
     await io.flowBuilder.fill(
       selectors.basePagePO.INPUT_NAME_SELECTOR,
-      "IO_T451 Integration"
+      "IO_T451 Integration production"
     );
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.flowBuilder.click(selectors.homePagePO.CLONE_INTEGRATION);
@@ -22,6 +22,28 @@ test.describe(`IO_T451 To verify Pull from Integration dropdown field in both pr
     await io.flowBuilder.clickByText("Revisions");
     await io.flowBuilder.click(selectors.integrationPagePO.CREATE_PULL);
     const childIntegration = await io.flowBuilder.getText(selectors.integrationPagePO.CHILD_INTEGRATION_NAME);
-    await io.assert.expectToBeTrue(childIntegration.toString().includes("IO_T451 Integration"), "integration name doesn't match");
+    await io.assert.expectToBeTrue(childIntegration.toString().includes("IO_T451 Integration production"), "integration name doesn't match");
+  });
+  test(`@Env-All @Zephyr-IO_T451 C41570 To verify Pull from Integration dropdown field in the sandbox account(Admin access)`, async ({
+    page,
+    io
+  }) => {
+    await io.homePage.waitForElementAttached(selectors.homePagePO.SANDBOX_BUTTON);
+    await io.homePage.click(selectors.homePagePO.SANDBOX_BUTTON);
+    await io.flowBuilder.clickByText("Create");
+    await io.flowBuilder.click(selectors.homePagePO.CREATE_NEW_INTEGRATION);
+    await io.flowBuilder.fill(
+      selectors.basePagePO.INPUT_NAME_SELECTOR,
+      "IO_T451 Integration sandbox"
+    );
+    await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+    await io.flowBuilder.click(selectors.homePagePO.CLONE_INTEGRATION);
+    await io.flowBuilder.click(selectors.homePagePO.CLONE_INTEGRATION_BUTTON);
+    await io.flowBuilder.click(selectors.basePagePO.INSTALL);
+    await io.myAccountPage.loadingTime();
+    await io.flowBuilder.clickByText("Revisions");
+    await io.flowBuilder.click(selectors.integrationPagePO.CREATE_PULL);
+    const childIntegration = await io.flowBuilder.getText(selectors.integrationPagePO.CHILD_INTEGRATION_NAME);
+    await io.assert.expectToBeTrue(childIntegration.toString().includes("IO_T451 Integration sandbox"), "integration name doesn't match");
   });
 });
