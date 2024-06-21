596951e93bffee5d979f3c52128a9f0fffaa40d8
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2024-06-21 18:06:07 +0530

    fixed connection issues

--- __tests__/uiux_suite01/connections/TC_C111349.ts
+++ __tests__/uiux_suite01/connections/TC_C111349.ts
@@ -20,27 +20,21 @@ test.describe("TC_C111349_C111342   Verify the message when api failed to fetch 
     await io.flowBuilder.click(selectors.connectionsPagePO.IMPORT_RECORDS);
     await io.flowBuilder.click(selectors.basePagePO.CREATE_FROM_SCRATCH)
     await io.flowBuilder.click(selectors.exportsPagePO.CONNECTIONS_DROPDOWN);
-    await io.flowBuilder.waitForElementAttached(selectors.importPagePO.IMPORT_CREATE_CONNECTION );
+    await io.flowBuilder.waitForElementAttached(selectors.importPagePO.IMPORT_CREATE_CONNECTION);
     await io.flowBuilder.clickByText("POSTGRE OFFLINE CONNECTION - Offline");
     await io.flowBuilder.click(selectors.basePagePO.SAVE);
     await io.flowBuilder.waitForElementAttached(selectors.exportsPagePO.NAME);
     await io.flowBuilder.fill(selectors.exportsPagePO.NAME, "offline");
-    await io.flowBuilder.click(selectors.flowBuilderPagePO.DESTINATIONTABLESEARCHPOSTGRE);
-    await io.importsPage.click(selectors.importPagePO.EXPAND_MOCK_RESPONSE);
-    await io.flowBuilder.waitForElementAttached(selectors.basePagePO.MUI_CIRCULAR_PROGRESS);
-    await page.waitForTimeout(8000);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.DESTINATIONTABLESEARCHPOSTGRE);
     await io.flowBuilder.waitForElementAttached('text="Unable to retrieve table list. Enter a new query or refresh the  page."')
     await io.assert.verifyElementDisplayedByText(
       "Unable to retrieve table list. Enter a new query or refresh the  page.",
       "'Path to file in HTTP response body' is not displayed"
     );
-
-
     await io.importsPage.click(selectors.flowBuilderPagePO.DESTINATIONTABLE_HELPTEXT_ICON);
     await io.assert.verifyElementDisplayedByText(`Select a data destination for bulk inserts. You can bulk insert data into a table.`,
-          'prefix lookup text is incorrect');
-        await io.flowBuilder.click(selectors.connectionsPagePO.HELPTEXT_CLOSE);
+      'prefix lookup text is incorrect');
+    await io.flowBuilder.click(selectors.connectionsPagePO.HELPTEXT_CLOSE);
   });
 }
 )
