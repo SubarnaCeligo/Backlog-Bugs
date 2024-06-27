3b87e6c76503a129ff0ba5a4d967e14f0fb5edb6
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-06-27 19:18:52 +0530

    C98376

--- __tests__/account/TC_C102862.ts
+++ __tests__/account/TC_C102862.ts
@@ -15,10 +15,9 @@ test.describe("C102862 Verify all the available fields in the Forgot your passwo
     await io.assert.verifyElementNotBeFound("input[placeholder=Role]");
 
     await io.signInPage.fill(selectors.basePagePO.NAME, "Test Automation");
-    const randomNumber = Math.floor(Math.random() * 90000) + 10000;
     await io.signInPage.fill(
       selectors.loginPagePO.EMAIL,
-      `testemail+${randomNumber}@celigo.com`
+      `testemail+${Date.now()}@celigo.com`
     );
     await io.signInPage.fill(selectors.loginPagePO.COMPANY, "Celigo");
     await io.signInPage.click(selectors.basePagePO.AGREETOSANDPP);
@@ -29,6 +28,23 @@ test.describe("C102862 Verify all the available fields in the Forgot your passwo
     const elementText = await (
       await page.$("body")
     ).evaluate(el => el.textContent);
+    await io.assert.expectToContainValue(
+      "Account successfully created. Please check your inbox for an account activation link.",
+      elementText,
+      "The page body does not contain account created message"
+    );
+
+    await io.signInPage.clickByText('Switch to EU');
+
+    await io.signInPage.fill(selectors.basePagePO.NAME, "Test Automation");
+    await io.signInPage.fill(
+      selectors.loginPagePO.EMAIL,
+      `testemail+${Date.now()}@celigo.com`
+    );
+    await io.signInPage.fill(selectors.loginPagePO.COMPANY, "Celigo");
+    await io.signInPage.click(selectors.basePagePO.AGREETOSANDPP);
+    await io.signInPage.click(selectors.loginPagePO.SIGN_UP_BUTTON);
+
     await io.assert.expectToContainValue(
       "Account successfully created. Please check your inbox for an account activation link.",
       elementText,
--- __tests__/account/account.spec.ts
+++ __tests__/account/account.spec.ts
@@ -1,7 +1,7 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
   "C102862",
-  "C65455",
+  // "C65455",
   // "C53063",
   // "IO-T28437",
  // "C110068" // C110068 should be at the end.,
