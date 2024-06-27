61ef837720059cb8aa98289f0896e2b750d5c3b5
Bhumesh Domala <bhumesh.domala@celigo.com>
2024-06-28 03:20:06 +0530

    updated test cases

--- __tests__/account/account.spec.ts
+++ __tests__/account/account.spec.ts
@@ -1,11 +1,10 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
   "C102862",
-  // "C65455",
   // "C53063",
   // "IO-T28437",
  // "C110068" // C110068 should be at the end.,
-//  "T32664"
+ "T32664"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
