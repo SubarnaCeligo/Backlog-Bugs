281638ea9fdf3a953b927eeb9dc8386b0f61ce26
Karthika A Nair <karthika.estrella@gmail.com>
2024-06-27 16:25:01 +0530

    C27422

--- __tests__/uiux_suite03/IAF2.0/IAF2.0.spec.ts
+++ __tests__/uiux_suite03/IAF2.0/IAF2.0.spec.ts
@@ -2,6 +2,7 @@ import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
   "C58036",
   "T1127",
+  "C27422"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
     (async () => {
