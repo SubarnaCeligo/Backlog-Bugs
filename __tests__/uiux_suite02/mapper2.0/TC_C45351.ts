3ca98f49d2fd666f97e13f85f50a0de98b0da6db
paresh <paresh.vilas.more@celigo.com>
2024-07-01 01:45:31 +0530

    fixes

--- __tests__/uiux_suite02/mapper2.0/TC_C45351.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_C45351.ts
@@ -11,12 +11,14 @@ test.describe("C45351 Verify the names of the 'Source field' and 'Destination fi
     await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.IMPORT_MAPPING);
     await io.flowBuilder.fill(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER, 'abc');
     await io.flowBuilder.fill(selectors.flowBuilderPagePO.SOURCE_MAPPING_PLACEHOLDER, 'def');
-    await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.ADDBUTTONS);
+    await io.homePage.loadingTime()
+    await page.click(selectors.mappings.MAPPER2DOT0PO.ADDBUTTONS);
+    await page.click(selectors.mappings.MAPPER2DOT0PO.ADDBUTTONS);
     await io.flowBuilder.fillByIndex(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER, 'abc', 1);
     await io.flowBuilder.click(selectors.basePagePO.SAVE);
     await io.assert.verifyElementText(
-      selectors.basePagePO.NOTIFICTION_BAR,
-      "Mapper 2.0: Duplicate destination field(s): abc"
+      selectors.flowGroupingPagePO.ALERT_MESSAGE,
+      "Mapper 2.0: One or more destination field values not entered."
     );
     await io.api.deleteFlowViaAPI(id);
   });
