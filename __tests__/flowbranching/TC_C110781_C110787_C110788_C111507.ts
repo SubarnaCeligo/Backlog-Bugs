4d6bf61f3f226679bff94890bbf1e6cd55b079db
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-07-01 16:04:53 +0530

    png deleted

--- __tests__/flowbranching/TC_C110781_C110787_C110788_C111507.ts
+++ __tests__/flowbranching/TC_C110781_C110787_C110788_C111507.ts
@@ -2,9 +2,8 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import fb from "@testData/flowbranching/TC_C110781_C110787_C110788_C111507.json";
 
-test.describe.configure({ retries: 1 });
-
 test.describe("TC_C110781_C110787_C110788_C111507", () => {
+  test.describe.configure({ retries: 1 });
   let flowid;
   test.afterEach(async ({ io }) => {
     await io.api.deleteFlowViaAPI(flowid);
