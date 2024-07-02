9541382a84ac57f022af2430fbc474da1a00061e
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-07-02 20:51:04 +0530

    change

--- __tests__/monitor_suite/TC_C93703.ts
+++ __tests__/monitor_suite/TC_C93703.ts
@@ -3,7 +3,7 @@ import * as selectors from "@celigo/aut-selectors";
 import testData from "@testData/monitorSuite/monitor_all.json";
 
 test.describe('C93703 Validate that user is able to see "formInit" function wherever “Insert function stub” field is present.(Tile level access)', () => {
-
+    
     test.beforeEach(async ({ io }) => {
         const res = await io.api.putCall(
             `v1/ashares/${process.env.IO_Ashare_ID}`,
@@ -21,7 +21,6 @@ test.describe('C93703 Validate that user is able to see "formInit" function wher
         await io.integrationPage.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, "C68510_DND");
         await io.homePage.loadingTime()
         await io.flowBuilder.clickByText('C68510_DND');
-        await io.homePage.loadingTime()
         await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR);
         await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR, 0);
         await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.EXPORT_HOOK);
