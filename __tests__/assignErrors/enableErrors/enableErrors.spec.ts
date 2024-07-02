27802139ba3693358d4a56bfbe865ec978634c2b
akshayakunduru <akshayaravinder@gamil.com>
2024-07-02 15:48:39 +0530

    changes

--- __tests__/assignErrors/enableErrors/enableErrors.spec.ts
+++ __tests__/assignErrors/enableErrors/enableErrors.spec.ts
@@ -1,9 +1,9 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
   "C118286_C118287_C28390_C118292",
-  // "C119823",
-  // "C119821",
-  // "C119819"
+  "C119823",
+  "C119821",
+  "C119819"
   //This suite is dedicated to assign error epic which is using a dediacted account, please do not add new cases here
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
