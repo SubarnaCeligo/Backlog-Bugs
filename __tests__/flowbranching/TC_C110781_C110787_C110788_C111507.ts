5d46efd1347de2ded422cf31c0802d54b71901e2
akshayakunduru <akshayaravinder@gamil.com>
2024-07-03 11:49:12 +0530

    change

--- __tests__/flowbranching/TC_C110781_C110787_C110788_C111507.ts
+++ __tests__/flowbranching/TC_C110781_C110787_C110788_C111507.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import fb from "@testData/flowbranching/TC_C110781_C110787_C110788_C111507.json";
 
-test.describe("TC_ C110781_C110787_C110788_C111507", () => {
+test.describe("TC_C110781_C110787_C110788_C111507", () => {
   test.describe.configure({ retries: 1 });
   let flowid;
   test.afterEach(async ({ io }) => {
