a7ec34e6ee0579ed5340184abf0e0b4df15b8bfa
srujithceligo <srujith.reddy.aleti@celigo.com>
2024-07-04 19:41:50 +0530

    flow brnaching fixes

--- __tests__/flowbranching/TC_T833.ts
+++ __tests__/flowbranching/TC_T833.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("TC_T833 Verify user is able to edit/delete the Related lists in Salesforce Real time Export", () => {
-  test("@Epic-IO-71413 @Priority-P2 @Zephyr-IO-T833 @Env-All TC_T833 Verify user is able to delete the Related lists in Salesforce Real time Export", async ({ io }) => {
+  test("@Epic-IO-71413 @Priority-P2 @Zephyr-IO-T833 @Env-All TC_T833 Verify user is able to delete the Related lists in Salesforce Real time Export", async ({ io, page }) => {
     await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
     await io.flowBuilder.loadingTime();
     await io.homePage.waitForElementAttached(selectors.basePagePO.TOOLS);
@@ -11,12 +11,15 @@ test.describe("TC_T833 Verify user is able to edit/delete the Related lists in S
     await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.ADD_SOURCE);
     await io.homePage.click(selectors.flowBuilderPagePO.ADD_SOURCE);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.APPLICATION);
-    await io.flowBuilder.clickByTextByIndex("Salesforce", 0,);
+    await io.flowBuilder.clickByTextByIndex("Salesforce", 0);
     await io.homePage.loadingTime();
+    let realTimeData = await page.getByText("Listen for real-time data from source application").nth(0);
+    await realTimeData.waitFor({state:'attached', timeout: 500000});
     await io.flowBuilder.clickByText('Listen for real-time data from source application');
     await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.MARKETPLACE_RESOURCES);
     await io.flowBuilder.clickByTextByIndex("SF Realtime", 0);
     await io.flowBuilder.click(selectors.basePagePO.SAVE);
+    await io.flowBuilder.loadingTime();
     await io.flowBuilder.click(selectors.basePagePO.CONNECTION_DROPDOWN);
     await io.flowBuilder.clickByTextByIndex("TC_T833 SalesForceConnection", 0);
     await io.flowBuilder.click(selectors.flowBuilderPagePO.EDIT_RELATED_LIST);
