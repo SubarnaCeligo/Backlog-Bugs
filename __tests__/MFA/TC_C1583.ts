521b0a3362d5169ba070320a81ae84a92f4a5aab
ladimanish <ladi.manish@celigo.com>
2024-07-10 16:38:48 +0530

    skip case

--- __tests__/MFA/TC_C1583.ts
+++ __tests__/MFA/TC_C1583.ts
@@ -1,8 +1,8 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe.skip("C1583 Verify Transfer Ownership notification shows the integration name", () => {
-  test("@Env-All C1583 Verify Transfer Ownership notification shows the integration name", async ({
+test.describe("C1583 Verify Transfer Ownership notification shows the integration name", () => {
+  test.skip("@Env-All C1583 Verify Transfer Ownership notification shows the integration name", async ({
     io,
     page
   }) => {
