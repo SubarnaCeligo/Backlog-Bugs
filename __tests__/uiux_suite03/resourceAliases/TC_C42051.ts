a634bc0289d1ce9c897a7c5a5656aa9b451a6ccd
Manish <ladi.manish@celigo.com>
2024-07-10 19:10:43 +0530

    Update TC_C42051.ts

--- __tests__/uiux_suite03/resourceAliases/TC_C42051.ts
+++ __tests__/uiux_suite03/resourceAliases/TC_C42051.ts
@@ -5,7 +5,7 @@ test.describe(`C42051 Verify aliases tab should not be shown for IA's`, () => {
     test.beforeEach(async ({ io }) => {
         await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
       });
-    test(`C42051 @Zephyr-IO-T1202 Verify aliases tab should not be shown for IA's`, async({io,page}) => {
+    test(`@Env-All C42051 @Zephyr-IO-T1202 Verify aliases tab should not be shown for IA's`, async({io,page}) => {
        await io.homePage.loadingTime()
        await io.homePage.fill(selectors.homePagePO.SEARCH_INTEGRATION, "IA_DND")
        await io.homePage.loadingTime()
