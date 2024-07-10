15b41e4724ec8584259d471241f82ae36621be3e
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-07-10 14:17:00 +0530

    admin suite fixes

--- __tests__/admin_suite/TC_C41573.ts
+++ __tests__/admin_suite/TC_C41573.ts
@@ -49,13 +49,13 @@ test.describe(`TC_C41573 To verify Pull from Integration dropdown field doesn't 
       );
       await io.flowBuilder.clickButtonByIndex("Close", 0);
       await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
-      await io.flowBuilder.clickByText("TC_C41573 Integration");
+      await io.flowBuilder.clickByTextByIndex("TC_C41573 Integration",0);
       await io.flowBuilder.click(selectors.homePagePO.DELETE_INTEGRATION);
       await io.flowBuilder.click(selectors.basePagePO.DELETE);
-      await io.flowBuilder.clickByText("Clone TC_C41573 Integration");
+      await io.flowBuilder.clickByTextByIndex("Clone TC_C41573 Integration",0);
       await io.flowBuilder.click(selectors.homePagePO.DELETE_INTEGRATION);
       await io.flowBuilder.click(selectors.basePagePO.DELETE);
-      await io.flowBuilder.clickByText("GrandChildIntegration");
+      await io.flowBuilder.clickByTextByIndex("GrandChildIntegration",0);
       await io.flowBuilder.click(selectors.homePagePO.DELETE_INTEGRATION);
       await io.flowBuilder.click(selectors.basePagePO.DELETE);
     }
