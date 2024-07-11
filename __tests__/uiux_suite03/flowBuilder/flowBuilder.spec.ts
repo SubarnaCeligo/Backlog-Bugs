05c578bced148b6460f7e257eea5b1a5e6676684
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-11 12:28:52 +0530

    Moving files

--- __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
+++ __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
@@ -1,8 +1,10 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-    "C102620",
-    "IO-T34774",
-    "IO-T34771",
+  // "C102620",
+  // "IO-T34774",
+  // "IO-T34771",
+  "T17086_T17088",
+  "T18947_T18949_T18948",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
