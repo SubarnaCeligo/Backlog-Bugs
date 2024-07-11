3dab5cbaf0b2cd835fc16ba8e1646c6ded82ef03
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-07-11 11:06:38 +0530

    AUGUST RELEASE BUG

--- __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
+++ __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
@@ -3,6 +3,8 @@ var testCases = [
     "C102620",
     "IO-T34774",
     "IO-T34771",
+    "IO-T34772",
+    "IO-T33536",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
