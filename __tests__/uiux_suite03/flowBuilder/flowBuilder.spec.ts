44ed68ffb17296d9243cb756ded52f20cdf1930d
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-11 13:16:13 +0530

    minor fix

--- __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
+++ __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
@@ -1,8 +1,8 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  // "C102620",
-  // "IO-T34774",
-  // "IO-T34771",
+  "C102620",
+  "IO-T34774",
+  "IO-T34771",
   "T17086_T17088",
   "T18947_T18949_T18948",
 ];
