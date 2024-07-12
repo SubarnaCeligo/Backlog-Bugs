bd566b54f9b840c7347df4d0258fd4beab848c4c
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-12 15:12:40 +0530

    TC_T14276_T14277

--- __tests__/uiux_suite03/templates/templates.spec.ts
+++ __tests__/uiux_suite03/templates/templates.spec.ts
@@ -1,11 +1,12 @@
 import { filterTestCases } from "@celigo/aut-utilities"
 var testCases = [
-  "T1686",
-  "T26356",
-  "IO-T31836",
-  "IO-T32339",
-  "C108853",
-  "C19911",
+  // "T1686",
+  // "T26356",
+  // "IO-T31836",
+  // "IO-T32339",
+  // "C108853",
+  // "C19911",
+  "T14276_T14277",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
