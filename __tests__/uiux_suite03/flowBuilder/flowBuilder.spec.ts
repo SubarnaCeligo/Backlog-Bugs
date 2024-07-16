c93ca929700e9436c89aa9e656af3f51f6cfd534
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-16 12:25:51 +0530

    TC_T24753_T18478_T18480_T12559_T24316

--- __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
+++ __tests__/uiux_suite03/flowBuilder/flowBuilder.spec.ts
@@ -1,17 +1,18 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  "C102620",
-  "IO-T34774",
-  "IO-T34771",
-  "IO-T34772",
-  "IO-T33536",
-  "C102620",
-  "IO-T34774",
-  "IO-T34771",
-  "T17086_T17088",
-  "T18947_T18949_T18948",
-  "IO-T35026",
-  "IO-T34557",
+  // "C102620",
+  // "IO-T34774",
+  // "IO-T34771",
+  // "IO-T34772",
+  // "IO-T33536",
+  // "C102620",
+  // "IO-T34774",
+  // "IO-T34771",
+  // "T17086_T17088",
+  // "T18947_T18949_T18948",
+  // "IO-T35026",
+  // "IO-T34557",
+  "T24316",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
