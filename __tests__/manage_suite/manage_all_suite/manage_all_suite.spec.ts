36d46ca388bce4c0bf75ee4e2f1b9b701653e3a7
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-17 13:17:30 +0530

    uncomment

--- __tests__/manage_suite/manage_all_suite/manage_all_suite.spec.ts
+++ __tests__/manage_suite/manage_all_suite/manage_all_suite.spec.ts
@@ -1,14 +1,14 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-// "C24898",
-// "C24936",
-// "C24943",
-// "C2014",
-// "C1560",
-// "C23055",
-// "T30796",
-// "IOT452",
-// "IOT455",
+"C24898",
+"C24936",
+"C24943",
+"C2014",
+"C1560",
+"C23055",
+"T30796",
+"IOT452",
+"IOT455",
 "T24496",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
