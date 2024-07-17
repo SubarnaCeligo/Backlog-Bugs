d34b648ba1f829d742a8783b34b459f277eb044e
Mayank Omar <mayank.omar@celigo.com>
2024-07-17 15:33:44 +0530

    updated test

--- __tests__/account/account.spec.ts
+++ __tests__/account/account.spec.ts
@@ -5,7 +5,8 @@ var testCases = [
   // "C53063",
   // "IO-T28437",
  // "C110068" // C110068 should be at the end.,
- "T32664"
+ "T32664",
+ 'IOT1455'
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
