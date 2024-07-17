e3ddda0e4b80ffc61259e3e9563ea9fa6a3a6424
Anmolika <anmolikasood@gmail.com>
2024-07-17 16:04:04 +0530

    TC_C53288

---
+++ __tests__/uiux_suite01/exports/TC_C53288.ts
@@ -0,0 +1,28 @@
+import {expect, test} from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("C53288 Verify 'Limit - export a set number of records' for all the adaptors", () => {
+  test("@Env-All @Zephyr-IO-T14432  C53288 Verify 'Limit - export a set number of records' for all the adaptors", async ({io, page}) => {
+    await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+    await io.flowBuilder.loadingTime();
+    await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.CREATEFLOW);
+    await io.homePage.click(selectors.flowBuilderPagePO.CREATEFLOW);
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.ADD_SOURCE);
+    await io.flowBuilder.fill(selectors.settingsPagePO.APP_NAME_INPUT, 'http');
+    await io.flowBuilder.loadingTime();
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.HTTP_FORM_SWITCH);
+    await io.homePage.click(selectors.basePagePO.CREATE_FROM_SCRATCH);
+    await io.exportsPage.fill(selectors.exportsPagePO.NAME, "HTTP_EXPORT_NEW");
+    await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN);
+    await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN,'HTTP ZENDESK CONNECTION');
+    await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTION_OPTION_TEXT);
+      await io.connectionPage.click(selectors.exportsPagePO.HTTP_METHOD);
+        await io.connectionPage.clickByText("GET");
+    await io.flowBuilder.fill(selectors.exportsPagePO.LOOKUP.HTTP_RELATIVE_URI, "/users");
+    await io.exportsPage.click(selectors.exportsPagePO.EXPORT_TYPE_DROPDOWN);
+    await io.homePage.addStep("*** Clicked on Export Type Dropdown ***");
+    await io.exportsPage.clickByText('Limit - export a set number of records');
+    await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
+  });
+});
