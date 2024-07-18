ac9c527f5b030b3e0ae335b8839587fed2d37706
Swaraj Cheguri <swaraj.cheguri@celigo.com>
2024-07-18 15:52:53 +0530

    T5295: download button should open filter

--- __tests__/uiux_suite01/auditLogs/auditLogs.spec.ts
+++ __tests__/uiux_suite01/auditLogs/auditLogs.spec.ts
@@ -1,13 +1,14 @@
 import {filterTestCases} from "@celigo/aut-utilities"
 var testCases = [
-"C59668",
-"C60604",
-"C35067",
-"C68897",
-"C68993",
-"C46938",
-"C46932",
-"C46931"
+// "C59668",
+// "C60604",
+// "C35067",
+// "C68897",
+// "C68993",
+// "C46938",
+// "C46932",
+// "C46931",
+"T5295"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 
