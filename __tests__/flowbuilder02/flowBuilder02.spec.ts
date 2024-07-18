37fd84919b72b5d037bcc36099225cbdd35b61b1
sagna123 <sagnasagnaperala@gamil.com>
2024-07-18 16:58:12 +0530

    latest pull

--- __tests__/flowbuilder02/flowBuilder02.spec.ts
+++ __tests__/flowbuilder02/flowBuilder02.spec.ts
@@ -131,6 +131,7 @@ var testCases = [
   "C59677",
   "T5409",
   "T5388_T5387",
+  //please donot add any more new test cases to this flowbuilder02 suite,already this suite is overloaded when might cause issues during jenkinsrun/ci build
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
