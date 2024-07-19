0e62f95708adfd827e4ec1a5f48fec5766c87405
Anmolika <anmolikasood@gmail.com>
2024-07-19 15:13:54 +0530

    minor fix

--- __tests__/uiux_suite02/flowDebugger/TC_C55454.ts
+++ __tests__/uiux_suite02/flowDebugger/TC_C55454.ts
@@ -1,10 +1,10 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import TC from '../../../testData/inputData/FlowDebugger/C104576.json';
 
 test.describe("C55454 Verify users can enter data manually in the mock output field - export & lookup", () => {
     test("@Env-All @Zephyr-IO-T14454 C55454 Verify users can enter data manually in the mock output field - export & lookup", async ({ io, page }) => {
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.homePage.loadingTime()
         await io.homePage.goToMenu("Tools", "Flow builder");
         await io.homePage.loadingTime()
         await io.flowBuilder.click(selectors.flowBuilderPagePO.ADD_SOURCE);
@@ -13,8 +13,10 @@ test.describe("C55454 Verify users can enter data manually in the mock output fi
         await io.flowBuilder.click(selectors.basePagePO.CREATE_FROM_SCRATCH);
         await io.exportsPage.fill(selectors.exportsPagePO.NAME, "HTTP_EXPORT_NEW");
         await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN);
-        await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTION_INPUT, "HTTP ZENDESK CONNECTION");
-        await io.flowBuilder.clickByText('HTTP ZENDESK CONNECTION');
+        await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTION_INPUT, "HTTP CONNECTION");
+        await io.flowBuilder.click(
+         selectors.connectionsPagePO.CONNECTION_OPTION_TEXT
+       );
         await io.connectionPage.click(selectors.exportsPagePO.HTTP_METHOD);
         await io.connectionPage.clickByText("GET");
         await io.flowBuilder.fill(selectors.exportsPagePO.LOOKUP.HTTP_RELATIVE_URI, "/users");
@@ -54,6 +56,7 @@ test.describe("C55454 Verify users can enter data manually in the mock output fi
           }));
         await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
 
+        await io.homePage.loadingTime()
         await io.flowBuilder.waitForElementAttached(selectors.mappings.ADD_IMPORT);
         await io.flowBuilder.click(selectors.mappings.ADD_IMPORT);
         await io.flowBuilder.fill(selectors.settingsPagePO.APP_NAME_INPUT, 'HTTP');
@@ -62,8 +65,10 @@ test.describe("C55454 Verify users can enter data manually in the mock output fi
         await io.flowBuilder.click(selectors.basePagePO.CREATE_FROM_SCRATCH);
         await io.exportsPage.fill(selectors.exportsPagePO.NAME, "HTTP_EXPORT_NEW");
         await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN);
-        await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTION_INPUT, "HTTP ZENDESK CONNECTION");
-        await io.flowBuilder.clickByText('HTTP ZENDESK CONNECTION');
+        await io.flowBuilder.fill(selectors.connectionsPagePO.CONNECTION_INPUT, "HTTP CONNECTION");
+        await io.flowBuilder.click(
+         selectors.connectionsPagePO.CONNECTION_OPTION_TEXT
+       );
         await io.connectionPage.click(selectors.exportsPagePO.LOOKUP.HTTP_METHOD);
         await io.connectionPage.clickByText("GET");
         await io.flowBuilder.fill(selectors.exportsPagePO.LOOKUP.HTTP_RELATIVE_URI, "/users");
