2c492827321f0c6a9986a7754a2de1a05f635209
prathamsingh <prathamsingh96@gmail.com>
2024-07-19 23:33:18 +0530

    minnor changes

--- __tests__/uiux_suite02/mapper2.0/TC_T34260.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_T34260.ts
@@ -1,21 +1,23 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import T34260 from '@testData/Flows/T32564.json';
+import T34260 from '@testData/Mapper2.0/T34260.json';
 
 test.describe("T34260 Verify for DB adaptors ,mapper 2.0 is unable to detect rows when user has array based input data", () => {
   test("@Env-All @Zephyr-IO-T34260 @Priority-P2 T34260 Verify for DB adaptors ,mapper 2.0 is unable to detect rows when user has array based input data", async ({io, page}) => {
       let id = await io.createResourceFromAPI(T34260, "FLOWS");
-      await io.homePage.navigateTo(
-        process.env["IO_Integration_URL"] + "flowBuilder/" + id
-      );
-      await io.flowBuilder.loadingTime()
+
       await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
       await io.flowBuilder.loadingTime();
       
       await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.CHANGEOUTPUTFORMAT);
       await io.flowBuilder.loadingTime();
-      await io.flowBuilder.clickByText("Create destination rows [ ] from source rows [ ]");
-      await io.flowBuilder.loadingTime();
+    //   await io.flowBuilder.clickByText("Create destination rows [ ] from source rows [ ]");
+
+      await page.pause();
+
+      const text = await page.locator("text = 'Create destination rows [ ] from source rows [ ]'");
+
+      console.log("text", text);
      
       await io.assert.verifyElementDisplayedByText(
         "Create destination rows [ ] from source rows [ ]",
