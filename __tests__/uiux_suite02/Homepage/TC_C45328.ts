f17317e522277037d97f80918e5250cb74b61dca
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2024-07-19 11:17:15 +0530

    Fixed homePage test cases

--- __tests__/uiux_suite02/Homepage/TC_C45328.ts
+++ __tests__/uiux_suite02/Homepage/TC_C45328.ts
@@ -7,7 +7,7 @@ test.describe("C45328_Verify whether user is able to vote on features and add su
         await io.homePage.loadingTime()
         await io.homePage.selectTabInHelperMenu("Product portal")
         await io.homePage.loadingTime()
-        const iframe = page.frameLocator(selectors.homePagePO.IFRAME_PRODUCT_PORTAL);
+        const iframe = page.frameLocator('[title="Product portal"]');
         await iframe.locator(selectors.myAccountPagePO.PRODUCT_PORTAL_SUBMIT_BUTTON).click();
         await iframe.locator(selectors.homePagePO.SUBMIT_IDEA_COMMENT).fill('Test')
         await iframe.getByText('Nice-to-have').click()
