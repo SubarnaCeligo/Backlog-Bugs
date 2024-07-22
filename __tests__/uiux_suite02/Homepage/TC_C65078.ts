5852d02d7908db683683ddd45f9bc0d9d0b8cc70
vijayakrishna-celigo <vijayakrishna.gopalam@celigo.com>
2024-07-22 16:39:50 +0530

    Fixed Homepage issues

--- __tests__/uiux_suite02/Homepage/TC_C65078.ts
+++ __tests__/uiux_suite02/Homepage/TC_C65078.ts
@@ -7,9 +7,10 @@ test.describe('C65078 Verify sub-menu option of "My account" is showing after ho
     page
   }) => {
     await io.homePage.navigateTo(process.env["IO_UI_CONNECTOR_URL"] + "home");
-    await io.homePage.loadingTime()
+    await io.homePage.waitForElementAttached(selectors.homePagePO.HOME_PROFILE_MENU);
     await io.homePage.click(selectors.homePagePO.HOME_PROFILE_MENU);
     await io.homePage.addStep("Hovered over 'Account'");
+    await io.homePage.waitForElementAttached(selectors.homePagePO.SIGN_OUT);
     await io.assert.verifyElementIsDisplayed(
       selectors.homePagePO.SIGN_OUT,
       "Sub-menu is not visible"
