c200cee822ed8ef32f2860590edf86c15b643652
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-07-22 15:22:35 +0530

    change

--- __tests__/uiux_suite03/flowBuilder/TC_IO-T35692.ts
+++ __tests__/uiux_suite03/flowBuilder/TC_IO-T35692.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("@Author_MaheshNivruttiSutar Verify The connection Search icon is not overflowing in the Firefox browser.", () => {
-    test.describe.configure({ retries: 1 })
+    test.describe.configure({ retries: 2 })
     test("@Bug-IO-87619 @Env-QA @Priority-P2 @Zephyr-IO-T35692", async ({ io, page }) => {
         await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
         await io.homePage.loadingTime()
