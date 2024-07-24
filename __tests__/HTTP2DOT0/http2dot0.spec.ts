40f4349e2f134b30e32840b66eb9eec15e5d662c
Revathi-D <revathi.dasari@celigo.com>
2024-07-24 19:08:34 +0530

    specupdate

--- __tests__/HTTP2DOT0/http2dot0.spec.ts
+++ __tests__/HTTP2DOT0/http2dot0.spec.ts
@@ -1,27 +1,27 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  "C58006",
-  "C119138",
-  "CIO58085",
-  "CIO58085two",
-  "C58006",
-  "C66039",
-  //"T27692_T27693_T27694", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
- // "T27696_T27705_T27706", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
-  "T27702_T27703",
- // "T27704", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
- // "T27708_T27709_T27695", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
-  "CIO29647",
- "CT26354T26351T28950T19565",
-  "CIO54642",
-  "CT30323",
-   // "CT30379", //jira connector got unpublished in all the environments by connector team
-  "T31944",
-  "T31950",
-  "T18927_T18928_T18929",
-  "T18925",
-  "T17062",
-  "T24753",
+//   "C58006",
+//   "C119138",
+//   "CIO58085",
+//   "CIO58085two",
+//   "C58006",
+//   "C66039",
+//   //"T27692_T27693_T27694", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
+//  // "T27696_T27705_T27706", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
+//   "T27702_T27703",
+//  // "T27704", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
+//  // "T27708_T27709_T27695", //commenting these cases as there are some issues in the functionality and these will be handled as part of an upcoming epic https://celigo.atlassian.net/browse/IO-77231
+//   "CIO29647",
+//  "CT26354T26351T28950T19565",
+//   "CIO54642",
+//   "CT30323",
+//    // "CT30379", //jira connector got unpublished in all the environments by connector team
+//   "T31944",
+//   "T31950",
+//   "T18927_T18928_T18929",
+//   "T18925",
+//   "T17062",
+//   "T24753",
  "CIO80906"
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
