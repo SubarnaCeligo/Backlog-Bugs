be1d3ef0f535cfbcbaf0b814d3acf00b4e25ce32
Anmolika <anmolikasood@gmail.com>
2024-07-24 14:13:25 +0530

    TC_T20433

--- __tests__/uiux_suite03/flowBuilder/TC_T20433.ts
+++ __tests__/uiux_suite03/flowBuilder/TC_T20433.ts
@@ -5,29 +5,15 @@ import T20433 from "@testData/FlowBuilder/T20433.json";
 
 test.describe("@Zephyr-IO-T20433 C66282 Verify flow cancel information with flow having multiple exports and make connection offline", () => {
     let flowId;
-    test.beforeEach(async ({ io }) => {
-      await io.myAccountPage.navigateTo(io.data.links.CONNECTIONS_PAGE_URL);
-      await io.homePage.click(selectors.connectionsPagePO.CREATE_CONNECTION);
-      await io.flowBuilder.fill(selectors.settingsPagePO.APP_NAME_INPUT, 'Http');
-      await io.flowBuilder.click(selectors.connectionsPagePO.HTTP_CNNECTOR);
-      await io.flowBuilder.loadingTime();
-      await io.homePage.fill(selectors.basePagePO.NAME, 'Offline Connection');
-      await io.homePage.fill(selectors.connectionsPagePO.BASE_URI_INPUT, 'https://anyrandom.url');
-      await io.homePage.click(selectors.connectionsPagePO.SLACK_AUTH_TYPE);
-      await io.homePage.clickByText("Custom");
-      await io.homePage.click(selectors.basePagePO.SAVE_AND_CLOSE);
-      await io.flowBuilder.loadingTime();
-      // await io.homePage.clickByIndex(selectors.basePagePO.SAVE,1);
-    });
     test("@Env-All @Zephyr-IO-T20433 C66282 Verify flow cancel information with flow having multiple exports and make connection offline", async ({ io, page }) => {
        
     flowId = await io.createResourceFromAPI(T20433, "FLOWS");
     await io.flowBuilder.click(selectors.flowBuilderPagePO.RUN_FLOW);
     await io.homePage.addStep("Running the flow");
     await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.EXPORT,0);
-    await io.flowBuilder.fill(selectors.basePagePO.CONNECTION_DROPDOWN, 'Offline Connection');
+    await io.flowBuilder.fill(selectors.basePagePO.CONNECTION_DROPDOWN, '- Offline');
     await io.flowBuilder.waitForElementAttached(selectors.connectionsPagePO.CONNECTIONS_DROPDOWN_LIST);
-    await io.flowBuilder.clickByTextByIndex('Offline Connection', 0);
+    await io.flowBuilder.click(selectors.connectionsPagePO.CONNECTION_OPTION_TEXT);
     await io.flowBuilder.click(selectors.basePagePO.SAVE_AND_CLOSE);
     await io.homePage.click(selectors.flowBuilderPagePO.REPLACE);
     await io.flowBuilder.loadingTime();
