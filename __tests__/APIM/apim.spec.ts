b2eba8c578b13bef223718997a5c7e384edf36c7
Vikram Thakre <vikram.waman.thakre@celigo.com>
2024-07-29 14:00:12 +0530

    uncomment

--- __tests__/APIM/apim.spec.ts
+++ __tests__/APIM/apim.spec.ts
@@ -1,11 +1,11 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-  // "C59279",
-  "C59255",
-  // "C46760",
-  // "C65053",
-  // "C46760s",
-  // "T28864"
+  "C59279",
+ "C59255",
+  "C46760",
+  "C65053",
+  "C46760s",
+  "T28864"
 
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
