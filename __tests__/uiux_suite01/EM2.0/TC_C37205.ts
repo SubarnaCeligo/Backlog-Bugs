f519535751f7de1f56c77f4aa65d12f3fffde634
prathamsingh <prathamsingh96@gmail.com>
2024-07-29 16:07:44 +0530

    minor fix

--- __tests__/uiux_suite01/EM2.0/TC_C37205.ts
+++ __tests__/uiux_suite01/EM2.0/TC_C37205.ts
@@ -1,29 +1,35 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-import C51628 from "@testData/EM2.0/C51628.json";
 
 test.describe("C37205 Verify on integration level success should be clickable", () => {
-  let id;
-  test.afterEach(async ({ io }) => {
-    await io.api.deleteFlowsWithId(id);
+  test.beforeEach(async ({ io }) => {
+    await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
   test("@Zephyr-IO-T7533 @Env-All @Priority-P2 C37205 Verify on integration level success should be clickable", async ({
     io,
-    page,
-    context
+    page
   }) => {
-    id = await io.createResourceFromAPI(C51628, "FLOWS");
-    await io.api.runBatchFlowViaAPI("TC_C51623", id);
+    await io.homePage.loadingTime();
+    await io.homePage.navigateTo(process.env.IO_Integration_URL);
 
-    await io.flowBuilder.loadingTime();
-    await io.flowBuilder.loadingTime();
+    // Search for a flow
+    await io.integrationPage.waitForElementAttached(
+      selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR
+    );
+    await io.integrationPage.fill(
+      selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR,
+      "C108673_DND"
+    );
+    await io.homePage.loadingTime();
+    await io.flowBuilder.clickByText("Success");
 
-    //Open errors dashborad
-    await io.flowBuilder.click(
-      selectors.flowBuilderPagePO.ACCOUNT_DASHBOARD_OPEN_ERRORS
-    );
-    await io.flowBuilder.waitForElementAttached(
-      selectors.em2DotOLineGraphPO.ASSIGN_ERRORS
-    );
+    let tableBody = await page.$$("table > tbody > tr");
+    let count = await tableBody.length;
+    for (let i = 8; i <= count; i++) {
+      await tableBody[i].scrollIntoViewIfNeeded();
+      if (await io.homePage.isVisible(`tbody tr:has-text('Success')`)) {
+        await io.flowBuilder.clickByText("Success");
+      }
+    }
   });
 });
