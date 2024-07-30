54df598791a458116d74965f090d7c1a6107754a
Swaraj Cheguri <swaraj.cheguri@celigo.com>
2024-07-30 15:35:03 +0530

    updated with correct code

--- __tests__/email_suite/TC_T5842.ts
+++ __tests__/email_suite/TC_T5842.ts
@@ -1,4 +1,4 @@
-import {  test } from "@celigo/ui-core-automation";
+import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("T5842 - To verify 404 page is not displayed when clicked on the access token link provided in email.", () => {
@@ -11,18 +11,23 @@ test.describe("T5842 - To verify 404 page is not displayed when clicked on the a
     await io.homePage.goToMenu("Resources", "API tokens");
     await io.assert.verifyElementDisplayedByText('Create API token', "App crashed");
     await page.getByText("Create API token").click();
-    const res = await io.emailVal.getLinkFromEmail("ALERT: API token was created",false,
-    "pwqa1");
-    console.log({res});
+
     await page.locator('[data-test="name"]').getByRole('textbox').click();
     await page.locator(selectors.basePagePO.ADD_NAME).getByRole('textbox').fill('T5842');
     await page.locator(selectors.connectionsPagePO.AUTOPURGETOKEN).getByLabel('Please select').click();
     await page.getByRole('menuitem', { name: '1 Hour' }).click();
-     await page.pause();
+
     await page.locator(selectors.basePagePO.SAVE_AND_CLOSE).click();
-    await page.pause();
+
+    await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+    const res = await io.emailVal.getLinkFromEmail("ALERT: API token was created", true,
+      "pwqa1");
+
+    expect(res.length).toBeGreaterThan(0);
+    await io.homePage.navigateTo(res[0].split("><pre>")[0]);
+    await io.assert.verifyElementDisplayedByText('Create API token', "Page not directed to the correct page");
   });
 
 }
-)  
+)
 
