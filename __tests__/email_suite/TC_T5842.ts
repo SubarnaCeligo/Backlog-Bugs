5b9de9d718ebee21ee6e47d0d2cbe4ba18a2f87f
Swaraj Cheguri <swaraj.cheguri@celigo.com>
2024-07-30 15:54:02 +0530

    udpated code

--- __tests__/email_suite/TC_T5842.ts
+++ __tests__/email_suite/TC_T5842.ts
@@ -24,8 +24,8 @@ test.describe("T5842 - To verify 404 page is not displayed when clicked on the a
       "pwqa1");
 
     expect(res.length).toBeGreaterThan(0);
-    await io.homePage.navigateTo(res[0].split("><pre>")[0]);
-    await io.assert.verifyElementDisplayedByText('Create API token', "Page not directed to the correct page");
+    const response = await page.goto(res[0].split("><pre>")[0]);
+    expect(response.status()).not.toEqual(404);
   });
 
 }
