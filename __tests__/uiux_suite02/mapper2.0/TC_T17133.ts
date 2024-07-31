b60401d0a25e3a34fe3c37e15ca83fea8076bcee
prathamsingh <prathamsingh96@gmail.com>
2024-07-31 16:44:16 +0530

    fixes

--- __tests__/uiux_suite02/mapper2.0/TC_T17133.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_T17133.ts
@@ -18,7 +18,7 @@ test.describe("C59462 Verify Whenever the input filter is updated, we clear the 
 
       expect(
         page.getByText(
-          "FALSE:"
+          "TRUE:"
         )
       ).toBeVisible();
   });
