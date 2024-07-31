b249b51e45ae226d2aad4060f4419d2e839b0631
Apoorv Kautilya <apoorv.kautilya@celigo.com>
2024-07-31 13:29:33 +0530

    Added T18016

--- __tests__/uiux_suite03/flowBuilder/TC_T2419_T18016.ts
+++ __tests__/uiux_suite03/flowBuilder/TC_T2419_T18016.ts
@@ -2,13 +2,13 @@ import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import flow from "@testData/FlowBuilder/T2419.json";
 
-test.describe("Verify the tooltips on the source mapping drop down", () => {
+test.describe("Verify the tooltips on the source mapping drop down and verify Dynamic lookup is not present in Field mapping type", () => {
   let id;
   test.afterEach(async ({ io }) => {
     await io.api.deleteFlowViaAPI(id);
   });
 
-  test("@Zephyr-IO-T2419 @Env-All @Epic-IO-86262 @Priority-P2 - Verify the tooltips on the source mapping drop down", async ({
+  test("@Zephyr-IO-T2419 @Zephyr-IO-T18016 @Env-All @Epic-IO-86262 @Priority-P2 - Verify the tooltips on the source mapping drop down and verify Dynamic lookup is not present in Field mapping type", async ({
     io,
     page
   }) => {
@@ -35,5 +35,14 @@ test.describe("Verify the tooltips on the source mapping drop down", () => {
       cursor,
       "Tooltip is not displayed"
     );
+    
+    await io.flowBuilder.clickByIndex(selectors.flowBuilderPagePO.SETTINGS_MARIADB, 1);
+    await io.flowBuilder.waitForElementAttached(selectors.mappings.MAPPER2DOT0PO.MAPPING_TYPE);
+    await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.MAPPING_TYPE);
+    await io.assert.verifyElementContainsText(
+      selectors.importPagePO.DYNAMICLOOKUP,
+      "Lookup - static"
+    );
+    
   });
 });
