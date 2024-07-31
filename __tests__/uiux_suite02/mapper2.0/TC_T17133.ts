3c3d6f15b3781807bd96f1580394c2b7a6440d74
prathamsingh <prathamsingh96@gmail.com>
2024-07-31 13:18:56 +0530

    fixes

--- __tests__/uiux_suite02/mapper2.0/TC_T17133.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_T17133.ts
@@ -6,7 +6,7 @@ test.describe("C59462 Verify Whenever the input filter is updated, we clear the 
   test("@Zephyr-IO-T17133 @Env-All @Priority-P2 C59462 Verify Whenever the input filter is updated, we clear the cached data. Hence if user opens any AFE of that lookup/import say outputFilter for lookup, it fetches again based on the updated input filter.", async ({io, page}) => {
       await io.createResourceFromAPI(T17133, "FLOWS");
 
-      await io.flowBuilder.click(selectors.importPagePO.INPUT_FILTER_HOTSPOT_ICON);
+      await io.flowBuilder.click(selectors.importPagePO.INPUT_FILTER_ICON);
 
       await io.homePage.loadingTime();
 
