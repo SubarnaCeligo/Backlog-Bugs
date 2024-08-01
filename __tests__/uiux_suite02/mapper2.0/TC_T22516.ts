e39277a7db531a742613767775ec6ce35b9f1e11
prathamsingh <prathamsingh96@gmail.com>
2024-08-02 00:07:30 +0530

    code changes

--- __tests__/uiux_suite02/mapper2.0/TC_T22516.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_T22516.ts
@@ -14,33 +14,97 @@ test.describe(`C52210 Verify the UI error pop up when there are multiple errors 
     await io.flowBuilder.click(
       selectors.mappings.MAPPER2DOT0PO.DESTINATIONFIELDS
     );
-
     await page.keyboard.type("company");
 
     await io.flowBuilder.click(
-        selectors.mappings.MAPPER2DOT0PO.DESTINATIONFIELDS_DATA_TYPE
+      selectors.mappings.MAPPER2DOT0PO.DESTINATIONFIELDS_DATA_TYPE
     );
-
-    const close1 = await page.$$(selectors.mappings.MAPPER2DOT0PO.DATATYPESLISTITEM);
-    await close1[2].click();
-
+    const DT1 = await page.$$(
+      selectors.mappings.MAPPER2DOT0PO.DATATYPESLISTITEM
+    );
+    await DT1[5].click();
 
     await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.SOURCEFIELDS);
     await page.keyboard.type("$[*].id");
 
-    await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.ADD_FIELD_MAPPING);
+    await page.waitForSelector('button > span:text("string")');
+    await page.click('button > span:text("string")');
+    const cl1 = await page.$$("li > div> span");
+    await cl1[cl1.length - 6].click();
+
+    await io.flowBuilder.click(
+      selectors.mappings.MAPPER2DOT0PO.ADD_FIELD_MAPPING
+    );
+
+    await page
+      .locator(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER)
+      .nth(1)
+      .fill("test");
+    await io.flowBuilder.click(
+      selectors.mappings.MAPPER2DOT0PO.DESTINATIONFIELDS_DATA_TYPE
+    );
+    const DT2 = await page.$$(selectors.mappings.MAPPER2DOT0PO.DATATYPESLISTITEM);
+    await DT2[5].click();
+
+    await page
+      .locator(selectors.mappings.MAPPER2DOT0PO.SOURCEFIELDS)
+      .nth(1)
+      .click();
+    await page.keyboard.type("$[*].recordType");
 
     await io.homePage.loadingTime();
+
+    await page.waitForSelector('button > span:text("string")');
+    await page.click('button > span:text("string")');
+    const cl2 = await page.$$("li > div> span");
+    await cl2[cl2.length - 4].click();
+
+    await io.flowBuilder.click(
+      selectors.mappings.MAPPER2DOT0PO.ADD_FIELD_MAPPING
+    );
+
+    await page
+      .locator(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER)
+      .nth(2)
+      .fill("name");
+    await io.flowBuilder.click(
+      selectors.mappings.MAPPER2DOT0PO.DESTINATIONFIELDS_DATA_TYPE
+    );
+    const DT3 = await page.$$(
+      selectors.mappings.MAPPER2DOT0PO.DATATYPESLISTITEM
+    );
+    await DT3[5].click();
+
+    await page
+      .locator(selectors.mappings.MAPPER2DOT0PO.SOURCEFIELDS)
+      .nth(2)
+      .click();
+    await page.keyboard.type("$[*].ID"); 
+
+    await io.homePage.loadingTime();
+
+    await page.waitForSelector('button > span:text("string")');
+    await page.click('button > span:text("string")');
+    const cl3 = await page.$$("li > div> span");
+    await cl3[cl3.length - 3].click();
+
+    await page.pause();
 
     await io.flowBuilder.click(selectors.mappings.MAPPER2DOT0PO.PREVIEW);
     await io.flowBuilder.addStep("Clicked on 'Preview'");
 
     await io.homePage.loadingTime();
-    let text = (await io.homePage.getText(selectors.mappings.MAPPER2DOT0PO.CODE_PANEL)).toString();
+    let text = (
+      await io.homePage.getText(selectors.mappings.MAPPER2DOT0PO.CODE_PANEL)
+    ).toString();
 
     let result = false;
-    if(text.includes(`Message:·company:·You·can't·map·stringarray·(source)·to·boolean·(destination)`)){
-        result = true;
+    if (
+      text.includes(
+        `Message:·company:·You·can't·map·stringarray·(source)·to·boolean·(destination)`
+      )
+    ) {
+      result = true;
     }
     await expect(result).toBeTruthy();
   });
