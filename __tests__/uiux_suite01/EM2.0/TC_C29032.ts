579ecc27273592c261a6ea889630d002cfe3e9b9
prathamsingh <prathamsingh96@gmail.com>
2024-08-02 10:07:53 +0530

    minor change

--- __tests__/uiux_suite01/EM2.0/TC_C29032.ts
+++ __tests__/uiux_suite01/EM2.0/TC_C29032.ts
@@ -5,10 +5,10 @@ test.describe("C29032 Auto-resolved feature will be counted in the auto-resolved
 test("@Env-All @Zephyr-IO-T6413 @Priority-P2 C29032 Auto-resolved feature will be counted in the auto-resolved column", async ({io, page}) => {
     await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL)
     await io.homePage.clickByText("Automation Flows")
-    await io.homePage.waitForElementAttached("text='JWTDOCUSIGN_DND'")
-    await io.homePage.clickByText("JWTDOCUSIGN_DND")
+    await io.homePage.waitForElementAttached("text='Newflow_DND'")
+    await io.homePage.clickByText("Newflow_DND")
     await io.flowBuilder.loadingTime();
     const autoResolvedErrorCount = await io.flowBuilder.getText(selectors.flowBuilderPagePO.AUTORESOLVED)
-    expect(autoResolvedErrorCount).toBe("1")
+    expect(autoResolvedErrorCount).toBe("3")
 });
 });
