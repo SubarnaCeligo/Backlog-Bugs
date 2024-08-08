2858ae967673eab96c1df4ab14abd663e307425a
sagna123 <sagnasagnaperala@gamil.com>
2024-08-08 19:26:11 +0530

    commented out a test case

--- __tests__/account/account.spec.ts
+++ __tests__/account/account.spec.ts
@@ -1,7 +1,7 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
 "C65455",
-"C102862",
+//"C102862",
   // "C53063",
   // "IO-T28437",
  // "C110068" // C110068 should be at the end.,
