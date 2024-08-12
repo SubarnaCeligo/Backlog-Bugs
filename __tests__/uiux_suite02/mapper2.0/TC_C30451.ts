7d3a0fcd4ca98db134d8bcc01eb6752ef3c11459
Revathi-D <revathi.dasari@celigo.com>
2024-08-12 15:57:02 +0530

    fixes

--- __tests__/uiux_suite02/mapper2.0/TC_C30451.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_C30451.ts
@@ -10,17 +10,22 @@ test.describe(`TC_C30451 - Missing fields on Lookup Criteria for Subscription re
     await io.createResourceFromAPI(testData, "FLOWS");
     await io.connectionPage.addStep("Flow created");
     await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT);
+    await io.flowBuilder.loadingTime();
     await io.connectionPage.addStep("Clicked on Import");
     await io.flowBuilder.click(selectors.connectionsPagePO.DELETE_CONNECTION);
+    await io.flowBuilder.loadingTime();
     await io.connectionPage.addStep("Clicked on Delete");
     await io.importsPage.click(selectors.basePagePO.NETSUITE_INTERNAL_LOOKUP);
+    await io.flowBuilder.loadingTime();
     await io.connectionPage.addStep("Clicked on Netsuite Internal Lookup");
     await io.flowBuilder.click(selectors.flowBranchingPO.GROUP_RULE);
+    await io.flowBuilder.loadingTime();
     await io.connectionPage.addStep("Clicked on dropdown for all the subscription record types");
 
     const options = await page.$$(`${selectors.flowBuilderPagePO.FILTER_CONTAINER} option`);
     const length = options?.length || 0;
     expect(length).not.toBe(0);
+
     await io.connectionPage.addStep("Verified subscription record types dropwdown has fields");
 
     await io.assert.verifyElementContainsText(selectors.flowBuilderPagePO.FILTER_CONTAINER,"Advance Renewal Period");
