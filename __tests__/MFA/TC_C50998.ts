7d48444e9fde1980345e7158f89b5212560b809d
Anmolika <anmolikasood@gmail.com>
2024-08-14 12:03:15 +0530

    fix

--- __tests__/MFA/TC_C50998.ts
+++ __tests__/MFA/TC_C50998.ts
@@ -17,19 +17,19 @@ test.describe(`Zephyr-IO-T19634 C50998`, () => {
     if(isToggleEnable) {
         await io.flowBuilder.clickByIndex(
             selectors.flowBuilderPagePO.HELP_TEXT_ICON,
-            6
+            5
           );
           const trustedDevicesPopup = await page.$(selectors.myAccountPagePO.HELP_BUBBLE);
           const trustedDevicesHelpText = await trustedDevicesPopup.textContent();
-          expect(trustedDevicesHelpText).toContain('Check this box to remove the ability for users to add trusted devices. Allowing users to add their own computer/browser to the trusted device list eliminates the need for them to enter their six-digit authentication code every time they sign in from the computer and browser they commonly use. If checked, MFA users can’t add trusted devices and must enter the six-digit code from their authentication device every time they sign in regardless of which device they use.');
+          expect(trustedDevicesHelpText).toContain('Do not allow trusted devicesCheck this box to remove the ability for users to add trusted devices. Allowing users to add their own computer/browser to the trusted device list eliminates the need for them to enter their six-digit authentication code every time they sign in from the computer and browser they commonly use. If checked, MFA users can’t add trusted devices and must enter the six-digit code from their authentication device every time they sign in regardless of which device they use.');
           await io.flowBuilder.click(selectors.connectionsPagePO.HELPTEXT_CLOSE);
           await io.flowBuilder.clickByIndex(
             selectors.flowBuilderPagePO.HELP_TEXT_ICON,
-            7
+            6
           );
           const daysPopup = await page.$(selectors.myAccountPagePO.HELP_BUBBLE);
           const daysHelpText = await daysPopup.textContent();
-          expect(daysHelpText).toContain('If left blank, MFA users who sign in from trusted devices are only required to enter their six-digit authentication code after 90 days of inactivity from the trusted device. To extend or reduce the default, enter the number of days of inactivity before re-authentication is required.');
+          expect(daysHelpText).toContain('Number of days until MFA is required again for trusted devicesIf left blank, MFA users who sign in from trusted devices are only required to enter their six-digit authentication code after 90 days of inactivity from the trusted device. To extend or reduce the default, enter the number of days of inactivity before re-authentication is required.');
           
     }
     else{
@@ -39,7 +39,7 @@ test.describe(`Zephyr-IO-T19634 C50998`, () => {
       );
       const trustedDevicesPopup = await page.$(selectors.myAccountPagePO.HELP_BUBBLE);
       const trustedDevicesHelpText = await trustedDevicesPopup.textContent();
-      expect(trustedDevicesHelpText).toContain('Check this box to remove the ability for users to add trusted devices. Allowing users to add their own computer/browser to the trusted device list eliminates the need for them to enter their six-digit authentication code every time they sign in from the computer and browser they commonly use. If checked, MFA users can’t add trusted devices and must enter the six-digit code from their authentication device every time they sign in regardless of which device they use.');
+      expect(trustedDevicesHelpText).toContain('Do not allow trusted devicesCheck this box to remove the ability for users to add trusted devices. Allowing users to add their own computer/browser to the trusted device list eliminates the need for them to enter their six-digit authentication code every time they sign in from the computer and browser they commonly use. If checked, MFA users can’t add trusted devices and must enter the six-digit code from their authentication device every time they sign in regardless of which device they use.');
       await io.flowBuilder.click(selectors.connectionsPagePO.HELPTEXT_CLOSE);
       await io.flowBuilder.clickByIndex(
         selectors.flowBuilderPagePO.HELP_TEXT_ICON,
@@ -47,7 +47,7 @@ test.describe(`Zephyr-IO-T19634 C50998`, () => {
       );
       const daysPopup = await page.$(selectors.myAccountPagePO.HELP_BUBBLE);
       const daysHelpText = await daysPopup.textContent();
-      expect(daysHelpText).toContain('If left blank, MFA users who sign in from trusted devices are only required to enter their six-digit authentication code after 90 days of inactivity from the trusted device. To extend or reduce the default, enter the number of days of inactivity before re-authentication is required.');
+      expect(daysHelpText).toContain('Number of days until MFA is required again for trusted devicesIf left blank, MFA users who sign in from trusted devices are only required to enter their six-digit authentication code after 90 days of inactivity from the trusted device. To extend or reduce the default, enter the number of days of inactivity before re-authentication is required.');
     }
   });
 });
