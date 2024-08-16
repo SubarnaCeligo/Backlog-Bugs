2917ffb8b41e5d74e55ce8d15a3ba748a27a07ad
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-08-16 12:10:31 +0530

    flowBranching fixes

--- __tests__/uiux_suite01/flowBranching/TC_C68565.ts
+++ __tests__/uiux_suite01/flowBranching/TC_C68565.ts
@@ -13,57 +13,96 @@ test.describe(`C68565 Verify user is upload the integration zip file having line
     const res2 = await io.api.deleteCall(
       `v1/integrations/${testdata.firstString}`,
     );
+    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
+    await io.homePage.goToMenu("Resources", "Templates");
+    await io.homePage.waitForElementAttached(selectors.basePagePO.SEARCH_RECYCLEBIN);
+    await io.homePage.fill(selectors.basePagePO.SEARCH_RECYCLEBIN, "C68565_Automation");
+    await io.homePage.loadingTime();
+
+    //Unpublish the template
+    let isPublished = await io.homePage.isVisible(selectors.flowBuilderPagePO.MAP_ZOOM_TO_FIT);
+    if (isPublished) {
+      await io.homePage.click(selectors.basePagePO.TEMPLATE_PUBLISH_UNPUBLISH);
+      await io.homePage.loadingTime();
+      await io.homePage.click(selectors.integrationPagePO.UNPUBLISH);
+      await io.homePage.loadingTime();
+    }
 
   });
   test(`@Env-All @Zephyr-IO-T17466 C68565 Verify user is upload the integration zip file having linear flow (with input/output/mapping/hooks defined) in the template ad able to install the template`, async ({
     io,
     page
   }) => {
-    await io.homePage.clickByText("Resources");
-    await io.homePage.clickByText("Templates");
-    await io.homePage.waitForElementAttached(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR)
-    await io.homePage.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, "temp3_DND")
-    await io.homePage.waitForElementAttached(`:has-text("temp3") ${selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU}`);
-    await io.homePage.click(`tbody tr:has-text("temp3_DND") ${selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU}`);
-    await io.homePage.clickByText("Upload template zip");
+    await io.homePage.goToMenu("Resources", "Templates");
+    await io.homePage.waitForElementAttached(selectors.basePagePO.SEARCH_RECYCLEBIN);
+    await io.homePage.fill(selectors.basePagePO.SEARCH_RECYCLEBIN, "C68565_Automation");
+    await io.homePage.waitForElementAttached(
+      `:has-text("C68565_Automation") ${selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU}`
+    );
+    await io.homePage.click(
+      `tbody tr:has-text("C68565_Automation") ${selectors.flowBuilderPagePO.OPEN_ACTIONS_MENU}`
+    );
+    await io.homePage.click('[data-test="uploadTemplateZip"]');
     const fileInput = await page.$(selectors.basePagePO.UPLOAD_FILE);
-    await fileInput.setInputFiles('testData/inputData/Templates/C68565.zip');
-    await io.homePage.clickByText("Marketplace")
-    await io.marketplacePage.fill('[placeholder="Search marketplace"]', "temp3_DND")
-    await io.marketplacePage.click(selectors.homePagePO.INSTALL_TEMPLATE);
-    await io.marketplacePage.clickByText("Install now")
-    await io.marketplacePage.waitForElementAttached(selectors.integrationPagePO.SETUP_INTEGRATION_CONFIGURE_BUTTON)
+    await fileInput.setInputFiles("testData/inputData/Templates/C68565_NEW.zip");
+    await io.homePage.loadingTime();
+
+    //Publish the template
+    let isPublished = await io.homePage.isVisible(selectors.flowBuilderPagePO.MAP_ZOOM_TO_FIT);
+    if (!isPublished) {
+      await io.homePage.click(selectors.basePagePO.TEMPLATE_PUBLISH_UNPUBLISH);
+      await io.homePage.loadingTime();
+      await io.homePage.click(selectors.integrationPagePO.PUBLISH);
+      await io.homePage.loadingTime();
+      //Verify that toggle is checked
+      await io.assert.verifyElementAttributeContainsText(selectors.basePagePO.TEMPLATE_TOGGLE, 'class', 'react-toggle--checked');
+    }
+
+    //Go to marketplace
+    await io.homePage.click(selectors.basePagePO.MARKETPLACE);
+    await io.homePage.reloadPage();
+    await io.marketplacePage.fill(
+      selectors.marketplacePagePO.SEARCH_MARKETPLACE,
+      "C68565_Automation"
+    );
+    await io.homePage.loadingTime();
+    await io.marketplacePage.waitForElementAttached(selectors.homePagePO.INSTALL_TEMPLATE);
+    await io.marketplacePage.clickByIndex(selectors.homePagePO.INSTALL_TEMPLATE, 0);
+    await io.marketplacePage.clickByText("Install now");
+    await io.marketplacePage.waitForElementAttached(
+      selectors.integrationPagePO.SETUP_INTEGRATION_CONFIGURE_BUTTON
+    );
+
 
     await io.homePage.click(
       selectors.integrationPagePO.SETUP_INTEGRATION_CONFIGURE_BUTTON
     );
-    await io.homePage.clickByText("Use existing connection");
-    await io.homePage.clickByText("Please select");
-    // await page
-    //   .locator(selectors.connectionsPagePO.CONNECTION_LIST_MODAL)
-    //   .getByText("FTP CONNECTION").first()
-    //   .click();
+    await io.homePage.click(selectors.connectionsPagePO.EXISTING);
+    await io.homePage.click(selectors.exportsPagePO.CREATE_SELECT_CONNECTION);
     let connMap = await io.api.loadConnections();
     var connId = connMap.get("FTP CONNECTION");
     await io.connectionPage.selectTextfromDropDown(page, connId)
+    await io.homePage.loadingTime();
     await io.connectionPage.click(selectors.basePagePO.SAVE);
+    await io.homePage.loadingTime();
+
     await io.homePage.click(selectors.basePagePO.INSTALL);
-    await io.homePage.loadingTime()
-    //await io.homePage.clickByText('C68565');
-    await io.homePage.click("//a[contains(text(), 'C68565')]");
-    const linkUrl = await page.url();
-    const match = linkUrl.match(/\/integrations\/(\w+)\/flowBuilder\/(\w+)/);
-    testdata.firstString = match[1];
-    testdata.secondString = match[2];
-    await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
-    await io.homePage.loadingTime()
-    await io.homePage.waitForElementAttached(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR)
-    await io.homePage.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, 'C68565')
-   //Commenting below code as per the BUG https://celigo.atlassian.net/browse/IO-78629 
-    // only integration would be shown not the flows in home page, this is expected behaviour
+    await io.homePage.loadingTime();
+
+    //Verify if the flow is installed
+    await io.integrationPage.clickByIndex('td a', 0);
+
+    //get num of bubbles
+    await io.flowBuilder.waitForElementAttached(selectors.flowBuilderPagePO.TRANSFER);
+    await io.flowBuilder.click(selectors.flowBuilderPagePO.MAP_ZOOM_TO_FIT);
+    let bubbles = await page.$$(selectors.flowBuilderPagePO.TRANSFER);
+    await io.assert.expectToBeValue((bubbles.length).toString(), "2", 'Template is not fully installed');
+
+    //Verify data
+    await io.assert.verifyElementIsDisplayed(selectors.basePagePO.OUTPUTFILTER, "Export filter not copied");
+    await io.assert.verifyElementIsDisplayed(selectors.exportsPagePO.EXPORT_HOOKS, "Export hooks not copied");
+    await io.assert.verifyElementIsDisplayed(selectors.basePagePO.INPUTFILTER, "inputFilter not copied");
+    await io.assert.verifyElementIsDisplayed(selectors.flowBuilderPagePO.IMPORT_MAPPINGS, "importMapping not copied");
     
-    // await io.homePage.waitForElementAttached("text='C68545'")
-    // const flow = await io.homePage.isVisible("text='C68545'")
-    // await io.assert.expectToBeValue(flow.toString(), 'true', "Template flow not found")
   });
 });
