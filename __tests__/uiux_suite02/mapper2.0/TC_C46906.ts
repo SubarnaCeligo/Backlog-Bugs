4f50d0a22a03fc1a2f6bce3497c28fba4298588f
Revathi-D <revathi.dasari@celigo.com>
2024-08-21 10:11:31 +0530

    smallfix

--- __tests__/uiux_suite02/mapper2.0/TC_C46906.ts
+++ __tests__/uiux_suite02/mapper2.0/TC_C46906.ts
@@ -7,18 +7,10 @@ test.describe("C46906 Verify by adding the duplicate mappings for 'destination r
       await io.createResourceFromAPI(C46906, "FLOWS");
       await page.locator(selectors.flowBuilderPagePO.ADD_DATA_PROCESSOR).nth(1).click();
       await io.flowBuilder.click(selectors.flowBuilderPagePO.IMPORT_MAPPINGS);
-
       await page.locator(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER).nth(0).fill('test');
       await page.getByLabel('add',{exact: true}).nth(0).click();
       await page.locator(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER).nth(1).fill('test');
-      await page.getByLabel('add',{exact: true}).nth(1).click();
-      await io.homePage.loadingTime();
-      await page.locator(selectors.flowBuilderPagePO.DESTINATION_MAPPING_PLACEHOLDER).nth(2).fill('test');
-      //await page.getByLabel('add',{exact: true}).nth(0).click(); 
       await io.importsPage.click(selectors.mappings.MAPPER2DOT0PO.PREVIEW);
-      await io.homePage.loadingTime();
-      await io.importsPage.click("[data-test='save']");
-      await io.homePage.loadingTime();
       const errorMessage = await page.locator('#error').evaluate(e => {
                 // @ts-ignore
                 const editor = ace.edit(e);
