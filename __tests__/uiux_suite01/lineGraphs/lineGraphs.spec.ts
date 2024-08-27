25e77f86976e59286341fd941019a857e0ec8c78
akshayakunduru <akshayaravinder@gamil.com>
2024-08-27 15:00:59 +0530

    date ranger test cases

--- __tests__/uiux_suite01/lineGraphs/lineGraphs.spec.ts
+++ __tests__/uiux_suite01/lineGraphs/lineGraphs.spec.ts
@@ -1,5 +1,7 @@
 import { filterTestCases } from "@celigo/aut-utilities"
-var testCases = ["T2976"];
+var testCases = [
+  // "T2976",
+  "T3866"];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
   await filterTestCases(testCases, flakycases, "lineGraphs")
