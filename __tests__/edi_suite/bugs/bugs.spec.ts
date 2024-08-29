725c4d41d89062e6590a4ed991252a8282e00be5
prathamsingh <prathamsingh96@gmail.com>
2024-08-29 15:48:38 +0530

    IO-T37752

--- __tests__/edi_suite/bugs/bugs.spec.ts
+++ __tests__/edi_suite/bugs/bugs.spec.ts
@@ -1,10 +1,11 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
-    "T29350",
-    "T30612",
-    "T32334",
-    "T32428",
-    "T37293"
+    // "T29350",
+    // "T30612",
+    // "T32334",
+    // "T32428",
+    // "T37293",
+    "T37752",
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
 (async () => {
