2d7028ee6e376e1d2171dd1da5cefb2563c34f1b
phani1123 <sai.phanindra.godavarthi@celigo.com>
2024-09-04 12:20:06 +0530

    text size fix

--- __tests__/user_notify/TC_C118366_C118376_C118375_C118371_C118373_C118367.ts
+++ __tests__/user_notify/TC_C118366_C118376_C118375_C118371_C118373_C118367.ts
@@ -34,7 +34,7 @@ test.describe("C118366_C118376_C118375_C118371_C118373_C118367", () => {
     await io.flowBuilder.waitForElementAttached(selectors.em2DotOLineGraphPO.ASSIGNEE_PILL);
 
     const assigneePill = (await io.flowBuilder.getText(selectors.em2DotOLineGraphPO.ASSIGNEE_PILL)).toString();
-    await io.assert.expectToContainValue('Pending - qaautomation1+a', assigneePill, 'Error is not assigned');
+    await io.assert.expectToContainValue('Pending - qaautomation', assigneePill, 'Error is not assigned');
     await io.flowBuilder.hover(selectors.em2DotOLineGraphPO.ASSIGNEE_PILL, 0, false);
     await io.flowBuilder.waitForElementAttached(selectors.em2DotOLineGraphPO.ASSIGNEE_PILL_HOVERTEXT);
     const hoverText = (await io.flowBuilder.getText(selectors.em2DotOLineGraphPO.ASSIGNEE_PILL_HOVERTEXT)).toString();
