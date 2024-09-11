c10dbc86d38595b75529e730638d565523687f6f
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-09-11 22:05:43 +0530

    added missing tag

--- __tests__/edi_suite/filter/TC_T37558.ts
+++ __tests__/edi_suite/filter/TC_T37558.ts
@@ -5,7 +5,7 @@ test.describe("@Author-Shriti S Verify filter tooling section is added on the ED
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
   });
-  test("@Env-QA @Epic-IO-89826 @Priority-P2 @Zephyr-IO-T37558 Verify filter tooling section is added on the EDI documents dashboard", async ({ io, page }) => {
+  test("@Env-QA @Env-IAQA @Epic-IO-89826 @Priority-P2 @Zephyr-IO-T37558 Verify filter tooling section is added on the EDI documents dashboard", async ({ io, page }) => {
 
     //Go to Dashboard
     await io.myAccountPage.navigateTo(process.env["IO_UI_CONNECTOR_URL"] + "dashboard");
