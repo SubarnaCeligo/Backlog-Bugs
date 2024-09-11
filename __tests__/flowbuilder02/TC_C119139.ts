f908400c07bdf06df31bc371587260361592d671
sagna123 <sagnasagnaperala@gamil.com>
2024-09-11 16:02:15 +0530

    adding zephyr tag

--- __tests__/flowbuilder02/TC_C119139.ts
+++ __tests__/flowbuilder02/TC_C119139.ts
@@ -2,7 +2,7 @@ import { test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe("C119139 Verify After refreshing page also Dashboard showing Running/completed flows as expected.)", () => {
-    test("C119139 Verify After refreshing page also Dashboard showing Running/completed flows as expected. @Env-All @Priority-P2", async ({ io, page }) => {
+    test("C119139 Verify After refreshing page also Dashboard showing Running/completed flows as expected.@Zephyr-IO-T17974 @Env-All @Priority-P2 ", async ({ io, page }) => {
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
         await io.homePage.addStep("*** Navigated to home page ***");
         await io.homePage.click(selectors.basePagePO.DASHBOARD);
