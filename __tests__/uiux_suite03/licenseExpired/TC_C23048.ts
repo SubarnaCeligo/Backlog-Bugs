864a8a92915fe7542d92987b2fd84da2e1a09791
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-09-17 14:36:14 +0530

    Fix for licenseExpired suite

--- __tests__/uiux_suite03/licenseExpired/TC_C23048.ts
+++ __tests__/uiux_suite03/licenseExpired/TC_C23048.ts
@@ -1,10 +1,23 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
+import payload from "@testData/edi_suite/LicenseUpdate.json"
 
 
 test.describe(
   "C23048 Customers should be presented with a button on the home page “Your license has expired. Renew now.”. ",
   () => {
+    test.beforeEach(async ({ io }) => {
+      //get licensce ID
+      let licenses = await io.api.getCall('v1/licenses');
+      let licenseID = licenses[0]._id;
+      let type = licenses[0].type;
+  
+      // Update license
+      let endPoint = 'v1/test/licenses/' + licenseID;
+      payload.type = type;
+      payload.expires = "2020-01-01T00:00:00.000Z",
+      await io.api.putCall(endPoint, payload);
+    });
     test("@Env-All @Zephyr-IO-T1431 C23048 Customers should be presented with a button on the home page “Your license has expired. Renew now.”. ", async ({
       io,
       page
@@ -19,6 +32,18 @@ test.describe(
         const renewButtonClick = await io.homePage.isVisible('text="Request to renew subscription"');
         await io.assert.expectToBeValue(expiredMsg.toString(), "true", "Renew button not clickable");
 
+    });
+    test.afterEach(async ({ io }) => {
+      //get licensce ID
+      let licenses = await io.api.getCall('v1/licenses');
+      let licenseID = licenses[0]._id;
+      let type = licenses[0].type;
+  
+      // Update license
+      let endPoint = 'v1/test/licenses/' + licenseID;
+      payload.expires = "2050-01-01T00:00:00.000Z",
+      payload.type = type;
+      await io.api.putCall(endPoint, payload);
     });
   }
 );
--- __tests__/uiux_suite03/licenseExpired/TC_C23055.ts
+++ __tests__/uiux_suite03/licenseExpired/TC_C23055.ts
@@ -1,10 +1,22 @@
 import { expect, test } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
-
+import payload from "@testData/edi_suite/LicenseUpdate.json"
 
 test.describe(
   "C23055 Verify Account Renewal for Monitor or manage user ",
   () => {
+    test.beforeEach(async ({ io }) => {
+      //get licensce ID
+      let licenses = await io.api.getCall('v1/licenses');
+      let licenseID = licenses[0]._id;
+      let type = licenses[0].type;
+  
+      // Update license
+      let endPoint = 'v1/test/licenses/' + licenseID;
+      payload.type = type;
+      payload.expires = "2020-01-01T00:00:00.000Z",
+      await io.api.putCall(endPoint, payload);
+    });
     test("@Env-All @Zephyr-IO-T1433 C23055 Verify Account Renewal for Monitor or manage user ", async ({
       io,
       page
@@ -19,6 +31,18 @@ test.describe(
         const renewButtonClick = await io.homePage.isVisible('text="We will contact you to renew your subscription."');
         await io.assert.expectToBeValue(expiredMsg.toString(), "true", "error message not displayed");
 
+    });
+    test.afterEach(async ({ io }) => {
+      //get licensce ID
+      let licenses = await io.api.getCall('v1/licenses');
+      let licenseID = licenses[0]._id;
+      let type = licenses[0].type;
+  
+      // Update license
+      let endPoint = 'v1/test/licenses/' + licenseID;
+      payload.expires = "2050-01-01T00:00:00.000Z",
+      payload.type = type;
+      await io.api.putCall(endPoint, payload);
     });
   }
 );
