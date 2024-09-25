1c9d50ac174b293c43cf3dc74f53195605f33c2f
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-09-25 18:51:42 +0530

    testing tag validator issue

--- __tests__/flowBuilder/TC_C27342.ts
+++ __tests__/flowBuilder/TC_C27342.ts
@@ -14,7 +14,7 @@ test.describe("TC_C27342 | TC_C27334 ", () => {
     test.step("** Deleted flow **", async () => { });
   });
 
-  test("@Env-All @Zephyr-IO-T2823 | Verify Tracekey is overidden with the Override trace key template value when given field reference. ", async ({ io, page }, testInfo) => {
+  test("@Env-All @Zephyr-IO-T2823 |  Verify Tracekey is overidden with the Override trace key template value when given field reference. ", async ({ io, page }, testInfo) => {
     await io.api.createImpOrExpAndFlowsThruAPI(TC);
     flowId = await io.api.getFlowId(TC.name);
     await test.step(
@@ -76,7 +76,7 @@ test.describe("TC_C27342 | TC_C27334 ", () => {
     expect(customTraceKey).toContain(expectedTracekey);
   });
 
-  test('@Env-All @Zephyr-IO-T2819 | Verify the Page processor import form does not have "Override trace key template" field in the Advanced section when the one to many is set to false', async ({ io, page }) => {
+  test('@Env-All @Zephyr-IO-T2819| Verify the Page processor import form does not have "Override trace key template" field in the Advanced section when the one to many is set to false', async ({ io, page }) => {
     await io.api.createImpOrExpAndFlowsThruAPI(TC);
     flowId = await io.api.getFlowId(TC.name);
     await io.flowBuilderDashboard.navigateToFlowBuilderInFB(flowId, true);
