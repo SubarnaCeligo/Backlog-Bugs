72613f2e66d75b82085bee12ad7b01f1c1847e42
Vikram Thakre <vikram.waman.thakre@celigo.com>
2024-09-25 14:10:14 +0530

    Suite_fixes

--- __tests__/uiux_suite03/iclientFramework/TC_C64881.ts
+++ __tests__/uiux_suite03/iclientFramework/TC_C64881.ts
@@ -2,24 +2,31 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
 test.describe(`C64881 Verify Iclient under HTTP`, () => {
-  test(`@Env-All @Zephyr-IO-T17564 C64881 Verify Iclient under HTTP`, async ({ io, page }) => {
+  test(`@Env-All @Zephyr-IO-T17564 C64881 @Zephyr-IO-T17599 C68273  Verify Iclient under HTTP verify application field under HTTP iclient is disabled`, async ({ io, page }) => {
     await io.connectionPage.navigateTo(io.data.links.CONNECTIONS_PAGE_URL);
     await io.connectionPage.click(selectors.connectionsPagePO.CREATE_CONNECTION);
     await io.connectionPage.click(selectors.connectionsPagePO.GUSTO_CONNECTION);
     await io.flowBuilder.click(selectors.connectionsPagePO.CREATE_ICLIENT);
     await io.homePage.loadingTime();
-    await io.exportsPage.click(selectors.importPagePO.HTTP_IMPORT);
+    await io.connectionPage.waitForElementAttached(selectors.basePagePO.HTTP_2DOT0)
+    await io.myAccountPage.clickByIndex(selectors.basePagePO.HTTP_2DOT0,1);
+    await io.homePage.loadingTime();
     await expect(
       page.locator(selectors.connectionsPagePO.AUTHORIZATION_URL)
     ).not.toHaveValue("");
     await io.connectionPage.addStep(
       "Verified 'Authorization URL' is pre-configured"
     );
+    await io.homePage.loadingTime();
     await expect(
       page.locator(selectors.connectionsPagePO.ACCESS_TOKEN_URL)
     ).not.toHaveValue("");
     await io.connectionPage.addStep(
       "Verified 'Access token URL' is pre-configured"
     );
+    await expect(
+      page.locator(selectors.connectionsPagePO.CONNECTION_SEARCH)
+    ).toBeDisabled();
+    await io.connectionPage.addStep("Verified application field is disabled");
   });
 });
--- __tests__/uiux_suite03/iclientFramework/iclientFramework.spec.ts
+++ __tests__/uiux_suite03/iclientFramework/iclientFramework.spec.ts
@@ -2,11 +2,10 @@
 import { filterTestCases } from "@celigo/aut-utilities";
 var testCases = [
 
-  "C64881",
-  "C64882",
-  "C68273",
-  "C64900",
-  "IO-T27292"
+  //"C64881",
+  // "C64882",
+   "C64900",
+  // "IO-T27292"
 
 ];
 var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
