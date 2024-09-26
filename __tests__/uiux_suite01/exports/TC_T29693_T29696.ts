6ebbd9d744da28add191a32be3c6a32a43166971
Revathi-D <revathi.dasari@celigo.com>
2024-09-26 22:25:36 +0530

    exportfixes

--- __tests__/uiux_suite01/exports/TC_T29693_T29696.ts
+++ __tests__/uiux_suite01/exports/TC_T29693_T29696.ts
@@ -12,10 +12,14 @@ test.describe("TC_T29693_T29696_Test to validate that user is able to run a flow
         await io.flowBuilder.loadingTime();
         await io.homePage.clickByText("AFE_AUTOSUGGESTIONS_mapper2.0_DND");
         await io.homePage.addStep("*** Opened the integration ***");
-        await io.homePage.clickByText("synapse_export_flow_run_DND");
+        await io.homePage.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, "synapse_export_flow_run_DND");
+        await io.homePage.addStep("*** Searched for the flow ***");
+        await io.flowBuilder.loadingTime();
+        await io.homePage.clickByText("Azure_synapse_export_flow_run_DND");
         await io.homePage.addStep("*** Opened the flow ***");
         await io.flowBuilder.loadingTime();
         await io.assert.expectToBeTrue(await (await page.$(selectors.flowBuilderPagePO.EXPORT_TRANSFORMATION)).isVisible(), "Transaformation rules are not visible");
+        await io.flowBuilder.loadingTime();
         await io.homePage.click(selectors.flowBuilderPagePO.RUN_FLOW);
         await io.homePage.addStep("*** Ran the flow ***");
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
