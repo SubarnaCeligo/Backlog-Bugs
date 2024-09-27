fc2728db4233a1267f04b25c0a482a11406f8fff
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-09-27 11:46:36 +0530

    fixing flaky cases in uiux-connections

--- __tests__/uiux_suite01/connections/TC_C23866.ts
+++ __tests__/uiux_suite01/connections/TC_C23866.ts
@@ -1,6 +1,7 @@
 
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
+import { randomString } from "@celigo/aut-utilities";
 
 
 test.describe("C23866 To Verify user is able to edit and save the 'Freshworks CRM' Connection", () => {
@@ -9,8 +10,8 @@ test.describe("C23866 To Verify user is able to edit and save the 'Freshworks CR
       await io.homePage.fill(selectors.flowBuilderPagePO.SEARCH, "FRESHSALES_TEST");
       await io.homePage.waitForElementAttached(selectors.flowBuilderPagePO.JOBS_ROWS);
       await io.homePage.clickByTextByIndex("FRESHSALES_TEST", 0);
-      await io.connectionPage.waitForElementAttached(selectors.basePagePO.HTTP_SBDOMAIN)
-      await io.connectionPage.fill(selectors.basePagePO.HTTP_SBDOMAIN,"subdomain_value")
+      await io.connectionPage.waitForElementAttached(selectors.connectionsPagePO.FRESHWORKS_SUBDOMAIN);
+      await io.connectionPage.fill(selectors.connectionsPagePO.FRESHWORKS_SUBDOMAIN,"subdomain_value_" + randomString(6));
       await io.connectionPage.waitForElementAttached(selectors.basePagePO.HTTP_APIKEY)
       await io.connectionPage.fill(selectors.basePagePO.HTTP_APIKEY,"APIkey_value")
       await io.connectionPage.waitForElementAttached(selectors.basePagePO.HTTP_APIKEY)
