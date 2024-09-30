49573066247fb6c3320883ea89669b0253f2d86b
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-09-30 16:28:08 +0530

    changes

--- __tests__/uiux_suite01/auditLogs/TC_IO-T38522.ts
+++ __tests__/uiux_suite01/auditLogs/TC_IO-T38522.ts
@@ -10,33 +10,29 @@ test.describe("@Author_MaheshNivruttiSutar Verify new field added on the differe
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
         await io.flowBuilder.loadingTime();
 
-        //Resource drawer:
-        const sections = ["Connections", "Imports", "Exports", "Agents", "iClients", "API tokens", "Stacks", "My APIs", "Scripts"];
-        for (const section of sections) {
-            await io.homePage.loadingTime();
-            await io.homePage.goToMenu("Resources", section);
-            await io.homePage.loadingTime();
-            await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
-            await io.homePage.click(selectors.basePagePO.AUDITLOG);
-            await io.homePage.loadingTime();
-            await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
-            await io.homePage.click(selectors.flowBuilderPagePO.CLOSEPOPUP);
-        }
+        // //Resource drawer:
+        // const sections = ["Connections", "Imports", "Exports", "Agents", "iClients", "API tokens", "Stacks", "My APIs", "Scripts"];
+        // for (const section of sections) {
+        //     await io.homePage.loadingTime();
+        //     await io.homePage.goToMenu("Resources", section);
+        //     await io.homePage.loadingTime();
+        //     await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+        //     await io.homePage.click(selectors.basePagePO.AUDITLOG);
+        //     await io.homePage.loadingTime();
+        //     await page.getByText("Last 30 days").waitFor({ state: "visible", timeout: 360000 });
+        //     await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
+        //     await io.homePage.click(selectors.flowBuilderPagePO.CLOSEPOPUP);
+        // }
 
         //Flowbuilder page
         await io.homePage.loadingTime();
-        await io.homePage.goToMenu("Tools", "Flow builder");
-        await io.homePage.loadingTime();
-        await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
-        await io.homePage.loadingTime();
-        await io.homePage.click(selectors.flowBuilderPagePO.AUDIT_LOGS);
-        await io.homePage.loadingTime();
-        await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
-        await io.homePage.loadingTime();
         await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+        await page.getByText("Integration flows").waitFor({ state: "visible", timeout: 360000 });
         await io.homePage.loadingTime();
-        await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+        await io.homePage.clickButtonByIndex(selectors.flowBuilderPagePO.DEBUG_LOG_ACTIONS_MENU, 1);
+        await io.homePage.loadingTime();
         await io.homePage.click(selectors.basePagePO.AUDITLOG);
+        await page.getByText("Last 30 days").waitFor({ state: "visible", timeout: 360000 });
         await io.homePage.loadingTime();
         await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
         await io.homePage.click(selectors.flowBuilderPagePO.CLOSEPOPUP);
@@ -44,6 +40,7 @@ test.describe("@Author_MaheshNivruttiSutar Verify new field added on the differe
 
         //Integration Page
         await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
+        await page.getByText("Last 30 days").waitFor({ state: "visible", timeout: 360000 });
         await io.homePage.loadingTime();
         await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
 
@@ -52,6 +49,7 @@ test.describe("@Author_MaheshNivruttiSutar Verify new field added on the differe
         await io.homePage.loadingTime();
         await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
         await io.homePage.click(selectors.basePagePO.AUDITLOG);
+        await page.getByText("Last 30 days").waitFor({ state: "visible", timeout: 360000 });
         await io.homePage.loadingTime();
         await io.assert.verifyElementIsDisplayed('[data-test="AuditLogDateFilter"]', "Element is not displayed properly on");
     });
