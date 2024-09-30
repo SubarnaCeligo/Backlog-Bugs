055b7644f550d2a425b658ce4e24d81153472df5
MaheshNivruttiSutar <maheshsutar254@gmail.com>
2024-09-30 11:30:11 +0530

    IO-86043 Epic

--- __tests__/manage_suite/manage_all_suite/TC_C1560.ts
+++ __tests__/manage_suite/manage_all_suite/TC_C1560.ts
@@ -14,16 +14,19 @@ test.describe(`C1560 Veirfy,Generating and uploading a template zip file in user
     await io.homePage.navigateTo(
         `${io.data.links.HOME_PAGE_URL}/installIntegration`
       );
+      await io.flowBuilder.loadingTime();
       await io.homePage.addStep(
         "Navigated to install integration page (/home/installIntegration)"
       );
       const fileChooserPromise = page.waitForEvent("filechooser");
       await io.homePage.clickByText("Choose file");
+      await io.flowBuilder.loadingTime();
       const fileChooser = await fileChooserPromise;
       await fileChooser.setFiles("testData/inputData/Templates/C68650copy.zip");
       await io.homePage.addStep("Uploaded integration zip file");
       await io.homePage.clickByText("Install integration");
       await io.homePage.click(selectors.basePagePO.DIALOG_PROCEED_BUTTON);
+      await io.flowBuilder.loadingTime();
       await io.homePage.navigateTo(io.data.links.HOME_PAGE_URL);
       await io.homePage.loadingTime();
       await io.flowBuilder.fill(selectors.integrationPagePO.INTEGRATION_PAGE_SEARCH_BAR, 'TC100370_FTP_TO_FTP');
@@ -31,6 +34,6 @@ test.describe(`C1560 Veirfy,Generating and uploading a template zip file in user
       const flow = await io.homePage.isVisible("text='TC100370_FTP_TO_FTP'")
       await io.assert.expectToBeValue(flow.toString(),'true', "Template flow not found")
 
-     
+
   });
 });
---
+++ __tests__/uiux_suite01/auditLogs/TC_IO-T38522.ts
@@ -0,0 +1,58 @@
+import { expect, test } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+
+test.describe("@Author_MaheshNivruttiSutar Verify new field added on the different places to filter Audit log with dates'", () => {
+    test.beforeEach(async ({ io }) => {
+        await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.flowBuilder.loadingTime();
+    });
+    test("@Bug-IO-97121 @Priority-P2 @Env-QA @Zephyr-IO-T38522'", async ({ io, page, context }) => {
+        await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.flowBuilder.loadingTime();
+
+        //Resource drawer:
+        const sections = ["Connections", "Imports", "Exports", "Agents", "iClients", "API tokens", "Stacks", "My APIs", "Scripts"];
+        for (const section of sections) {
+            await io.homePage.loadingTime();
+            await io.homePage.goToMenu("Resources", section);
+            await io.homePage.loadingTime();
+            await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+            await io.homePage.click(selectors.basePagePO.AUDITLOG);
+            await io.homePage.loadingTime();
+            await io.assert.verifyElementIsDisplayed(selectors.homePagePO.AUDIT_FILTER, "Element is not displayed properly on");
+            await io.homePage.click(selectors.flowBuilderPagePO.CLOSEPOPUP);
+        }
+
+        //Flowbuilder page
+        await io.homePage.loadingTime();
+        await io.homePage.goToMenu("Tools", "Flow builder");
+        await io.homePage.loadingTime();
+        await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+        await io.homePage.loadingTime();
+        await io.homePage.click(selectors.flowBuilderPagePO.AUDIT_LOGS);
+        await io.homePage.loadingTime();
+        await io.assert.verifyElementIsDisplayed(selectors.homePagePO.AUDIT_FILTER, "Element is not displayed properly on");
+        await io.homePage.loadingTime();
+        await io.homePage.navigateTo(process.env["IO_Integration_URL"]);
+        await io.homePage.loadingTime();
+        await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+        await io.homePage.click(selectors.basePagePO.AUDITLOG);
+        await io.homePage.loadingTime();
+        await io.assert.verifyElementIsDisplayed(selectors.homePagePO.AUDIT_FILTER, "Element is not displayed properly on");
+        await io.homePage.click(selectors.flowBuilderPagePO.CLOSEPOPUP);
+        await io.homePage.loadingTime();
+
+        //Integration Page
+        await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
+        await io.homePage.loadingTime();
+        await io.assert.verifyElementIsDisplayed(selectors.homePagePO.AUDIT_FILTER, "Element is not displayed properly on");
+
+        //Register connection page
+        await io.homePage.click(selectors.basePagePO.CONNECTIONS);
+        await io.homePage.loadingTime();
+        await io.homePage.clickButtonByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU, 0);
+        await io.homePage.click(selectors.basePagePO.AUDITLOG);
+        await io.homePage.loadingTime();
+        await io.assert.verifyElementIsDisplayed(selectors.homePagePO.AUDIT_FILTER, "Element is not displayed properly on");
+    });
+});
