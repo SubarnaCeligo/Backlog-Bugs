795db0f038a070e4114945d3797a42c6fe9c0790
ShritiSCeligoIO <shriti.b.s@celigo.com>
2024-10-09 17:41:04 +0530

    added 1 more account switching case

--- __tests__/account_switching/TC_T17814.ts
+++ __tests__/account_switching/TC_T17814.ts
@@ -6,7 +6,7 @@ test.describe("@Author-Shriti S Verify audit logs when user switches from accoun
     test.beforeEach(async ({ io }) => {
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
     });
-    test("@Env-All @Priority-P2 @Zephyr-IO-T38740 Verify audit logs when user switches from account 'A' to account 'B'.", async ({ io, page }) => {
+    test("@Env-All @Priority-P2 @Zephyr-IO-T17814 Verify audit logs when user switches from account 'A' to account 'B' (Owner and shared user).", async ({ io, page }) => {
         //Login with owner account
         await io.loginPage.waitForElementAttached(selectors.loginPagePO.EMAIL);
         await io.loginPage.fill(selectors.loginPagePO.EMAIL, process.env["IO_OWNER_EMAIL"]);
@@ -52,30 +52,83 @@ test.describe("@Author-Shriti S Verify audit logs when user switches from accoun
         await io.loginPage.click(selectors.loginPagePO.SIGN_IN_BUTTON);
         await io.homePage.loadingTime();
 
-         //Click Profile
-         await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
-         await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
-         await io.homePage.loadingTime();
+        //Click Profile
+        await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
+        await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
+        await io.homePage.loadingTime();
 
         //Get audit log row1 and row2
         row1 = (await io.homePage.getText(selectors.integrationPagePO.LICENSE_TABLE + ':nth-child(1)')).toString();
         let row2 = (await io.homePage.getText(selectors.integrationPagePO.LICENSE_TABLE + ':nth-child(2)')).toString();
 
-         //Get logged in username
-         await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
-         await io.homePage.click(selectors.myAccountPagePO.PROFILE);
- 
-         let loggenInUserNameUser = await loggenInUserNameLocator.getAttribute('value');
-         await io.homePage.loadingTime();
- 
-         await io.assert.expectToContainValue(loggenInUserNameUser, row1, '');
-         await io.assert.expectToContainValue('Sign in', row1, '');
-         await io.assert.expectToContainValue('IP address', row1, '');
+        //Get logged in username
+        await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
+        await io.homePage.click(selectors.myAccountPagePO.PROFILE);
 
-         await io.assert.expectToContainValue(loggenInUserNameOwner, row2, '');
-         await io.assert.expectToContainValue('Sign out', row2, '');
-         await io.assert.expectToContainValue('IP address', row2, '');
+        let loggenInUserNameUser = await loggenInUserNameLocator.getAttribute('value');
+        await io.homePage.loadingTime();
+
+        await io.assert.expectToContainValue(loggenInUserNameUser, row1, '');
+        await io.assert.expectToContainValue('Sign in', row1, '');
+        await io.assert.expectToContainValue('IP address', row1, '');
+
+        await io.assert.expectToContainValue(loggenInUserNameOwner, row2, '');
+        await io.assert.expectToContainValue('Sign out', row2, '');
+        await io.assert.expectToContainValue('IP address', row2, '');
 
 
     });
+    test("@Env-All @Priority-P2 @Zephyr-IO-T17814 Verify audit logs when user switches from account 'A' to account 'B' (shared user with multiple owners).", async ({ io, page }) => {
+        // Login to a shared user who has access to multiple owner accounts
+        await io.loginPage.waitForElementAttached(selectors.loginPagePO.EMAIL);
+        await io.loginPage.fill(selectors.loginPagePO.EMAIL, process.env["IO_USER1_EMAIL"]);
+        await io.loginPage.waitForElementAttached(selectors.loginPagePO.PASSWORD);
+        await io.loginPage.fill(selectors.loginPagePO.PASSWORD, decrypt(process.env["IO_USER1_PASSWORD"]));
+        await io.loginPage.click(selectors.loginPagePO.SIGN_IN_BUTTON);
+        await io.homePage.loadingTime();
+
+        //Switch to Account A if not already selected.
+        await io.homePage.clickByIndex(selectors.homePagePO.TOP_ROW_BUTTONS, 0);
+
+        await io.homePage.waitForElementAttached(selectors.homePagePO.ACCOUNT_LIST+':nth-child(1)');
+        await io.homePage.click(selectors.homePagePO.ACCOUNT_LIST+':nth-child(1)');
+        await io.homePage.isPageLoaded();
+
+        //Click Profile
+        await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
+        await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
+        await io.homePage.loadingTime();
+
+        //Get audit log row1
+        let row1 = (await io.homePage.getText(selectors.integrationPagePO.LICENSE_TABLE + ':nth-child(1)')).toString();
+
+        //Check audit logs for Signin entry
+        await io.assert.expectToContainValue('Sign in', row1, '');
+        await io.assert.expectToContainValue('IP address', row1, '');
+
+
+        //Switch to Account B
+        await io.homePage.clickByIndex(selectors.homePagePO.TOP_ROW_BUTTONS, 0);
+
+        await io.homePage.waitForElementAttached(selectors.homePagePO.ACCOUNT_LIST+':nth-child(2)');
+        await io.homePage.click(selectors.homePagePO.ACCOUNT_LIST+':nth-child(2)');
+        await io.homePage.isPageLoaded();
+
+        //Check audit logs for Signin and Signout entry
+        await io.homePage.click(selectors.homePagePO.PROFILE_MENU);
+        await io.homePage.click(selectors.myAccountPagePO.AUDIT_LOG);
+        await io.homePage.isPageLoaded();
+
+        //Get audit log row1 and row2
+        row1 = (await io.homePage.getText(selectors.integrationPagePO.LICENSE_TABLE + ':nth-child(1)')).toString();
+        let row2 = (await io.homePage.getText(selectors.integrationPagePO.LICENSE_TABLE + ':nth-child(2)')).toString();
+
+        await io.assert.expectToContainValue('Sign in', row1, '');
+        await io.assert.expectToContainValue('IP address', row1, '');
+
+        await io.assert.expectToContainValue('Sign out', row2, '');
+        await io.assert.expectToContainValue('IP address', row2, '');
+
+    });
+
 });
