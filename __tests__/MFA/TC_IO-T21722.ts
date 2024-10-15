79d37adc4c70f1fb6ecfeccea464ac06c34e7231
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-10-15 17:47:00 +0530

    MFAmodification

--- __tests__/MFA/TC_IO-T21722.ts
+++ __tests__/MFA/TC_IO-T21722.ts
@@ -1,9 +1,9 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe("T17224 Verify if the accounts are listed in the primary account dropdown to which the user has access to.", () => {
-  test("@Env-All @Zephyr-IO-T17224 Verify if the accounts are listed in the primary account dropdown to which the user has access to.", async ({io, page}) => {
-    await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
+test.describe("IO-T21722 Verify if the 'Reset MFA' option is shown across Owner records in the admin account.", () => {
+  test("@Env-All @Zephyr-IO-T21722 Verify if the 'Reset MFA' option is shown across Owner records in the admin account.", async ({io, page}) => {
+     await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
     await io.myAccountPage.waitForElementAttached(selectors.myAccountPagePO.USERS);
     await io.myAccountPage.click(selectors.myAccountPagePO.USERS);
     await io.myAccountPage.clickByIndex(selectors.myAccountPagePO.OPEN_ACTIONSMENU,2);
