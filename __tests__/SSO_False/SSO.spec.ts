f7a6a1634cea9da24ba6c844dfee5665adb56827
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-10-15 12:31:35 +0530

    SSO_False

---
+++ __tests__/SSO_False/SSO.spec.ts
@@ -0,0 +1,10 @@
+require("./TC_IO-T18708")
+// import { filterTestCases } from "@celigo/aut-utilities";
+// var testCases = [
+//     "IO-T18708"
+
+// ];
+// var flakycases = JSON.parse(process.env.FLAKY_TEST_CASES);
+// (async () => {
+//   await filterTestCases(testCases, flakycases);
+// })();
