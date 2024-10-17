0e4cc5be5664138ee09da94aa34ccdaff9db742a
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-10-17 10:44:04 +0530

    added mfa2 without secret

---
+++ __tests__/MFA2/TC_IO-T17241.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@celigo/ui-core-automation";
+import * as selectors from "@celigo/aut-selectors";
+import { decrypt } from "@celigo/aut-utilities";
+
+test.describe("IO-T17241 Verify if the message prompt is shown when the user clicks on 'Need Help?.", () => {
+    test("@Env-All @Zephyr-IO-T17241 Verify if the message prompt is shown when the user clicks on 'Need Help?", async ({ io, page }) => {
+        await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
+        await io.homePage.clickByText('Need help?');
+        expect(await page.getByText('Contact an admin or owner of')).toBeVisible();
+    });
+});
