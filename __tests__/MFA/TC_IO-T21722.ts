0be52811e62489a8e1086282f3950e48e1c58506
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-10-18 14:33:49 +0530

    few modification

--- __tests__/MFA/TC_IO-T21722.ts
+++ __tests__/MFA/TC_IO-T21722.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 
-test.describe("IO-T21722 Verify if the 'Reset MFA' option is shown across Owner records in the admin account.", () => {
+test.describe("Verify if the 'Reset MFA' option is shown across Owner records in the admin account.", () => {
   test("@Env-All @Zephyr-IO-T21722 Verify if the 'Reset MFA' option is shown across Owner records in the admin account.", async ({io, page}) => {
      await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
     await io.myAccountPage.waitForElementAttached(selectors.myAccountPagePO.USERS);
--- __tests__/MFA2/TC_IO-T17241.ts
+++ __tests__/MFA2/TC_IO-T17241.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@celigo/ui-core-automation";
 import * as selectors from "@celigo/aut-selectors";
 import { decrypt } from "@celigo/aut-utilities";
 
-test.describe("IO-T17241 Verify if the message prompt is shown when the user clicks on 'Need Help?.", () => {
+test.describe("Verify if the message prompt is shown when the user clicks on 'Need Help?.", () => {
     test("@Env-All @Zephyr-IO-T17241 Verify if the message prompt is shown when the user clicks on 'Need Help?", async ({ io, page }) => {
         await io.myAccountPage.navigateTo(io.data.links.HOME_PAGE_URL);
         await io.homePage.clickByText('Need help?');
