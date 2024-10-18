bb9fa2ba9d048127da815bdea28900cb484a1949
SubarnaCeligo <subarna.ghatak@celigo.com>
2024-10-18 17:52:29 +0530

    did few modification

--- __tests__/account/TC_C752.ts
+++ __tests__/account/TC_C752.ts
@@ -6,6 +6,7 @@ test.describe("C752 Verify Change password with invalid current pass and valid n
 
   test.beforeEach(async ({ io }) => {
     await io.myAccountPage.navigateTo(io.data.links.MY_ACCOUNT_PAGE_URL);
+    await io.myAccountPage.loadingTime();
   });
 
   test("@Env-All @Zephyr-IO-T1401 C752 Verify Change password with invalid current pass and valid new pass - should show error messsage @smoke", async ({
